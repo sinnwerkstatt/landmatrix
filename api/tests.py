44fe1c7d5524f972fdd97d25b9e54088fc2c9bb4
Lene Preuss <lene.preuss@gmail.com>
2015-06-03 18:39:43 +0200

    added some tests for the REST API

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from django.core.urlresolvers import reverse
+from landmatrix.models import Involvement
+import json
 
-# Create your tests here.
+class ApiViewTest(TestCase):
+
+    def test_view_gets_called(self):
+        response = self.client.get('/api/involvement/')
+        self.assertEqual(200, response.status_code)
+
+    def test_view_returns_json(self):
+        response = self.client.get('/api/involvement/')
+        json.loads(response.content.decode('utf-8'))
+
+    def test_view_content(self):
+        i = Involvement()
+        i.save();
+        i = Involvement(investment_ratio = 1.23)
+        i.save();
+
+        response = self.client.get('/api/involvement/')
+        content = json.loads(response.content.decode('utf-8'))
+        print(content)
+        self.assertIsInstance(content, list)
+        self.assertGreaterEqual(2, len(content))
+        self.assertIsInstance(content[0], dict)
+        self.assertIsInstance(content[1], dict)
