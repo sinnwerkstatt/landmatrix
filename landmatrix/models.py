a1d04740a9d0ac060ea136cb33fb6e70b962bddb
Lene Preuss <lene.preuss@gmail.com>
2015-06-03 15:34:16 +0200

    added a very basic model (with equally basic tests)

--- landmatrix/models.py
+++ landmatrix/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+class Involvement(models.Model):
+
+    investment_ratio = models.DecimalField(_("Investment ratio"), blank=True, null=True, max_digits=19, decimal_places=2)
+
+    def __str__(self):
+        return str(dict(filter(lambda item: not item[0].startswith('_'), vars(self).items())))
--- landmatrix/tests.py
+++ landmatrix/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from landmatrix import models
 
-# Create your tests here.
+from .models import Involvement
+
+class InvolvementTest(TestCase):
+
+    DUMMY_INVESTMENT_RATIO = 1.23
+
+    def test_gets_created(self):
+        involvement = Involvement()
+        self.assertIsInstance(involvement, Involvement)
+
+    def test_accepts_investment_ratio(self):
+        involvement = Involvement(investment_ratio=self.DUMMY_INVESTMENT_RATIO)
+        self.assertEqual(self.DUMMY_INVESTMENT_RATIO, involvement.investment_ratio)
+
+    def test_str(self):
+        involvement = Involvement(investment_ratio=self.DUMMY_INVESTMENT_RATIO)
+        self.assertTrue(str(self.DUMMY_INVESTMENT_RATIO) in str(involvement))
