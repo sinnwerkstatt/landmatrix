bb6c85ee2fc1e20e4a2d5574f8b3751e142ec6a9
Lene Preuss <lene.preuss@gmail.com>
2015-06-04 12:54:39 +0200

    added (dumb, for now) models for activity and stakeholder

--- landmatrix/models.py
+++ landmatrix/models.py
@@ -12,3 +12,13 @@ class DefaultStringRepresentation:
 class Involvement(DefaultStringRepresentation, models.Model):
     investment_ratio = models.DecimalField(_("Investment ratio"), blank=True, null=True, max_digits=19, decimal_places=2)
 
+class Activity(DefaultStringRepresentation, models.Model):
+    activity_identifier = models.IntegerField(_("Activity identifier"), db_index=True)
+    version = models.IntegerField(_("Version"), db_index=True)
+    availability = models.FloatField(_("availability"), blank=True, null=True)
+    fully_updated = models.DateTimeField(_("Fully updated"), blank=True, null=True)
+
+class Stakeholder(models.Model):
+    id = models.AutoField(primary_key=True)
+    stakeholder_identifier = models.IntegerField(_("Stakeholder id"), db_index=True)
+    version = models.IntegerField(_("Version"), db_index=True)
--- landmatrix/tests.py
+++ landmatrix/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from .models import Involvement
+from .models import Involvement, Activity, Stakeholder
 
 class InvolvementTest(TestCase):
 
@@ -24,3 +24,32 @@ class InvolvementTest(TestCase):
         involvement.save()
         self.assertEqual(old_count+1, Involvement.objects.count())
         self.assertEqual(self.DUMMY_INVESTMENT_RATIO, float(Involvement.objects.last().investment_ratio))
+
+from django.utils import timezone
+
+class ActivityTest(TestCase):
+
+    def test_gets_created_with_params(self):
+        activity = Activity(activity_identifier=1, version=1, availability=0.5, fully_updated=timezone.now())
+        self.assertIsInstance(activity, Activity)
+        self.assertEqual(1, activity.activity_identifier)
+        self.assertEqual(1, activity.version)
+        self.assertEqual(0.5, activity.availability)
+
+    def test_gets_saved(self):
+        Activity(activity_identifier=1, version=1, availability=0.5, fully_updated=timezone.now()).save()
+        activity = Activity.objects.last()
+        self.assertIsInstance(activity, Activity)
+        self.assertEqual(1, activity.activity_identifier)
+        self.assertEqual(1, activity.version)
+        self.assertEqual(0.5, activity.availability)
+
+
+class StakeholderTest(TestCase):
+
+    def test_gets_saved(self):
+        Stakeholder(stakeholder_identifier=1, version=2).save()
+        stakeholder = Stakeholder.objects.last()
+        self.assertIsInstance(stakeholder, Stakeholder)
+        self.assertEqual(1, stakeholder.stakeholder_identifier)
+        self.assertEqual(2, stakeholder.version)
