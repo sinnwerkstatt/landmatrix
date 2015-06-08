f970c6b916a5abfde6232585f398ee53b9e03fa0
Lene Preuss <lp@sinnwerkstatt.com>
2015-06-08 13:23:13 +0200

    added model Language

---
+++ landmatrix/models/language.py
@@ -0,0 +1,10 @@
+__author__ = 'Lene Preuss <lp@sinnwerkstatt.com>'
+
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from landmatrix.models.default_string_representation import DefaultStringRepresentation
+
+class Language(DefaultStringRepresentation, models.Model):
+    english_name = models.CharField(_("English name"), max_length=255)
+    local_name = models.CharField(_("Local name"), max_length=255)
+    locale = models.CharField(_("Locale"), max_length=31)
