15e5f27827cf17831f4815aed5cd56dd2f52f75c
Lene Preuss <lp@sinnwerkstatt.com>
2015-06-08 13:54:53 +0200

    removed duplicate code from tests

---
+++ landmatrix/tests/with_status.py
@@ -0,0 +1,8 @@
+__author__ = 'Lene Preuss <lp@sinnwerkstatt.com>'
+
+from django.test import TestCase
+from landmatrix.models import Status
+
+class WithStatus(TestCase):
+    def setUp(self):
+        self.status = Status.objects.get(id=1)
