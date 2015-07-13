45b0bd74add0157ecee8139c5d8f5dee21f16523
Lene Preuss <lp@sinnwerkstatt.com>
2015-07-13 12:06:27 +0200

    execute live db test from inside unit tests. still fails, though.

--- global_app/tests/test_sql_builder.py
+++ global_app/tests/test_sql_builder.py
@@ -11,3 +11,9 @@ class TestSQLBuilder(TestCase):
         self.assertTrue('LEFT JOIN landmatrix_activity' in join)
         self.assertTrue('AS a' in join)
         self.assertTrue('ON local.fk_activity_id = a.id' in join)
+
+    def test_compare_to_v1_data(self):
+        from .compare_sql_results import run_test
+        num_errors = run_test(False, False)
+        print('num errors:', num_errors)
+        self.assertEqual(0, num_errors)
