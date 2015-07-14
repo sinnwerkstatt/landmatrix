ac4d46879a466a44069149762780e5b430a40cbb
Lene Preuss <lp@sinnwerkstatt.com>
2015-07-14 19:08:12 +0200

    first (baby) steps in using the ORM to generate queries

--- global_app/tests/test_sql_builder.py
+++ global_app/tests/test_sql_builder.py
@@ -4,6 +4,7 @@ from django.test import TestCase
 from landmatrix.models import Activity
 
 from global_app.views.sql_builder import join_expression
+
 class TestSQLBuilder(TestCase):
 
     def test_join_expression(self):
@@ -18,3 +19,17 @@ class TestSQLBuilder(TestCase):
         dir = os.path.dirname(os.path.realpath(__file__))
         num_errors = call(['python', dir+'/compare_sql_results.py'])
         self.assertEqual(0, num_errors)
+
+
+from .deals_test_data import DealsTestData
+class TestORMGeneratedQueries(TestCase, DealsTestData):
+
+    def test_simple_join(self):
+        for i in range(1,7):
+            self.create_activity_with_status(i, act_id=i%3+1, version=i/3+1)
+        objects = Activity.objects.all()
+        self.assertEqual(6, len(objects))
+        objects = Activity.objects.filter(fk_status__name__in=['active', 'overwritten'])
+        self.assertEqual(2, len(objects))
+
+#    def test_
