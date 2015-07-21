e29a7a7d1a7219cb3e6a4be4bdccf14b21f2dc6f
Lene Preuss <lene.preuss@gmail.com>
2015-07-21 18:32:36 +0200

    replaced testdata that were generated with a borked sql query (ignoring deals which have no intention set)

--- global_app/tests/data/generate_sql_testdata.py
+++ global_app/tests/data/generate_sql_testdata.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
 
 
-proj_path = os.path.join(os.path.dirname(__file__), "../..")
+proj_path = os.path.join(os.path.dirname(__file__), "../../..")
 # This is so Django knows where to find stuff.
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "landmatrix.settings")
 sys.path.append(proj_path)
@@ -28,7 +28,7 @@ def read_data(filename):
     with open(os.path.dirname(os.path.realpath(__file__)) + '/' + filename, 'r') as f:
         lines = f.readlines()
     parameters = eval(lines[1])
-    return parameters['data'][0]
+    return parameters['data']
 
 files_to_generate = [
     'by_crop',
@@ -39,11 +39,10 @@ files_to_generate = [
 ]
 
 for filename in files_to_generate:
-    postdata = read_data(filename+'.out')
+    postdata = read_data('landmatrix_' + filename + '.out')
     protocol = DummyActivityProtocol()
     request = HttpRequest()
     request.POST = {'data': postdata}
     res = protocol.dispatch(request, action="list_group").content
-    print(res)
     open('/tmp/landmatrix_%s.out' % filename, 'a+').write(str(dict(request.POST))+"\n")
     open('/tmp/landmatrix_%s.out' % filename, 'a+').write(str(json.loads(res.decode('utf-8'))['activities'])+"\n")
