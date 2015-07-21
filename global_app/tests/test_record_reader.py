93ba754e25feac646cf3c19f852a71c801a0283c
Lene Preuss <lene.preuss@gmail.com>
2015-07-21 15:37:28 +0200

    use SubQueryBuilder and RecordReader to tie together columns from multiple queries into one result. not sure the result is perfect yet.

--- global_app/tests/test_record_reader.py
+++ global_app/tests/test_record_reader.py
@@ -1,1 +1,75 @@
-__author__ = 'lene'
+__author__ = 'Lene Preuss <lp@sinnwerkstatt.com>'
+
+from global_app.views.record_reader import RecordReader
+
+from django.test import TestCase
+
+null = None
+
+class TestRecordReader(TestCase):
+
+    GROUP_VIEW_PARAMETERS = [
+        {"columns": ["deal_id", "target_country", "primary_investor", "investor_name", "investor_country", "intention", "negotiation_status", "implementation_status", "intended_size", "contract_size"],
+         "filters": {"deal_scope": "transnational", "order_by": ["deal_id"], "group_by": "all", "limit": "",
+                     "activity": {"tags": {"pi_negotiation_status__in": ["Concluded (Oral Agreement)", "Concluded (Contract signed)"]}}, "group_value": "",
+                     "investor": {"tags": {}}, "starts_with": null}},
+        {"columns": ["crop", "intention", "deal_count", "availability"],
+         "filters": {"deal_scope": "transnational", "order_by": ["crop"], "group_by": "crop", "limit": "",
+                     "activity": {"tags": {"pi_negotiation_status__in": ["Concluded (Oral Agreement)", "Concluded (Contract signed)"]}}, "group_value": "",
+                     "investor": {"tags": {}}, "starts_with": null}},
+        {"columns": ["data_source_type", "intention", "deal_count", "availability"],
+         "filters": {"deal_scope": "transnational", "order_by": ["data_source_type"], "group_by": "data_source_type", "limit": "",
+                     "activity": {"tags": {"pi_negotiation_status__in": ["Concluded (Oral Agreement)", "Concluded (Contract signed)"]}}, "group_value": "",
+                     "investor": {"tags": {}}, "starts_with": null}},
+        {"columns": ["intention", "deal_count", "availability"],
+         "filters": {"deal_scope": "transnational", "order_by": ["intention"], "group_by": "intention", "limit": "",
+                     "activity": {"tags": {"pi_negotiation_status__in": ["Concluded (Oral Agreement)", "Concluded (Contract signed)"]}}, "group_value": "",
+                     "investor": {"tags": {}}, "starts_with": null}},
+        {"columns": ["investor_name", "investor_country", "intention", "deal_count", "availability"],
+         "filters": {"deal_scope": "transnational", "order_by": ["investor_name"], "group_by": "investor_name", "limit": "",
+                     "activity": {"tags": {"pi_negotiation_status__in": ["Concluded (Oral Agreement)", "Concluded (Contract signed)"]}}, "group_value": "",
+                     "investor": {"tags": {}}, "starts_with": null}},
+        {"columns": ["investor_country", "investor_region", "intention", "deal_count", "availability"],
+         "filters": {"deal_scope": "transnational", "order_by": ["investor_country"], "group_by": "investor_country", "limit": "",
+                     "activity": {"tags": {"pi_negotiation_status__in": ["Concluded (Oral Agreement)", "Concluded (Contract signed)"]}}, "group_value": "",
+                     "investor": {"tags": {}}, "starts_with": null}},
+        {"columns": ["investor_region", "intention", "deal_count", "availability"],
+         "filters": {"deal_scope": "transnational", "order_by": ["investor_region"], "group_by": "investor_region", "limit": "",
+                     "activity": {"tags": {"pi_negotiation_status__in": ["Concluded (Oral Agreement)", "Concluded (Contract signed)"]}}, "group_value": "",
+                     "investor": {"tags": {}}, "starts_with": null}},
+        {"columns": ["target_country", "target_region", "intention", "deal_count", "availability"],
+         "filters": {"deal_scope": "transnational", "order_by": ["target_country"], "group_by": "target_country", "limit": "",
+                     "activity": {"tags": {"pi_negotiation_status__in": ["Concluded (Oral Agreement)", "Concluded (Contract signed)"]}}, "group_value": "",
+                     "investor": {"tags": {}}, "starts_with": null}},
+        {"columns": ["target_region", "intention", "deal_count", "availability"],
+         "filters": {"deal_scope": "transnational", "order_by": ["target_region"], "group_by": "target_region", "limit": "",
+                     "activity": {"tags": {"pi_negotiation_status__in": ["Concluded (Oral Agreement)", "Concluded (Contract signed)"]}}, "group_value": "",
+                     "investor": {"tags": {}}, "starts_with": null}},
+    ]
+
+    def _test_parameters_sql(self):
+        for parameters in self.GROUP_VIEW_PARAMETERS[:1]:
+            reader = RecordReader(parameters['filters'], parameters['columns'])
+            for column in parameters['columns']:
+                print('--', column)
+                print(reader.get_column_sql(column), ';')
+
+    def test_parameters_sql_syntax(self):
+        from django.db.utils import ProgrammingError
+        for parameters in self.GROUP_VIEW_PARAMETERS[:1]:
+            reader = RecordReader(parameters['filters'], parameters['columns'])
+            for column in parameters['columns']:
+                try:
+                    reader.get_column(column)
+                except ProgrammingError as e:
+                    self.fail('SQL for column "%s" failed:\n%s' % (column, reader.get_column_sql(column)))
+
+    def test_get_all_columns(self):
+        for parameters in self.GROUP_VIEW_PARAMETERS[:1]:
+            reader = RecordReader(parameters['filters'], parameters['columns'])
+            print(reader.get_all_columns())
+
+    def test_slap_columns_together(self):
+        for parameters in self.GROUP_VIEW_PARAMETERS[:1]:
+            reader = RecordReader(parameters['filters'], parameters['columns'])
+            print(reader.slap_columns_together())
--- global_app/views/subquery_builder.py
+++ global_app/views/subquery_builder.py
@@ -1,35 +1,112 @@
 __author__ = 'Lene Preuss <lp@sinnwerkstatt.com>'
 
-from .sql_builder import SQLBuilder, join_attributes
+from .sql_builder import join_attributes
+from .list_sql_builder import ListSQLBuilder
 
-class SubqueryBuilder(SQLBuilder):
+from landmatrix.models import Status
 
+class SubqueryBuilder(ListSQLBuilder):
+
+    def column_sql(self, c):
+        if c in ("intended_size", "contract_size", "production_size"):
+            return "NULLIF(ARRAY_TO_STRING(ARRAY_AGG(DISTINCT %(name)s.attributes->'%(name)s'), ', '), '') AS %(name)s,\n" % {"name": c}
+        elif c == "data_source":
+            return "                " + self.SQL_COLUMN_MAP.get(c)[0]
+
+        try:
+            return "                " + self.SQL_COLUMN_MAP.get(c)[0]
+        except TypeError as e:
+            print('column_sql', c, self.SQL_COLUMN_MAP.get(c))
+            raise e
+        # elif c == "data_source":
+        #     return "data_source_type, data_source_url, data_source_date, data_source_organisation,\n"
+        # else:
+        #     return "%(name)s,\n" % {"name": c}
+
+    def get_group_sql(self):
+        group_by_sql = " GROUP BY a.id "
+        additional_group_by = []
+        for c in [ col for col in self.columns if not col in ["intended_size", "contract_size", "production_size", "data_source"] and not self._is_aggregate_column(col)]:
+            additional_group_by.append("%(name)s" % {"name": c})
+        if additional_group_by: group_by_sql += ', ' + ', '.join(additional_group_by)
+
+        return group_by_sql
+
+    def _is_aggregate_column(self, c):
+        return 'ARRAY_AGG' in self.SQL_COLUMN_MAP[c][0]
 
     @classmethod
     def get_base_sql(cls):
         return u"""SELECT DISTINCT
-            a.id AS id,
-            %(name)s AS name,
-%(columns)s              'dummy' AS dummy
-          FROM
-              landmatrix_activity AS a
-              JOIN landmatrix_status ON (landmatrix_status.id = a.fk_status_id)
+            a.activity_identifier,
+%(columns)s              a.id AS id
+FROM landmatrix_activity AS a
 %(from)s""" + "\n" \
-                  + join_attributes('pi_deal') + "\n" \
-                  + join_attributes('deal_scope') + """
++ join_attributes('pi_deal') + "\n" \
++ join_attributes('deal_scope') + """
 %(from_filter)s
-          WHERE
-              a.version = (
-                  SELECT max(version)
-                  FROM landmatrix_activity amax, landmatrix_status st
-                  WHERE amax.fk_status_id = st.id
-                      AND amax.activity_identifier = a.activity_identifier
-                      AND st.name IN ('active', 'overwritten', 'deleted')
-              )
-              AND landmatrix_status.name IN ('active', 'overwritten')
-              AND pi_deal.attributes->'pi_deal' = 'True'
-              AND (NOT DEFINED(intention.attributes, 'intention') OR intention.attributes->'intention' != 'Mining')
+WHERE """ + "\nAND ".join([ cls.max_version_condition(), cls.status_active_condition(), cls.is_deal_condition(), cls.not_mining_condition() ]) + """
               %(where)s
               %(where_filter)s
-          GROUP BY a.id
-    """
+%(group_by)s"""
+
+    @classmethod
+    def max_version_condition(cls):
+        return """a.version = (
+            SELECT max(version) FROM landmatrix_activity amax
+            WHERE amax.activity_identifier = a.activity_identifier AND amax.fk_status_id IN (%s)
+        )""" % ', '.join(map(str, cls.registered_status_ids()))
+
+    @classmethod
+    def status_active_condition(cls):
+        return "a.fk_status_id IN (%s)" % ', '.join(map(str, cls.valid_status_ids()))
+
+    @classmethod
+    def is_deal_condition(cls):
+        return "pi_deal.attributes->'pi_deal' = 'True'"
+
+    @classmethod
+    def not_mining_condition(cls):
+        return "a.activity_identifier NOT IN (%s)" % ', '.join(map(str, cls.mining_deals()))
+
+    miningdeals = []
+    @classmethod
+    def mining_deals(cls):
+        if not cls.miningdeals:
+            cls.miningdeals = cls.read_mining_deals()
+        return cls.miningdeals
+
+    @classmethod
+    def read_mining_deals(cls):
+        from django.db import connection
+
+        sql = """SELECT DISTINCT a.activity_identifier AS deal_id
+FROM
+landmatrix_activity AS a
+              JOIN landmatrix_status ON (landmatrix_status.id = a.fk_status_id)
+LEFT JOIN landmatrix_activityattributegroup    AS pi_deal               ON a.id = pi_deal.fk_activity_id AND pi_deal.attributes ? 'pi_deal' AND NOT (pi_deal.attributes->'pi_deal') IS NULL
+LEFT JOIN landmatrix_activityattributegroup    AS intention             ON a.id = intention.fk_activity_id AND intention.attributes ? 'intention' AND NOT (intention.attributes->'intention') IS NULL
+          WHERE
+""" + "\nAND ".join([ cls.max_version_condition(), cls.status_active_condition(), cls.is_deal_condition() ]) + """
+        AND intention.attributes->'intention' = 'Mining'"""
+
+        cursor = connection.cursor()
+        cursor.execute(sql)
+        res = cursor.fetchall()
+        print('excluded deals:', res)
+        print('excluded deals SQL:\n', sql)
+        return ['0'] + [id for sublist in res for id in sublist]
+
+    registeredstatusids = []
+    @classmethod
+    def registered_status_ids(cls):
+        if not cls.registeredstatusids:
+            cls.registeredstatusids = Status.objects.filter(name__in=['active', 'overwritten', 'deleted']).values_list('id', flat=True)
+        return cls.registeredstatusids
+
+    validstatusids = []
+    @classmethod
+    def valid_status_ids(cls):
+        if not cls.validstatusids:
+            cls.validstatusids = Status.objects.filter(name__in=['active', 'overwritten']).values_list('id', flat=True)
+        return cls.validstatusids
