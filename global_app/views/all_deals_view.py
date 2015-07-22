abaf809d72cb090bdd7a5c99c24cc97b43f1d805
Lene Preuss <lene.preuss@gmail.com>
2015-07-22 18:26:35 +0200

    separated all deals view and table group view into one file per class

--- global_app/views/all_deals_view.py
+++ global_app/views/all_deals_view.py
@@ -5,6 +5,9 @@ from global_app.views.table_group_view import TableGroupView
 
 class AllDealsView(TableGroupView):
 
+    template_name = "all_deals.html"
+
     def dispatch(self, request, type, *args, **kwargs):
+        print('AllDealsView.dispatch', request.path, request.GET, request.POST)
         kwargs["group"] = "all%s" % (type and type or "")
         return super(AllDealsView, self).dispatch(request, *args, **kwargs)
