5f80ac7be325822c3610cb3f98e785f1ce068447
Lene Preuss <lene.preuss@gmail.com>
2015-07-23 16:39:55 +0200

    ensure that all views display SOME data. (not checking the data for correctness yet.) (except for the by year view, whose SQL is broken, as i discovered while doing the tests. ill fixx that later.)

--- global_app/tests/test_view_base.py
+++ global_app/tests/test_view_base.py
@@ -2,26 +2,55 @@ __author__ = 'Lene Preuss <lp@sinnwerkstatt.com>'
 
 from .deals_test_data import DealsTestData
 
+def extract_tag(string, open_tag, close_tag):
+    start = string.find('>', string.find(open_tag))
+    if start < 0: return ''
+    end = string.find(close_tag, start)
+    return string[start+1:end]
+
 
 class TestViewBase(DealsTestData):
 
+    """Sadly, every class derived from TestViewBase needs to explicitly call TestViewBase.setUp()"""
     def setUp(self):
         self.create_data()
-        self.content = self.get_url_following_redirects(self.VIEW_URL).content.decode('utf-8')
+        self.content = self._get_url_following_redirects(self.VIEW_URL).content.decode('utf-8')
 
     # disabled because no django-cms pages configured, but redirects are applied
     def _test_anything_loads(self):
-        response = self.get_url_following_redirects('/')
+        response = self._get_url_following_redirects('/')
         print(response.content.decode('utf-8'))
         self.assertEqual(200, response.status_code)
 
     def test_view_loads(self):
-        response = self.get_url_following_redirects(self.VIEW_URL)
+        response = self._get_url_following_redirects(self.VIEW_URL)
         self.assertEqual(200, response.status_code)
 
-    def get_url_following_redirects(self, url):
+    def test_content(self):
+        tbody = extract_tag(self.content, '<tbody>', '</tbody>')
+        if not tbody:
+            self.fail(self.__class__.__name__ + ' delivers crap')
+#            print(self.content)
+        else:
+            print(list(map(self._extract_info_from_cell, self._extract_cells(tbody))))
+
+    def _get_url_following_redirects(self, url):
         response = self.client.get(url)
         while response.status_code in range(300, 308):
             response = self.client.get(response.url)
         return response
+
+    def _extract_cells(self, tbody, found=list()):
+        start = tbody.find('<td')
+        if start < 0: return found
+        realstart = tbody.find('>', start)
+        end = tbody.find('</td>', realstart)
+        return self._extract_cells(tbody[end+1:], found + [tbody[realstart+1:end].strip()])
+
+    def _extract_info_from_cell(self, cell):
+        if '<a href="' in cell: return self._extract_info_from_a(cell)
+        return cell
+
+    def _extract_info_from_a(self, cell):
+        return extract_tag(cell, '<a href="', '</a>')
 
--- global_app/views/all_deals_view.py
+++ global_app/views/all_deals_view.py
@@ -8,6 +8,5 @@ class AllDealsView(TableGroupView):
     template_name = "all_deals.html"
 
     def dispatch(self, request, type, *args, **kwargs):
-        print('AllDealsView.dispatch', request.path, request.GET, request.POST)
         kwargs["group"] = "all%s" % (type and type or "")
         return super(AllDealsView, self).dispatch(request, *args, **kwargs)
