b664b45e01caadc18aed272c0d8e045540ff5e78
Lene Preuss <lene.preuss@gmail.com>
2015-07-23 11:46:07 +0200

    added template for group view

--- global_app/tests/test_all_deals_view.py
+++ global_app/tests/test_all_deals_view.py
@@ -1,7 +1,10 @@
 __author__ = 'Lene Preuss <lp@sinnwerkstatt.com>'
 
 from global_app.tests.test_view_base import TestViewBase
+
+from django.conf import settings
 from django.test import TestCase
+
 
 class TestAllDealsView(TestViewBase, TestCase):
 
@@ -12,13 +15,16 @@ class TestAllDealsView(TestViewBase, TestCase):
     def setUp(self):
         TestViewBase.setUp(self)
 
+    def test_view_contains_investor_name(self):
+        if True or settings.DEBUG: print(self.content, file=open('/tmp/testresult.html', 'w'))
+        self.assertIn(self.PI_NAME, self.content)
 
-if False:
+if True:
 
     class TestGroupView(TestViewBase, TestCase):
 
         # we use global_app as defined in landmatrix.url here because django-cms pages are not configured in test db
-        VIEW_URL = '/en/global_app/group/by-target-country/'
+        VIEW_URL = '/en/global_app/crop/by-target-country/'
 
         "Sadly, every class derived from TestViewBase needs to explicitly call TestViewBase.setUp()"
         def setUp(self):
--- global_app/tests/test_view_base.py
+++ global_app/tests/test_view_base.py
@@ -1,13 +1,13 @@
 __author__ = 'Lene Preuss <lp@sinnwerkstatt.com>'
 
-from django.conf import settings
-
 from .deals_test_data import DealsTestData
+
 
 class TestViewBase(DealsTestData):
 
     def setUp(self):
         self.create_data()
+        self.content = self.get_url_following_redirects(self.VIEW_URL).content.decode('utf-8')
 
     # disabled because no django-cms pages configured, but redirects are applied
     def _test_anything_loads(self):
@@ -18,11 +18,6 @@ class TestViewBase(DealsTestData):
     def test_view_loads(self):
         response = self.get_url_following_redirects(self.VIEW_URL)
         self.assertEqual(200, response.status_code)
-
-    def test_view_contains_data(self):
-        content = self.get_url_following_redirects(self.VIEW_URL).content.decode('utf-8')
-        if True or settings.DEBUG: print(content, file=open('/tmp/testresult.html', 'w'))
-        self.assertIn(self.PI_NAME, content)
 
     def get_url_following_redirects(self, url):
         response = self.client.get(url)
