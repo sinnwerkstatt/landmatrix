f7ca97a749da5f82ee79216b99f59524b03de12b
Lene Preuss <lene.preuss@gmail.com>
2015-07-27 12:04:13 +0200

    test all group views for presence of predefined words - not many as of yet

--- global_app/tests/test_all_deals_view.py
+++ global_app/tests/test_all_deals_view.py
@@ -1,6 +1,8 @@
 __author__ = 'Lene Preuss <lp@sinnwerkstatt.com>'
 
+from global_app.tests.deals_test_data import DealsTestData
 from global_app.tests.test_view_base import TestViewBase
+from landmatrix.models import Country
 
 from django.conf import settings
 from django.test import TestCase
@@ -14,13 +16,14 @@ class TestAllDealsView(TestViewBase, TestCase):
     "Sadly, every class derived from TestViewBase needs to explicitly call TestViewBase.setUp()"
     def setUp(self):
         TestViewBase.setUp(self)
+        self.create_country()
+        self.EXPECTED_VIEW_DATA = [ self.country.name, DealsTestData.PI_NAME ]
 
     def test_view_contains_investor_name(self):
         if True or settings.DEBUG: print(self.content, file=open('/tmp/testresult.html', 'w'))
         self.assertIn(self.PI_NAME, self.content)
 
     def test_view_contains_country(self):
-        from landmatrix.models import Country
         try:
             from html import unescape
             self.assertIn(Country.objects.last().name, unescape(self.content))
@@ -33,55 +36,73 @@ class TestAllDealsView(TestViewBase, TestCase):
 class TestInvestorRegionGroupView(TestViewBase, TestCase):
 
     VIEW_URL = '/en/global_app/by-investor-region/none/'
-    def setUp(self): TestViewBase.setUp(self)
+    EXPECTED_VIEW_DATA = [ 'Agriculture' ]
+    def setUp(self):
+        TestViewBase.setUp(self)
 
 if True:
 
     class TestTargetCountryGroupView(TestViewBase, TestCase):
 
         VIEW_URL = '/en/global_app/by-target-country/none/'
-        def setUp(self): TestViewBase.setUp(self)
+        EXPECTED_VIEW_DATA = [ 'Agriculture' ]
+        def setUp(self):
+            TestViewBase.setUp(self)
 
 
     class TestTargetRegionGroupView(TestViewBase, TestCase):
 
         VIEW_URL = '/en/global_app/by-target-region/none/'
-        def setUp(self): TestViewBase.setUp(self)
+        EXPECTED_VIEW_DATA = [ 'Agriculture' ]
+        def setUp(self):
+            TestViewBase.setUp(self)
 
 
     class TestInvestorNameGroupView(TestViewBase, TestCase):
 
         VIEW_URL = '/en/global_app/by-investor-name/none/'
-        def setUp(self): TestViewBase.setUp(self)
+        EXPECTED_VIEW_DATA = [ 'Agriculture' ]
+        def setUp(self):
+            TestViewBase.setUp(self)
 
 
     class TestInvestorCountryGroupView(TestViewBase, TestCase):
 
         VIEW_URL = '/en/global_app/by-investor-country/none/'
-        def setUp(self): TestViewBase.setUp(self)
+        EXPECTED_VIEW_DATA = [ 'Agriculture' ]
+        def setUp(self):
+            TestViewBase.setUp(self)
 
 
     class TestIntentionGroupView(TestViewBase, TestCase):
 
         VIEW_URL = '/en/global_app/by-intention/none/'
-        def setUp(self): TestViewBase.setUp(self)
+        EXPECTED_VIEW_DATA = [ 'Agriculture' ]
+        def setUp(self):
+            TestViewBase.setUp(self)
 
 
     class TestCropGroupView(TestViewBase, TestCase):
 
         VIEW_URL = '/en/global_app/by-crop/none/'
-        def setUp(self): TestViewBase.setUp(self)
+        EXPECTED_VIEW_DATA = [ 'Agriculture' ]
+        def setUp(self):
+            TestViewBase.setUp(self)
 
 
     class TestDataSourceTypeView(TestViewBase, TestCase):
 
         VIEW_URL = '/en/global_app/by-data-source-type/none/'
-        def setUp(self): TestViewBase.setUp(self)
+        EXPECTED_VIEW_DATA = [ 'Agriculture' ]
+        def setUp(self):
+            TestViewBase.setUp(self)
 
 if False:
     class TestYearGroupView(TestViewBase, TestCase):
 
         VIEW_URL = '/en/global_app/by-year/none/'
-        def setUp(self): TestViewBase.setUp(self)
+        EXPECTED_VIEW_DATA = [ 'Agriculture' ]
+        def setUp(self):
+            TestViewBase.setUp(self)
 
 
--- global_app/tests/test_view_base.py
+++ global_app/tests/test_view_base.py
@@ -31,14 +31,22 @@ class TestViewBase(DealsTestData):
         self.assertNotEqual(tbody, '', self.__class__.__name__ + ' does not find any data.')
 
     def test_view_data_ok(self):
-        tbody = extract_tag(self.content, '<tbody>', '</tbody>')
-        print(list(map(self._extract_info_from_cell, self._extract_cells(tbody))))
+        self.assertTrue(all(expected in self.extract_view_data() for expected in self.EXPECTED_VIEW_DATA))
 
     def _get_url_following_redirects(self, url):
         response = self.client.get(url)
         while response.status_code in range(300, 308):
             response = self.client.get(response.url)
         return response
+
+    def extract_view_data(self):
+        try:
+            from html import unescape
+            tbody = extract_tag(unescape(self.content), '<tbody>', '</tbody>')
+            return list(map(self._extract_info_from_cell, self._extract_cells(tbody)))
+        except ImportError:
+            self.skipTest('html.unescape needs Python >= 3.4')
+
 
     def _extract_cells(self, tbody, found=list()):
         start = tbody.find('<td')
@@ -48,7 +56,9 @@ class TestViewBase(DealsTestData):
         return self._extract_cells(tbody[end+1:], found + [tbody[realstart+1:end].strip()])
 
     def _extract_info_from_cell(self, cell):
-        if '<a href="' in cell: return self._extract_info_from_a(cell)
+        if '<a href="' in cell: cell = self._extract_info_from_a(cell)
+        if '<span' in cell: cell = extract_tag(cell, '<span', '</span>')
+        if '<br/>' in cell: cell = cell[:-5]
         return cell
 
     def _extract_info_from_a(self, cell):
