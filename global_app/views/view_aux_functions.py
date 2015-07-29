0a7cb59ca78680893dbb32d8453fdb9deadfaa1c
Lene Preuss <lene.preuss@gmail.com>
2015-07-29 18:19:16 +0200

    refactoring TableGroupView - pulling out some functionalities that can be neatly separated

--- global_app/views/view_aux_functions.py
+++ global_app/views/view_aux_functions.py
@@ -1,5 +1,10 @@
 __author__ = 'Lene Preuss <lp@sinnwerkstatt.com>'
 
+from global_app.views.browse_condition_form import BrowseConditionForm
+
+from django.template import loader
+from django.http import HttpResponse
+
 def parse_browse_filter_conditions(formset, order_by=None, limit=None):
     data = {
         "activity": {},
@@ -148,3 +153,24 @@ def get_display_value_by_field(field, value):
         dvalue = value == "on" and "True" or value == "off" and "False" or None
         return dvalue or value
     return value
+
+FILTER_VAR_ACT = ["target_country", "location", "intention", "intended_size", "contract_size", "production_size", "negotiation_status", "implementation_status", "crops", "nature", "contract_farming", "url", "type", "company", "type"]
+FILTER_VAR_INV = ["investor_name", "country"]
+def create_condition_formset():
+    from django.forms.formsets import formset_factory
+    from django.utils.functional import curry
+
+    ConditionFormset = formset_factory(BrowseConditionForm, extra=0)
+    ConditionFormset.form = staticmethod(
+        curry(BrowseConditionForm, variables_activity=FILTER_VAR_ACT, variables_investor=FILTER_VAR_INV)
+    )
+    return ConditionFormset
+
+""" Returns a HttpResponse whose content is filled with the result of calling
+    django.template.loader.render_to_string() with the passed arguments."""
+def render_to_response(template_name, context, context_instance):
+    # Some deprecated arguments were passed - use the legacy code path
+    content = loader.render_to_string(template_name, context, context_instance)
+
+    return HttpResponse(content)
+
