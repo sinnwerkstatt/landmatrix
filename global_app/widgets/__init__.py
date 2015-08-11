344573fd910ce554cac6f4bc4bf18f9e15a4bc19
Lene Preuss <lene.preuss@gmail.com>
2015-08-11 18:14:59 +0200

    factored out more widgets into separate files

--- global_app/widgets/__init__.py
+++ global_app/widgets/__init__.py
@@ -1,6 +1,11 @@
 from .number_input import NumberInput
 from .decimal_input import DecimalInput
 from .comment_input import CommentInput
+from .title_field import TitleField
+from .user_model_choice_field import UserModelChoiceField
+from .location_widget import LocationWidget
+from .nested_checkbox_select_multiple import NestedCheckboxSelectMultiple
+from.select_all_checkbox_select_multiple import SelectAllCheckboxSelectMultiple
 
 from .widgets import *
 
