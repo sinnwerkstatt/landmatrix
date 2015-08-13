9c5a6e0ac85cf3bb503df6dca5b95f806f33436f
Lene Preuss <lene.preuss@gmail.com>
2015-08-13 16:49:28 +0200

    threw out more dead code, tightened up other code a little

--- global_app/views/sql_generation/subquery_builder.py
+++ global_app/views/sql_generation/subquery_builder.py
@@ -7,22 +7,14 @@ from global_app.views.sql_generation.list_sql_builder import ListSQLBuilder
 class SubqueryBuilder(ListSQLBuilder):
 
     def column_sql(self, c):
-        if c in ("intended_size", "contract_size", "production_size"):
-            return "NULLIF(ARRAY_TO_STRING(ARRAY_AGG(DISTINCT %(name)s.attributes->'%(name)s'), ', '), '') AS %(name)s" % {"name": c}
-
         try:
             return self.SQL_COLUMN_MAP.get(c)[0]
         except TypeError:
             raise KeyError(c)
 
     def get_group_sql(self):
-        group_by_sql = " GROUP BY a.id "
-        additional_group_by = []
-        for c in [ col for col in self.columns if not col in ["intended_size", "contract_size", "production_size", "data_source"] and not self.is_aggregate_column(col)]:
-            additional_group_by.append("%(name)s" % {"name": c})
-        if additional_group_by: group_by_sql += ', ' + ', '.join(additional_group_by)
-
-        return group_by_sql
+        group_by = ["a.id "] + [col for col in self.columns if not self.is_aggregate_column(col)]
+        return 'GROUP BY ' + ', '.join(group_by)
 
     @classmethod
     def get_base_sql(cls):
@@ -30,9 +22,9 @@ class SubqueryBuilder(ListSQLBuilder):
 a.activity_identifier,
 %(columns)s, a.id AS id
 FROM landmatrix_activity AS a
-%(from)s""" + "\n" \
-+ join_attributes('pi_deal') + "\n" \
-+ join_attributes('deal_scope') + """
+%(from)s
+""" + join_attributes('pi_deal') + """
+""" + join_attributes('deal_scope') + """
 %(from_filter)s
 WHERE """ + "\nAND ".join([ cls.max_version_condition(), cls.status_active_condition(), cls.is_deal_condition(), cls.not_mining_condition() ]) + """
 %(where)s
