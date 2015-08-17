6f9941715d39aa8671f6088d8af4c6526124725c
Lene Preuss <lene.preuss@gmail.com>
2015-08-17 13:16:17 +0200

    added apphook and route to chart_view

--- chart_view/views.py
+++ chart_view/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+from global_app.views.view_aux_functions import render_to_response
+
+from django.views.generic.base import TemplateView
+from django.template import RequestContext
+
+
+class ChartsView(TemplateView):
+    template_name = "plugins/overview.html"
+
+    def dispatch(self, request, *args, **kwargs):
+        print('BOOOOOOOOOOOOOOH!')
+        context = {
+            "view": "chart view",
+
+
+        }
+        return render_to_response(self.template_name, context, context_instance=RequestContext(request))
