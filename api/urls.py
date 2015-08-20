50c26dbdbf244121eeef72c140c634df63087044
Lene Preuss <lene.preuss@gmail.com>
2015-08-20 16:39:23 +0200

    created a view with some random static data needed by JS to render a graph

--- api/urls.py
+++ api/urls.py
@@ -18,6 +18,9 @@ if USE_TASTY:
     for klass in filter(lambda c: c.__module__.startswith('api.'), ModelResource.__subclasses__()):
         api.register(klass())
 
+    urlpatterns.append(
+        url(r'^(?P<type>.*\.json)', views.stats, name='landmatrix_api')
+    )
     urlpatterns.append(url(r'^api', include(api.urls)))
 
 if USE_REST:
@@ -30,6 +33,10 @@ if USE_REST:
     router.register(r'status', views.StatusViewSet)
     router.register(r'activity_attribute_group', views.ActivityAttributeGroupViewSet)
     router.register(r'statistics', views.stats, base_name='statistics')
+
+    urlpatterns.append(
+        url(r'^(?P<type>.*\.json)', views.stats, name='landmatrix_api')
+    )
 
     urlpatterns.append(url(r'^', include(router.urls)))
 
