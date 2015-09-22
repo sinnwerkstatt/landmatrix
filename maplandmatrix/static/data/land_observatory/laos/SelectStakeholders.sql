f5c2162a8227db41bb5f868c353ab4c952a86d68
Marie <marie.beggiato@sinnwerkstatt.com>
2015-09-22 13:21:23 +0200

    change clusters color: only unique markers have color by investment,changes in CSS for the control buttons, change coordinates projection of mouse control, create LayerSwitcher

--- maplandmatrix/static/data/land_observatory/laos/SelectStakeholders.sql
+++
@@ -1,9 +0,0 @@
---SELECT id_company FROM data.project WHERE data.project.project_code ILIKE '05%' GROUP BY id_company;
-
---SELECT data.project.id_company FROM data.geo_point
---JOIN data.project ON data.geo_point.project_code = data.project.project_code
---WHERE data.project.project_code ILIKE '05%' GROUP BY data.project.id_company;
-
-SELECT data.lut_company."name_eng", data.lut_country."name_eng" FROM data.lut_company
-JOIN data.lut_country ON data.lut_company.id_country = data.lut_country.id
-WHERE data.lut_company.id = 2614;
--- maplandmatrix/static/js/map.js
+++ maplandmatrix/static/js/map.js
@@ -2,52 +2,91 @@ var map;
 var cluster = new Array();
 
 $(document).ready(function() {
-	// GrundKarte Open Street Map + GeoJSON Countries Borders
-	map = new ol.Map({
-		layers: [
-			/**new ol.layer.Tile({
-				source: new ol.source.OSM()
-			}),*/
-			new ol.layer.Tile({
-				source: new ol.source.Stamen({
-					layer: 'toner'
+		// ALL Layers 
+		map = new ol.Map({
+			target: 'map',
+			// Base Maps Layers
+			layers: [
+				new ol.layer.Group({
+					'title':'Base Maps',
+					layers: [
+						new ol.layer.Tile({
+							title:'OSM',
+							type:'base',
+							visible:false,
+							source: new ol.source.OSM()
+						}),
+						new ol.layer.Tile({
+							title:'Satellite',
+							type:'base',
+							visible:false,
+							source: new ol.source.MapQuest({layer:'sat'})
+						}),
+						new ol.layer.Tile({
+							title:'Toner',
+							type:'base',
+							visible:true,
+							source: new ol.source.Stamen({
+								layer:'toner'
+							})
+						}),
+					]
+				}),
+				// Context Layers from the Landobservatory Geoserver
+				new ol.layer.Group({
+					title:'Context Layers',
+					layers:[
+						new ol.layer.Tile({
+							title:'Global Cropland',
+							source: new ol.source.TileWMS({
+								url:'',
+								params:{'LAYERS':''},
+								serverType:'geoserver'
+							})
+						}),
+						new ol.layer.Tile({
+							title:'Global Landcover',
+							source: new ol.source.TileWMS({
+								url:'',
+								params:{'LAYERS':''},
+								serverType:'geoserver'
+							})
+						}),
+					]
 				})
+			],
+			controls:[
+								new ol.control.Zoom (),
+								new ol.control.ScaleLine(),
+								new ol.control.MousePosition({
+    								projection: 'EPSG:4326',
+  			  						coordinateFormat: function(coordinate) {
+      									return ol.coordinate.format(coordinate, '{y}, {x}', 4);
+    								}
+    							}),
+								new ol.control.FullScreen(),
+								new ol.control.Attribution,
+			],
+			interactions : [
+								new ol.interaction.Select(),
+								new ol.interaction.MouseWheelZoom(),
+								new ol.interaction.PinchZoom(),
+								new ol.interaction.DragZoom (),
+								new ol.interaction.DoubleClickZoom(),
+								new ol.interaction.DragPan(), 
+			],
+			view: new ol.View({
+				center: [0,0],
+				zoom: 2
 			}),
-			new ol.layer.Vector({
-				source : new ol.source.Vector({
-					url :'/static/data/geodata/countries_simple.geojson',
-					format: new ol.format.GeoJSON(),
-				}),
-				style : new ol.style.Style({
-					stroke : new ol.style.Stroke({
-						color :  '#319FD3',
-						width : 0.5
-					}),
-					zIndex : 999
-				}),
-			}),
-		],
-		controls:[
-							new ol.control.Zoom (),
-							new ol.control.ScaleLine(),
-							new ol.control.MousePosition(),
-							new ol.control.FullScreen(),
-							new ol.control.Attribution,
-		],
-		interactions : [
-							new ol.interaction.Select(),
-							new ol.interaction.MouseWheelZoom(),
-							new ol.interaction.PinchZoom(),
-							new ol.interaction.DragZoom (),
-							new ol.interaction.DoubleClickZoom(),
-							new ol.interaction.DragPan(), 
-		],
-		target: 'map',
-		view: new ol.View({
-			center: [0,0],
-			zoom: 2
-		}),
-	});
+		});
+
+// LayerSwitcher Control by https://github.com/walkermatt/ol3-layerswitcher
+		var layerSwitcher = new ol.control.LayerSwitcher({
+			tipLabel:'Legende'
+		});
+		map.addControl(layerSwitcher);
+
  // var selectedFeatures = new ol.interaction.Select().getFeatures();
 }); //gesamte document.ready.function Klammer
 
@@ -68,39 +107,47 @@ function clusteredMarkers (longitude, latitude, intention) {
 
 	var styleCache = {};
 
-// Give a color to each Intention of Investment	
-	var color = "";
-	if (intention=='Agriculture'){
-		color = 'green';
-	}
-	else if (intention=='Forestry'){
-		color= 'yellow';
-	}
-	else if (intention=='Conservation'){
-		color= 'orange';
-	}
-	else if (intention=='Industry'){
-		color= 'black';
-	}
-	else if (intention=='Renewable Energy'){
-		color= 'pink';
-	}
-	else if (intention=='Tourism'){
-		color= 'red';
-	}
-	else if (intention=='Other'){
-		color= 'grey';
-	}
-	else {
-		color='blue';
-	}
-	
 	var clusters = new ol.layer.Vector({
 	  source: clusterSource,
 	  style: function(feature, resolution) {
 	    var size = feature.get('features').length;
-	    var style = styleCache[size];
-// Define a SIZE and RADIUS MIN-MAX
+
+	//Give a color to each Intention of Investment, only for single points
+	// PROBLEM : how to clustered the points by color? 	
+		var color = "";
+			if (size > 1){
+				color = '#4C76AB';
+			}
+			else if (intention=='Agriculture'){
+				color = '#1D6914';
+			}
+			else if (intention=='Forestry'){
+				color= '#2A4BD7';
+			}
+			else if (intention=='Conservation'){
+				color= '#575757';
+			}
+			else if (intention=='Industry'){
+				color= '#AD2323';
+			}
+			else if (intention=='Renewable Energy'){
+				color= '#81C57A';
+			}
+			else if (intention=='Tourism'){
+				color= '#9DAFFF';
+			}
+			else if (intention=='Other'){
+				color= '#8126C0';
+			}
+			else if (intention=='Mining'){
+				color='#814A19';
+			}
+			else {
+				color='black';
+			}
+
+		var style = styleCache[size];
+
 	  	var radius = size/2;
 	    	if (radius > 75) {
 	    		radius = 25;
@@ -108,16 +155,17 @@ function clusteredMarkers (longitude, latitude, intention) {
 	    	else if (radius < 10) {
 	    		radius = 7;
 	    	}
+
 	    if (!style) {
 	      style = [new ol.style.Style({
 	        image: new ol.style.Circle({
-	        	radius: radius ,
-	          stroke: new ol.style.Stroke({
+	        	radius: radius,
+	        	stroke: new ol.style.Stroke({
 	            color: '#fff'
-	          }),
-	          fill: new ol.style.Fill({
-	          	color: color, 
-	          })
+	          	}),
+		        fill: new ol.style.Fill({
+		        color: color, 
+		        })
 	        }),
 	        text: new ol.style.Text({
 	          text: size.toString(),
@@ -131,7 +179,6 @@ function clusteredMarkers (longitude, latitude, intention) {
 	    return style;
 	  }
 	});	
-
 	map.addLayer(clusters);
 
 	var popup = new ol.Overlay({
@@ -145,17 +192,14 @@ function clusteredMarkers (longitude, latitude, intention) {
 	
 		$(element).popover('destroy');
 		popup.setPosition(coordinate);
-  // the keys are quoted to prevent renaming in ADVANCED mode.
-  	$(element).popover({
-	    'placement': 'top',
-	    'animation': false,
-	    'html': true,
-	    'content': '<p>Latitude:</p><code>' + latitude + '</code>'+ '<p>Longitude:</p><code>' + longitude + '</code>'+ '<p>Intention of investment:</p><code>' + intention + '</code>'
-  	});
- 	 $(element).popover('show');	
-	}); 
-		
-
+	  	$(element).popover({
+		    'placement': 'top',
+		    'animation': false,
+		    'html': true,
+		    'content': '<p>Latitude:</p><code>' + latitude + '</code>'+ '<p>Longitude:</p><code>' + longitude + '</code>'+ '<p>Intention of investment:</p><code>' + intention + '</code>'
+	  	});
+	 	 $(element).popover('show');	
+		}); 
 };
 
 
