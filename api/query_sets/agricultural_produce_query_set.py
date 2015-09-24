fc5ab7c25a5cf30cad1c17c2e4b79da98303e323
Lene Preuss <lene.preuss@gmail.com>
2015-09-24 13:27:07 +0200

    cut down query execution time by a factor of 12 by removing unneccessary JOINs

--- api/query_sets/agricultural_produce_query_set.py
+++ api/query_sets/agricultural_produce_query_set.py
@@ -37,14 +37,12 @@ LEFT JOIN landmatrix_activityattributegroup AS size             ON a.id = size.f
             )
         END AS agricultural_produce
     FROM landmatrix_activity                       AS a
-    JOIN      landmatrix_status                                        ON landmatrix_status.id = a.fk_status_id
     LEFT JOIN landmatrix_involvement               AS i                ON i.fk_activity_id = a.id
-    LEFT JOIN landmatrix_stakeholder               AS s                ON i.fk_stakeholder_id = s.id
+--    LEFT JOIN landmatrix_stakeholder               AS s                ON i.fk_stakeholder_id = s.id
     LEFT JOIN landmatrix_primaryinvestor           AS pi               ON i.fk_primary_investor_id = pi.id
-    LEFT JOIN landmatrix_status                    AS pi_st            ON pi.fk_status_id = pi_st.id
-    LEFT JOIN landmatrix_stakeholderattributegroup AS skvf1            ON s.id = skvf1.fk_stakeholder_id AND skvf1.attributes ? 'country'
-    LEFT JOIN landmatrix_country                   AS investor_country ON CAST(skvf1.attributes->'country' AS NUMERIC) = investor_country.id
-    LEFT JOIN landmatrix_region                    AS investor_region  ON investor_country.fk_region_id = investor_region.id
+--    LEFT JOIN landmatrix_stakeholderattributegroup AS skvf1            ON s.id = skvf1.fk_stakeholder_id AND skvf1.attributes ? 'country'
+--    LEFT JOIN landmatrix_country                   AS investor_country ON CAST(skvf1.attributes->'country' AS NUMERIC) = investor_country.id
+--    LEFT JOIN landmatrix_region                    AS investor_region  ON investor_country.fk_region_id = investor_region.id
     LEFT JOIN landmatrix_activityattributegroup    AS intention        ON a.id = intention.fk_activity_id AND intention.attributes ? 'intention'
     LEFT JOIN landmatrix_activityattributegroup    AS target_country   ON a.id = target_country.fk_activity_id AND target_country.attributes ? 'target_country'
     LEFT JOIN landmatrix_country                   AS deal_country     ON CAST(target_country.attributes->'target_country' AS NUMERIC) = deal_country.id
@@ -52,7 +50,7 @@ LEFT JOIN landmatrix_activityattributegroup AS size             ON a.id = size.f
     LEFT JOIN landmatrix_activityattributegroup    AS negotiation      ON a.id = negotiation.fk_activity_id AND negotiation.attributes ? 'pi_negotiation_status'
     LEFT JOIN landmatrix_activityattributegroup    AS implementation   ON a.id = implementation.fk_activity_id AND implementation.attributes ? 'pi_implementation_status'
     LEFT JOIN landmatrix_activityattributegroup    AS bf               ON a.id = bf.fk_activity_id AND bf.attributes ? 'pi_deal'
-    LEFT JOIN landmatrix_activityattributegroup    AS size             ON a.id = size.fk_activity_id AND size.attributes ? 'pi_deal_size'
+--    LEFT JOIN landmatrix_activityattributegroup    AS size             ON a.id = size.fk_activity_id AND size.attributes ? 'pi_deal_size'
     LEFT JOIN landmatrix_activityattributegroup    AS deal_scope       ON a.id = deal_scope.fk_activity_id AND deal_scope.attributes ? 'deal_scope'
     WHERE
         a.version = (
@@ -65,7 +63,7 @@ LEFT JOIN landmatrix_activityattributegroup AS size             ON a.id = size.f
             SELECT MAX(version) FROM landmatrix_primaryinvestor AS amax
             WHERE amax.primary_investor_identifier = pi.primary_investor_identifier AND amax.fk_status_id IN (2, 3, 4)
         )
-        AND pi_st.name IN ('active', 'overwritten')
+        AND pi.fk_status_id IN (2, 3)
         %s
     GROUP BY a.id
 )                                           AS sub
