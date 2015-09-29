1590b801fcf2dae5ffe794ce481fab95223d8e3f
Lene Preuss <lene.preuss@gmail.com>
2015-09-29 16:52:14 +0200

    modularized a few more query sets

--- api/views/investor_country_summaries_json_view.py
+++ api/views/investor_country_summaries_json_view.py
@@ -1,4 +1,3 @@
-from django.core.urlresolvers import reverse
 from api.query_sets.investor_country_summaries_query_set import InvestorCountrySummariesQuerySet
 from api.views.decimal_encoder import DecimalEncoder
 
@@ -11,30 +10,9 @@ __author__ = 'Lene Preuss <lp@sinnwerkstatt.com>'
 class InvestorCountrySummariesJSONView:
 
     def dispatch(self, request, *args, **kwargs):
-        countries_summary = self.get_by_investor_country(request.GET)
-
-        countries = {}
-        for c in countries_summary:
-            if not c['country_id']: continue
-            country = countries.get(c['country_id'], {"domestic": 0, "transnational": 0})
-            country.update(self.to_json_record(c, country))
-            countries.update({c['country_id']: country})
-
-        output = [v for k,v in countries.items()]
-
+        output = self.get_by_investor_country(request.GET)
         return HttpResponse(json.dumps(output, cls=DecimalEncoder), content_type="application/json")
 
-    def to_json_record(self, c, country):
-        c['name'] = c['country']
-        c["country_slug"] =c['country'].lower().replace(" ", "-")
-        c["region_slug"] =c['region'].lower().replace(" ", "-")
-        c['url'] = reverse("table_list", kwargs={"group": "by-target-country", "list": c['country'].lower().replace(" ", "-")})
-        c[c['deal_scope']] = c['deals'] + country.get(c['deal_scope'], 0)
-        c['deals'] = c['deals'] + country.get("deals", 0)
-
-        return c
-
     def get_by_investor_country(self, get):
-
         queryset = InvestorCountrySummariesQuerySet(get)
         return queryset.all()
