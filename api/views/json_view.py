a0cf4fbbfe8d17bf851a8ce1a3ff2b6bc793b362
Lene Preuss <lene.preuss@gmail.com>
2015-09-30 14:20:28 +0200

    removed even more duplicate code by generating the JSONView classes with a generator function

--- api/views/json_view.py
+++ api/views/json_view.py
@@ -1,18 +1,44 @@
-from api.views.agricultural_produce_json_view import AgriculturalProduceJSONView
+from api.query_sets.agricultural_produce_query_set import AllAgriculturalProduceQuerySet
+from api.query_sets.implementation_status_query_set import ImplementationStatusQuerySet
+from api.query_sets.intention_query_set import IntentionQuerySet
+from api.query_sets.investor_country_summaries_query_set import InvestorCountrySummariesQuerySet
+from api.query_sets.negotiation_status_query_set import NegotiationStatusQuerySet
+from api.query_sets.target_country_summaries_query_set import TargetCountrySummariesQuerySet
+from api.query_sets.top_10_countries_query_set import Top10CountriesQuerySet
+from api.query_sets.transnational_deals_by_country_query_set import TransnationalDealsByCountryQuerySet
+from api.query_sets.transnational_deals_query_set import TransnationalDealsQuerySet
+
 from api.views.hectares_json_view import HectaresJSONView
-from api.views.target_country_summaries_json_view import TargetCountrySummariesJSONView
-from api.views.investor_country_summaries_json_view import InvestorCountrySummariesJSONView
-from api.views.top_10_countries_json_view import Top10CountriesJSONView
-from api.views.transnational_deals_by_country_json_view import TransnationalDealsByCountryJSONView
 from api.views.deals_json_view import DealsJSONView
-from api.views.transnational_deals_json_view import TransnationalDealsJSONView
-from api.views.implementation_status_json_view import ImplementationStatusJSONView
-from api.views.intention_of_investment_json_view import IntentionOfInvestmentJSONView
-from api.views.negotiation_status_json_view import NegotiationStatusJSONView
+
+from api.views.decimal_encoder import DecimalEncoder
+
+from django.http.response import HttpResponse
+import json
 
 from django.views.generic.base import TemplateView
 
 __author__ = 'Lene Preuss <lp@sinnwerkstatt.com>'
+
+
+def json_generator(query_set_class):
+
+    class Generator:
+        def dispatch(self, request):
+            data = query_set_class(request.GET).all()
+            return HttpResponse(json.dumps(data, cls=DecimalEncoder), content_type="application/json")
+
+    return Generator
+
+AgriculturalProduceJSONView = json_generator(AllAgriculturalProduceQuerySet)
+ImplementationStatusJSONView = json_generator(ImplementationStatusQuerySet)
+IntentionOfInvestmentJSONView = json_generator(IntentionQuerySet)
+InvestorCountrySummariesJSONView = json_generator(InvestorCountrySummariesQuerySet)
+NegotiationStatusJSONView = json_generator(NegotiationStatusQuerySet)
+TargetCountrySummariesJSONView = json_generator(TargetCountrySummariesQuerySet)
+Top10CountriesJSONView = json_generator(Top10CountriesQuerySet)
+TransnationalDealsJSONView = json_generator(TransnationalDealsQuerySet)
+TransnationalDealsByCountryJSONView = json_generator(TransnationalDealsByCountryQuerySet)
 
 
 class JSONView(TemplateView):
@@ -35,5 +61,8 @@ class JSONView(TemplateView):
 
     def dispatch(self, request, *args, **kwargs):
         if kwargs.get('type') in self.targets:
-            return self.targets[kwargs.get('type')]().dispatch(request, args, kwargs)
+            return self.targets[kwargs.get('type')]().dispatch(request)
         raise ValueError(str(kwargs) + ' could not be resolved to any of ' + str(list(self.targets.keys())))
+
+
+
