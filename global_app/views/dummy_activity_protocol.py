d8bcf9a06ce819e60ce73a7e04da0536568d124f
Lene Preuss <lene.preuss@gmail.com>
2015-10-01 14:32:11 +0200

    use deals.json api instead of ORM to find deals to display

--- global_app/views/dummy_activity_protocol.py
+++ global_app/views/dummy_activity_protocol.py
@@ -10,23 +10,26 @@ from global_app.views.sql_generation.record_reader import RecordReader
 
 class DummyActivityProtocol:
 
-    debug = False
+    DEBUG = False
+
     def dispatch(self, request, action):
 
         if request.POST:
             if False and settings.DEBUG: print(request.POST['data'])
-            self.data = json.loads(request.POST["data"])
+            data = json.loads(request.POST["data"])
 
-        res = {"errors": [], "activities": []}
-        res["activities"] = self._get_activities_by_filter_and_grouping(self.data["filters"], self.data["columns"])
+        res = {
+            "errors": [],
+            "activities":  self._get_activities_by_filter_and_grouping(data["filters"], data["columns"])
+        }
 
 #        return HttpResponse(json.dumps(res,encoding="cp1251"), mimetype="application/json")#FIXME, utf-8 breaks for get-the-detail view
-        return HttpResponse(json.dumps(res))
+        return HttpResponse(json.dumps(res), content_type="application/json")
 
     def _get_activities_by_filter_and_grouping(self, filters, columns):
 
         # if filters.get('group_value') == '':
         reader = RecordReader(filters, columns)
-        if self.debug:
+        if self.DEBUG:
             print(reader.get_all_sql())
         return reader.get_all(assemble=reader._make_padded_record_from_column_data)
--- maplandmatrix/models.py
+++ maplandmatrix/models.py
@@ -54,45 +54,6 @@ class MapPluginModel(CMSPlugin):
         self.save()
 
 
-# Create your models here.
-class Deals(models.Model):
-	deals_ID = models.CharField("Deals_ID", max_length=50, unique=True, blank=False, null=True)
-	investor = models.CharField("Investor", max_length = 200)
-	target_country = models.CharField("Country of investment", max_length = 100)
-	deal_size = models.IntegerField("Size in Hectare", blank=True, null=True)
-# Box with several choices here :
-	AGRICULTURE="Agriculture"
-	CONSERVATION="Conservation"
-	FORESTRY="Forestry"
-	INDUSTRY="Industry"
-	MINING="Mining"
-	ENERGY="Energy"
-	TOURISM="Tourism"
-	OTHER="Other"
-	land_use_choices = (
-		(AGRICULTURE, "Agriculture"),
-		(CONSERVATION, "Conservation"),
-		(FORESTRY, "Forestry"),
-		(INDUSTRY, "Industry"),
-		(MINING, "Mining"),
-		(ENERGY, "Energy"),
-		(TOURISM, "Tourism"),
-		(OTHER, "Other")
-	)
-	land_use = models.CharField("Intention of land use", max_length=500, choices=land_use_choices, default=AGRICULTURE)
-	date_created = models.DateTimeField(auto_now = True)
-	#lon = models.FloatField(blank=True, null= True)
-	#lat = models.FloatField(blank=True, null= True)
-	geom = models.PointField(srid=4326)
-	objects = models.GeoManager()
-
-	def __str__(self) :
-		return "%s %s %s %s %s %s" % (self.deals_ID,self.investor, self.target_country, self.land_use, self.geom.x, self.geom.y)
-
-	class Meta :
-		verbose_name = u'Deal'
-		verbose_name_plural = u'Deals'
-
 # This is an auto-generated Django model module created by ogrinspect.
 #class WorldBorder(models.Model):
 	#fips = models.CharField(max_length=2)
