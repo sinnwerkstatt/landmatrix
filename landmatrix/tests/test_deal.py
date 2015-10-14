d8891b1f9ef983cd627e3ba9718ab1916d6e1ad5
Lene Preuss <lene.preuss@gmail.com>
2015-10-14 12:53:11 +0200

    added the possibility to get all activityattributegroups from a deal

--- landmatrix/tests/test_deal.py
+++ landmatrix/tests/test_deal.py
@@ -1,3 +1,8 @@
+from datetime import datetime
+from landmatrix.models.activity_attribute_group import ActivityAttributeGroup
+from landmatrix.models.language import Language
+from landmatrix.tests.with_status import WithStatus
+
 __author__ = 'Lene Preuss <lp@sinnwerkstatt.com>'
 
 from landmatrix.models import Deal, Activity
@@ -7,9 +12,14 @@ from django.utils import timezone
 from django.db.models import Max
 
 
-class TestDeal(TestCase):
+class TestDeal(WithStatus):
 
     NUM_ACTIVITIES = 10
+
+    def setUp(self):
+        WithStatus.setUp(self)
+        Language(english_name='English', local_name='English', locale='en').save()
+        self.language = Language.objects.last()
 
     def test_gets_created_witout_activities(self):
         deal = Deal(1)
@@ -47,6 +57,41 @@ class TestDeal(TestCase):
         with self.assertRaises(IndexError):
             deals[self.NUM_ACTIVITIES]
 
+    def test_single_set_of_attributes(self):
+        self._create_activity(1)
+        self._create_attributes(1, {'some_attribute': 'some_value'})
+        deal = Deal(1)
+        self.assertDictEqual(deal.attributes, {'some_attribute': 'some_value'})
+
+    def test_multiple_attributes(self):
+        self._create_activity(1)
+        self._create_attributes(1, {'some_attribute': 'some_value'})
+        self._create_attributes(1, {'some_other_attribute': 'some_other_value'})
+        deal = Deal(1)
+
+        self.assertDictEqual(deal.attributes, {'some_attribute': 'some_value', 'some_other_attribute': 'some_other_value'})
+
+    def test_attribute_groups(self):
+        self._create_activity(1)
+        self._create_attributes(1, {'some_attribute': 'some_value'})
+        self._create_attributes(1, {'some_other_attribute': 'some_other_value'})
+        deal = Deal(1)
+        self.assertEqual(2, len(deal.attribute_groups()))
+        for group in deal.attribute_groups():
+            # Whoever thought it a good idea to deprecate assertDictIsSubset() deserves to step on Legos!
+            self.assertTrue(
+                set(group.attributes.items()).issubset(set({'some_attribute': 'some_value', 'some_other_attribute': 'some_other_value'}.items()))
+            )
+
+    def test_same_attribute_multiple_values(self):
+        self._create_activity(1)
+        self._create_attributes(1, {'the_same_attribute': 'some_value'})
+        self._create_attributes(1, {'the_same_attribute': 'some_other_value'})
+        deal = Deal(1)
+        self.assertEqual(2, len(deal.attribute_groups()))
+        values = map(lambda g: g.attributes['the_same_attribute'], deal.attribute_groups())
+        self.assertEqual(['some_other_value', 'some_value'], sorted(values))
+
     def _check_is_deal_list(self, deals, length):
         self.assertEqual(length, len(list(deals)))
         self._check_all_are_deals(deals)
@@ -60,3 +105,7 @@ class TestDeal(TestCase):
             activity_identifier=act_id, version=1, availability=0.5, fully_updated=timezone.now(),
             fk_status_id=2
         ).save()
+
+    def _create_attributes(self, act_id, attributes):
+        act = Activity.objects.filter(activity_identifier=act_id).first()
+        ActivityAttributeGroup(fk_activity=act, attributes=attributes, fk_language=self.language, date=datetime.now()).save()
