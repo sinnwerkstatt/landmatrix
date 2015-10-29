e03699d93ff937c583c062ac1731660179543ae3
Lene Preuss <lene.preuss@gmail.com>
2015-10-29 15:25:42 +0100

    conversion of tags into activityattributegroups seems to deliver consistent, if sometimes redundant, data now.

--- from_v1/map_tag_groups.py
+++ from_v1/map_tag_groups.py
@@ -1,5 +1,6 @@
 from pprint import pprint
 from django.db import models, transaction
+from django.utils.datastructures import MultiValueDict
 
 __author__ = 'Lene Preuss <lp@sinnwerkstatt.com>'
 
@@ -37,54 +38,53 @@ class MapTagGroups(MapModel):
 
     @classmethod
     def migrate_tag_group_set(cls, tag_groups):
-
-        print(tag_groups)
-
         for i, tag_group in enumerate(tag_groups):
             cls.migrate_tag_group(i, tag_group)
 
     @classmethod
     def migrate_tag_group(cls, i, tag_group):
-        attrs = {}
-        aag = None
         comments = get_comments(tag_group)
 
         relevant_tag_sets = [
+            [tag_group.fk_a_tag],
             A_Tag.objects.using(V1).filter(fk_a_tag_group=tag_group).select_related('fk_a_key', 'fk_a_value'),
-            [tag_group.fk_a_tag]
         ]
         for relevant_tags in relevant_tag_sets:
+
+            attrs = {}
+
             for tag in relevant_tags:
                 key = tag.fk_a_key.key
                 value = tag.fk_a_value.value
                 year = tag.fk_a_value.year
-                if not year in attrs:
-                    attrs[year] = {}
-                attrs[year][key] = value
-                print(i, key, value, year)
-        if attrs:
-            for year, data in attrs.items():
-                aag = ActivityAttributeGroup(
-                    fk_activity_id=tag_group.fk_activity.id, fk_language=cls.language,
-                    date=year_to_date(year), attributes=data
-                )
-        if aag:
+                print(i, key, value)
+                if key in attrs:
+                    cls.write_activity_attribute_group(attrs, comments, tag_group, year)
+                attrs[key] = value
 
-            if comments:
-                aag.attributes.update({
-                    tag_group.fk_a_tag.fk_a_value.value + '_comment': '\n'.join(comments)
-                })
-#                print(tag_group.fk_a_tag.fk_a_value.value + '_comment:', ' '.join(comments))
-            print(aag.attributes)
-
-            if cls.save:
-                aag.save(using=V2)
+            if attrs:
+                cls.write_activity_attribute_group(attrs, comments, tag_group, year)
 
         # cls._print_status(
         #     {
         #         key: value for key, value in tag_group.__dict__.items()
         #         if not callable(value) and not key.startswith('__')
         #     }, i)
+
+    @classmethod
+    def write_activity_attribute_group(cls, attrs, comments, tag_group, year):
+        aag = ActivityAttributeGroup(
+            fk_activity_id=tag_group.fk_activity.id, fk_language=cls.language,
+            date=year_to_date(year), attributes=attrs, name=attrs.get('name')
+        )
+        if comments:
+            aag.attributes.update({
+                tag_group.fk_a_tag.fk_a_value.value + '_comment': '\n'.join(comments)
+            })
+            print(aag.attributes)
+        if cls.save:
+            aag.save(using=V2)
+
 
 
 def get_comments(tag_group):
