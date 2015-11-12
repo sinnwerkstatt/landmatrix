3b5fa664e892eee8dd77ffbe8fa824d6e7244ebd
Lene Preuss <lene.preuss@gmail.com>
2015-11-12 17:20:02 +0100

    refactorings

--- landmatrix/managers/stakeholder_manager.py
+++ landmatrix/managers/stakeholder_manager.py
@@ -4,25 +4,11 @@ from django.db.models import Manager
 __author__ = 'Lene Preuss <lp@sinnwerkstatt.com>'
 
 
-@transaction.commit_manually
-def flush_transaction():
-    """
-    Flush the current transaction so we don't read stale data
-
-    Use in long running processes to make sure fresh data is read from
-    the database.  This is a problem with MySQL and the default
-    transaction mode.  You can fix it by setting
-    "transaction-isolation = READ-COMMITTED" in my.cnf or by calling
-    this function at the appropriate moment
-    """
-    transaction.commit()
-
-
 class StakeholderManager(Manager):
 
     def raw_choices(self):
-        flush_transaction()
-        return self.raw("""
+        with transaction.atomic():
+            return self.raw("""
 SELECT
   a.id,
   a.country,
