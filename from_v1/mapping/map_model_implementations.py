e97ab5e5448e56e9fddf561a96541180ed42c7cc
Lene Preuss <lene.preuss@gmail.com>
2015-11-26 16:52:29 +0100

    separated yet a few more map classes out of the monolithic file
    Former-commit-id: 9a6532e5cfb34d9af8ae18a0c78f38f4fd0b304a

--- from_v1/mapping/map_model_implementations.py
+++ from_v1/mapping/map_model_implementations.py
@@ -1,9 +1,6 @@
 
 __author__ = 'Lene Preuss <lp@sinnwerkstatt.com>'
 
-from mapping.map_model import MapModel
-import landmatrix.models
-import editor.models
 
 
 def year_to_date(year):
@@ -80,109 +77,13 @@ def is_number(s):
     except ValueError:
         return False
 
+from mapping.map_model import MapModel
+import landmatrix.models
+import editor.models
 from mapping.map_stakeholder import MapStakeholder
 from mapping.map_activity import MapActivity
 from mapping.map_primary_investor import MapPrimaryInvestor
 from migrate import V1
-
-
-class MapInvolvement(MapModel):
-    old_class = editor.models.Involvement
-    new_class = landmatrix.models.Involvement
-    depends = [ MapActivity, MapStakeholder, MapPrimaryInvestor ]
-
-    @classmethod
-    def all_records(cls):
-        activity_ids = MapActivity.all_ids()
-        primary_investor_ids = MapPrimaryInvestor.all_ids()
-        stakeholder_ids = MapStakeholder.all_ids()
-        records = cls.old_class.objects.using(V1).\
-            filter(fk_activity__in=activity_ids).\
-            filter(fk_primary_investor__in=primary_investor_ids).\
-            filter(fk_stakeholder__in=stakeholder_ids).values()
-        cls._count = len(records)
-        return records
-
-
-class MapRegion(MapModel):
-    old_class = editor.models.Region
-    new_class = landmatrix.models.Region
-
-
-class MapCountry(MapModel):
-    old_class = editor.models.Country
-    new_class = landmatrix.models.Country
-    attributes = {
-        'region': 'fk_region',
-    }
-    depends = [ MapRegion ]
-
-
-class MapBrowseRule(MapModel):
-    old_class = editor.models.BrowseRule
-    new_class = landmatrix.models.BrowseRule
-
-
-class MapBrowseCondition(MapModel):
-    old_class = editor.models.BrowseCondition
-    new_class = landmatrix.models.BrowseCondition
-    depends = [ MapBrowseRule ]
-
-
-class MapAgriculturalProduce(MapModel):
-    old_class = editor.models.AgriculturalProduce
-    new_class = landmatrix.models.AgriculturalProduce
-
-
-class MapCrop(MapModel):
-    old_class = editor.models.Crop
-    new_class = landmatrix.models.Crop
-    attributes = {
-        'agricultural_produce': 'fk_agricultural_produce',
-    }
-    depends = [ MapAgriculturalProduce ]
-
-
-class MapComment(MapModel):
-    old_class = editor.models.Comment
-    new_class = landmatrix.models.Comment
-    attributes = {
-        'activity_attribute_group': 'fk_activity_attribute_group',
-        'stakeholder_attribute_group': 'fk_stakeholder_attribute_group'
-    }
-
-
-def get_country_for_primary_investor(pi_id):
-
-    activity_ids = editor.models.Involvement.objects.using(V1).filter(fk_primary_investor=pi_id).values_list('fk_activity', flat=True)
-    if not activity_ids or None in activity_ids:
-        return None
-
-    max_activity = max(activity_ids)
-    stakeholder_ids = list(editor.models.Involvement.objects.using(V1).filter(fk_activity=max_activity).order_by('-investment_ratio').values_list('fk_stakeholder', flat=True))
-    while stakeholder_ids:
-        stakeholder_with_greatest_investment_ratio = stakeholder_ids.pop(0)
-
-        country = get_country_id_for_stakeholder(stakeholder_with_greatest_investment_ratio)
-
-        if country:
-            return landmatrix.models.Country(country)
-
-    return None
-
-
-class MapInvestor(MapPrimaryInvestor):
-    # old_class = editor.models.PrimaryInvestor
-    new_class = landmatrix.models.Investor
-    attributes = {
-        'primary_investor_identifier': 'investor_identifier',
-        'id': ('id', ('fk_country', get_country_for_primary_investor))
-    }
-
-
-def get_name_for_stakeholder(stakeholder_id):
-    investor_name = get_first_stakeholder_tag_value(stakeholder_id, 'investor_name')
-    return '' if investor_name is None else investor_name
 
 
 def get_stakeholder_tag_groups(stakeholder_id):
@@ -211,35 +112,6 @@ def get_country_id_for_stakeholder(stakeholder_id):
     if country:
         return country.pk
     return None
-
-
-def get_classification_for_stakeholder(stakeholder_id):
-    classification = get_first_stakeholder_tag_value(stakeholder_id, 'classification')
-    return {
-        'Private company': '10',
-        'Stock-exchange listed company': '20',
-        'Individual entrepreneur': '30',
-        'Investment fund': '40',
-        'Semi state-owned company': '50',
-        'State-/government(owned)': '60',
-        'State-/government(-owned)': '60',
-        'Other (please specify in comment field)': '70'
-    }.get(classification, None)
-
-
-class MapStakeholderInvestor(MapModel):
-    old_class = editor.models.Stakeholder
-    new_class = landmatrix.models.Investor
-    attributes = {
-        'stakeholder_identifier': 'investor_identifier',
-        'id': (
-            'id',
-            ('name', get_name_for_stakeholder),
-            ('country', get_country_id_for_stakeholder),
-            ('classification', get_classification_for_stakeholder)
-        )
-    }
-    DEBUG = False
 
 
 def get_status_for_investor(primary_investor_id):
---
+++ from_v1/mapping/map_stakeholder_investor.py
@@ -0,0 +1,41 @@
+from mapping.map_model import MapModel
+import landmatrix.models
+import editor.models
+from mapping.map_model_implementations import get_first_stakeholder_tag_value, get_country_id_for_stakeholder
+
+__author__ = 'Lene Preuss <lp@sinnwerkstatt.com>'
+
+
+def get_name_for_stakeholder(stakeholder_id):
+    investor_name = get_first_stakeholder_tag_value(stakeholder_id, 'investor_name')
+    return '' if investor_name is None else investor_name
+
+
+
+def get_classification_for_stakeholder(stakeholder_id):
+    classification = get_first_stakeholder_tag_value(stakeholder_id, 'classification')
+    return {
+        'Private company': '10',
+        'Stock-exchange listed company': '20',
+        'Individual entrepreneur': '30',
+        'Investment fund': '40',
+        'Semi state-owned company': '50',
+        'State-/government(owned)': '60',
+        'State-/government(-owned)': '60',
+        'Other (please specify in comment field)': '70'
+    }.get(classification, None)
+
+
+class MapStakeholderInvestor(MapModel):
+    old_class = editor.models.Stakeholder
+    new_class = landmatrix.models.Investor
+    attributes = {
+        'stakeholder_identifier': 'investor_identifier',
+        'id': (
+            'id',
+            ('name', get_name_for_stakeholder),
+            ('country', get_country_id_for_stakeholder),
+            ('classification', get_classification_for_stakeholder)
+        )
+    }
+    DEBUG = False
