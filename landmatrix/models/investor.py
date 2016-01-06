b373a5de9156939dd9ded17cbda8f3eb816364d3
Lene Preuss <lene.preuss@gmail.com>
2016-01-06 18:47:34 +0100

    replaced investor formset when adding a deal with an operational stakeholder form (which as of now displays an unhelpful representation of the OPs, but that will be fixed soon)

--- landmatrix/models/investor.py
+++ landmatrix/models/investor.py
@@ -12,10 +12,7 @@ __author__ = 'Lene Preuss <lp@sinnwerkstatt.com>'
 
 class Investor(DefaultStringRepresentation, models.Model):
 
-    investor_identifier = models.IntegerField(_("Investor id"), db_index=True)
-    name = models.CharField(_("Name"), max_length=1024)
-    fk_country = models.ForeignKey("Country", verbose_name=_("Country"), blank=True, null=True)
-    classification = models.CharField(max_length=2, choices=(
+    classification_choices = (
         ('10', _("Private company")),
         ('20', _("Stock-exchange listed company")),
         ('30', _("Individual entrepreneur")),
@@ -23,7 +20,11 @@ class Investor(DefaultStringRepresentation, models.Model):
         ('50', _("Semi state-owned company")),
         ('60', _("State-/government(owned)")),
         ('70', _("Other (please specify in comment field)"))
-    ), blank=True, null=True)
+    )
+    investor_identifier = models.IntegerField(_("Investor id"), db_index=True)
+    name = models.CharField(_("Name"), max_length=1024)
+    fk_country = models.ForeignKey("Country", verbose_name=_("Country"), blank=True, null=True)
+    classification = models.CharField(max_length=2, choices=classification_choices, blank=True, null=True)
     comment = models.TextField(_("Comment"), blank=True, null=True)
     fk_status = models.ForeignKey("Status", verbose_name=_("Status"))
     timestamp = models.DateTimeField(_("Timestamp"), auto_now_add=True)
@@ -45,6 +46,7 @@ class InvestorVentureInvolvement(models.Model):
     def __str__(self):
         return 'venture: %i stakeholder: %i percentage: %5.2f role: %s timestamp: %s' % \
                (self.fk_venture_id, self.fk_investor_id, self.percentage, self.role, self.timestamp)
+
 
 class InvestorActivityInvolvement(models.Model):
     fk_activity = models.ForeignKey("Activity", verbose_name=_("Activity"), db_index=True)
