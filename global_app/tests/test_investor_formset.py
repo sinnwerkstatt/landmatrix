a24afc44254a54a792f84d3c85eff09f4b1c0999
Lene Preuss <lene.preuss@gmail.com>
2016-01-07 14:36:46 +0100

    form for operational stakeholder

--- global_app/tests/test_investor_formset.py
+++ global_app/tests/test_investor_formset.py
@@ -2,6 +2,8 @@ from django.core.exceptions import ValidationError
 from django.test import TestCase
 
 from global_app.forms.investor_formset import InvestorFormSet
+from global_app.forms.operational_stakeholder_form import OperationalStakeholderForm
+from landmatrix.models.investor import Investor
 
 __author__ = 'Lene Preuss <lp@sinnwerkstatt.com>'
 
@@ -9,8 +11,7 @@ __author__ = 'Lene Preuss <lp@sinnwerkstatt.com>'
 class TestInvestorFormset(TestCase):
 
     def test_investor_formset_instantiates(self):
-        formset = InvestorFormSet()
-        self.assertFalse(formset.is_valid())
+        self._test_form_instantiates(InvestorFormSet)
 
     def test_investor_formset_management_form_invalid(self):
         formset = InvestorFormSet({'form-0-investor': 'nonsense'})
@@ -40,3 +41,27 @@ class TestInvestorFormset(TestCase):
             form_data.update(data)
         formset = formset_class(form_data)
         return formset
+
+    def _test_form_instantiates(self, form_class):
+        form = form_class()
+        self.assertFalse(form.is_valid())
+
+    def test_operational_stakeholder_initializes(self):
+        self._test_form_instantiates(OperationalStakeholderForm)
+
+    def test_operational_stakeholder_data(self):
+        Investor(investor_identifier=1, name='invTESTor', classification=10, version=1, fk_status_id=2).save()
+        id = Investor.objects.first().pk
+        form = OperationalStakeholderForm({'operational_stakeholder': id})
+        self.assertTrue(form.is_valid())
+
+    def test_operational_stakeholder_data_present(self):
+        Investor(investor_identifier=1, name='invTESTor', classification=10, version=1, fk_status_id=2).save()
+        id = Investor.objects.first().pk
+        form = OperationalStakeholderForm({'operational_stakeholder': id})
+        self.assertTrue(form.is_valid())
+        self.assertIn('operational_stakeholder', form.cleaned_data)
+
+    def test_operational_stakeholder_data_bad(self):
+        form = OperationalStakeholderForm({'operational_stakeholder': 1})
+        self.assertFalse(form.is_valid())
