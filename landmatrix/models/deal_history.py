797dd7305e5b4f4087ce8ab7f8935e81243d583a
Lene Preuss <lene.preuss@gmail.com>
2016-01-18 17:20:05 +0100

    implemented versioned activity attributes in deals

--- landmatrix/models/deal_history.py
+++ landmatrix/models/deal_history.py
@@ -1,5 +1,5 @@
 from landmatrix.models.activity_attribute_group import ActivityAttributeGroup
-from landmatrix.models.deal import Deal
+from landmatrix.models.deal import Deal, aggregate_activity_attributes
 
 from collections import OrderedDict
 
@@ -8,24 +8,30 @@ __author__ = 'Lene Preuss <lp@sinnwerkstatt.com>'
 
 class DealHistoryItem(Deal):
 
+    date = None
+    use_rounded_dates = True
+
     @classmethod
     def from_activity_with_date(cls, activity, date):
+        cls.date = date
         deal = super().from_activity(activity)
-        # ...
+        cls.date = None
         return deal
 
+    def get_activity_attributes(self):
+        attributes = ActivityAttributeGroup.objects.filter(fk_activity=self.activity)
 
-    def get_timestamp(self):
-        return self.activity.history_date
+        if self.date:
+            attributes = [a.history.as_of(DealHistoryItem.date) for a in attributes]
+
+        attributes_list = [a.attributes for a in attributes]
+        return aggregate_activity_attributes(attributes_list, {})
 
     def get_user(self):
         return self.activity.changed_by
 
     def get_history(self):
         return OrderedDict(sorted(self._activity_history(), key=lambda item: item[0]))
-        return OrderedDict(
-                (activity.history_date, DealHistoryItem.from_activity_with_date(activity)) for activity in self._activity_history()
-        )
 
     def _activity_history(self):
         date_and_activity = []
@@ -44,9 +50,11 @@ class DealHistoryItem(Deal):
             item[0]
             for item in ActivityAttributeGroup.history.filter(fk_activity_id=self.activity.id).values_list('history_date')
         ]
-        return attributes_history_dates
-        unique_dates = _unique_rounded_dates(attributes_history_dates)
-        return unique_dates
+
+        if not self.use_rounded_dates:
+            return attributes_history_dates
+
+        return _unique_rounded_dates(attributes_history_dates)
 
 
 def _unique_rounded_dates(dates):
