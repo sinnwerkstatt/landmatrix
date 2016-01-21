3f2fb3bfb496db75f197472861771a1bc70071b7
Lene Preuss <lene.preuss@gmail.com>
2016-01-21 18:24:11 +0100

    added a dummy view for editor dashboard check whether user is logged in added a login page
    Former-commit-id: f26b4ddd4909d9c771e6aebdb2c99efac754cd6e

--- editor/views.py
+++ editor/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
 
 # Create your views here.
+from django.template.context import RequestContext
+
+from django.views.generic import TemplateView
+
+from global_app.views.view_aux_functions import render_to_response
+
+__author__ = 'Lene Preuss <lp@sinnwerkstatt.com>'
+
+
+class EditorView(TemplateView):
+
+    template_name = 'dashboard.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        context = {'user': None}
+        print('user:', request.user)
+        if request.user.is_authenticated():
+            context['user'] = request.user
+
+        return render_to_response(self.template_name, context, RequestContext(request))
