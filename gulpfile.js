f8e848875dfa7cdf7f544e56102d1007f2b87108
riot <riot@hackerfleet.org>
2016-01-26 16:16:27 +0100

    Gulp task to watch over template and css changes
    Former-commit-id: 881fa913262630d9a14a87ea0587605ae72dd8c3

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+var livereload = require('gulp-livereload');
+var watch = require('gulp-watch');
+
+
+gulp.task('watch', function () {
+    livereload.listen();
+
+    gulp.watch('**/templates/**').on('change', livereload.changed);
+    gulp.watch('**/media/css/**').on('change', livereload.changed);
+
+});
+
+gulp.task('default' , ['watch']); 
