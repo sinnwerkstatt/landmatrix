88eac23ecf4dae872e42563c6da8b032d44c4104
Lene Preuss <lene.preuss@gmail.com>
2016-01-26 11:50:26 +0100

    stubs for dashboard functions reactivated tests for deals

--- editor/models.py
+++ editor/models.py
@@ -2,12 +2,12 @@ from landmatrix.models.country import Country
 from landmatrix.models.region import Region
 
 from django.db import models
-from django.contrib.auth.models import User
+from django.conf import settings
 
 
 class UserRegionalInfo(models.Model):
-    user = models.OneToOneField(User, related_name='user', on_delete=models.CASCADE)
-#    super_user = models.OneToOneField(User, related_name='super_user', blank=True, null=True)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    super_user = models.OneToOneField(settings.AUTH_USER_MODEL, related_name='super_user', blank=True, null=True)
     country = models.ManyToManyField(Country)
     region = models.ManyToManyField(Region)
 
