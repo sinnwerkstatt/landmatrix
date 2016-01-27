f8572a26d31c7a341d4f2316fd2006d79c5ca787
Lene Preuss <lene.preuss@gmail.com>
2016-01-27 18:26:25 +0100

    test more than one filter at once (two, actually)

--- api/tests/test_filters.py
+++ api/tests/test_filters.py
@@ -1,5 +1,6 @@
 import json
 
+from django.db.utils import ProgrammingError
 from django.test.testcases import TestCase
 
 from api.query_sets.fake_query_set import FakeQuerySet
@@ -23,9 +24,26 @@ class TestFilters(TestCase):
         FakeQuerySet.DEBUG = False
 
     def test_all_filters_single(self):
+        failed = []
         for filter in FILTER_VAR_ACT+FILTER_VAR_INV:
-            print(filter)
-            self._get_content(self._fake_url_params(filter))
+            try:
+                self._get_content(self._fake_url_params(filter))
+            except ProgrammingError:
+                failed.append(filter)
+        if failed:
+            self.fail("Filtering failed for tag(s)" + ', '.join(failed))
+
+    def test_all_filters_double(self):
+        failed = []
+        for index in range(len(FILTER_VAR_ACT+FILTER_VAR_INV)-1):
+            filter1 = (FILTER_VAR_ACT+FILTER_VAR_INV)[index]
+            filter2 = (FILTER_VAR_ACT+FILTER_VAR_INV)[index+1]
+            try:
+                self._get_content(self._fake_url_params_2_args(filter1, filter2))
+            except ProgrammingError:
+                failed.append((filter1, filter2))
+        if failed:
+            self.fail("Filtering failed for tag(s)" + ', '.join(failed))
 
     def _fake_url_params(self, filter):
         return '/api/agricultural-produce.json?negotiation_status=concluded&deal_scope=transnational'\
@@ -37,6 +55,20 @@ class TestFilters(TestCase):
                '&conditions_empty-0-variable=' + filter + \
                '&conditions_empty-0-operator=is&conditions_empty-0-value=' + str(self.FILTER_VALUES[filter]) + \
                '&conditions_empty-0-hidden_value='
+
+    def _fake_url_params_2_args(self, filter1, filter2):
+        return '/api/agricultural-produce.json?negotiation_status=concluded&deal_scope=transnational'\
+               '&filtered=true&limit=&order_by=deal_id' \
+               '&prefix=conditions_empty' \
+               '&conditions_empty-TOTAL_FORMS=2&conditions_empty-INITIAL_FORMS=2' \
+               '&conditions_empty-MIN_NUM_FORMS=&conditions_empty-MAX_NUM_FORMS=' \
+               '&conditions_empty-0-variable=' + filter1 + \
+               '&conditions_empty-0-operator=is&conditions_empty-0-value=' + str(self.FILTER_VALUES[filter1]) + \
+               '&hidden_conditions_empty-0-value=' + str(self.FILTER_VALUES[filter1]) + \
+               '&variable=&operator=&value=&hidden_value=' \
+               '&conditions_empty-1-variable=' + filter2 + \
+               '&conditions_empty-1-operator=is&conditions_empty-1-value=' + str(self.FILTER_VALUES[filter2]) + \
+               '&conditions_empty-1-hidden_value='
 
     def _get_content(self, resource):
         url, response = self._get_url_following_redirects(resource)
