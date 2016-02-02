6efa3b79a04e645f4557e7c2d7bd03db1e47e2cc
Lene Preuss <lene.preuss@gmail.com>
2016-02-02 17:49:33 +0100

    (incomplete) saving of deals when changing them
    Former-commit-id: 2341b80b12e5d4db2bb399bc9e27cdd722e85da0

--- global_app/views/change_deal.py
+++ global_app/views/change_deal.py
@@ -1,1 +1,21 @@
+from landmatrix.models.activity import Activity
+from landmatrix.models.deal import Deal
+from .save_deal_view import SaveDealView, FORMS
+
 __author__ = 'Lene Preuss <lp@sinnwerkstatt.com>'
+
+
+class ChangeDealView(SaveDealView):
+
+    template_name = 'change-deal.html'
+
+    def get_activity(self, **kwargs):
+        return Activity.objects.get(activity_identifier=kwargs.get('deal_id'))
+
+    def get_forms(self, data=None):
+        return [get_form(Deal(self.activity.activity_identifier), form) for form in FORMS]
+
+
+def get_form(deal, form_class):
+    data = form_class[1].get_data(deal)
+    return form_class[1](initial=data)
