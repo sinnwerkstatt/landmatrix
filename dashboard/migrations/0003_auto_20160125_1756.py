a08444672721ff6727363f813fa9a2087bcac9be
Lene Preuss <lene.preuss@gmail.com>
2016-02-09 18:03:41 +0100

    renamed app editor to dashboard to avoid name clash with V1 app during data migration
    Former-commit-id: 3f0bc336a9635b622ece61abe45f6a8c36408e40

--- dashboard/migrations/0003_auto_20160125_1756.py
+++ dashboard/migrations/0003_auto_20160125_1756.py
@@ -9,7 +9,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('editor', '0002_auto_20160125_1703'),
+        ('dashboard', '0002_auto_20160125_1703'),
     ]
 
     operations = [
