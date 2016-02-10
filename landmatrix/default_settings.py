18485166aeb9bbfd405950bcea362161ef2fc48d
Lene Preuss <lene.preuss@gmail.com>
2016-02-10 14:15:59 +0100

    actually show changesets in dashboard

--- landmatrix/default_settings.py
+++ landmatrix/default_settings.py
@@ -15,7 +15,7 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-FRONTENDDEV = True
+FRONTENDDEV = False
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
