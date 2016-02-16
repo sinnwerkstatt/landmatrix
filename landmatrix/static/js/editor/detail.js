d8bc23ae3028c869409969ed806731ed4310803e
riot <riot@hackerfleet.org>
2016-02-16 12:47:09 +0100

    Expand/Collapse button out

--- landmatrix/static/js/editor/detail.js
+++ landmatrix/static/js/editor/detail.js
@@ -1,30 +1,3 @@
-var collapseState = 'hide';
-
 $(document).ready(function () {
-
-
-    $('.expandcollapse').click(function () {
-
-        if (collapseState == 'hide') {
-            collapseState = 'show';
-        } else {
-            collapseState = 'hide';
-        }
-
-        console.log("Expanding/collapsing: ", collapseState);
-        $('.collapse').each(function(index) {
-            $(this).collapse(collapseState);
-        });
-
-
-        if ($(this).html() == "<i class=\"icon-white icon-plus-sign\"></i> Expand All") {
-            $(this).html("<i class=\"icon-white icon-minus-sign\"></i> Collapse All");
-        }
-        else {
-            $(this).html("<i class=\"icon-white icon-plus-sign\"></i> Expand All");
-        };
-    });
-
-    console.log("Detail.js loaded");
 
 });
