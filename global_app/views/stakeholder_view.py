2e1463411bdf35b953e308b7173da7a9ac9c15ff
Lene Preuss <lene.preuss@gmail.com>
2016-02-17 13:44:29 +0100

    added investor form and parent investor and stakeholder formsets to stakeholder view
    Former-commit-id: 66dd417f0efd971cb5c8dfae52300d6a01d3051b

--- global_app/views/stakeholder_view.py
+++ global_app/views/stakeholder_view.py
@@ -1,6 +1,19 @@
+from django.template.context import RequestContext
 from django.views.generic.base import TemplateView
+
+from global_app.forms.investor_formset import InvestorForm, InvestorFormSet, ParentStakeholderFormSet
+from global_app.views.view_aux_functions import render_to_response
 
 
 class StakeholderView(TemplateView):
 
     template_name = 'stakeholder.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['investor_form'] = InvestorForm()
+        context['parent_stakeholders'] = ParentStakeholderFormSet()
+        context['parent_investors'] = ParentStakeholderFormSet()
+
+        return render_to_response(self.template_name, context, RequestContext(request))
+
