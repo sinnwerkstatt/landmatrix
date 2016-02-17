ef9648694448aa79f326758b5cb43d9c3bf275f2
Lene Preuss <lene.preuss@gmail.com>
2016-02-17 15:23:30 +0100

    split form classes up into separate source files

--- global_app/forms/investor_formset.py
+++ global_app/forms/investor_formset.py
@@ -1,20 +1,7 @@
-from pprint import pprint
-
-from django.core.exceptions import ValidationError
-
-from global_app.forms.base_form import BaseForm
-from global_app.forms.operational_stakeholder_form import _investor_description
+from global_app.forms.investor_form import InvestorForm, InvestorField
 from landmatrix.models.comment import Comment
 
-from landmatrix.models.investor import Investor, InvestorActivityInvolvement
-
-from global_app.widgets import CommentInput, TitleField, LivesearchSelect
-
 from landmatrix.models.country import Country
-from landmatrix.models.region import Region
-
-from django import forms
-from django.utils.translation import ugettext_lazy as _
 
 from django.forms.formsets import formset_factory
 
@@ -22,100 +9,6 @@ from copy import copy
 
 __author__ = 'Lene Preuss <lp@sinnwerkstatt.com>'
 
-
-class InvestorChoicesMixin:
-
-    def __init__(self, *args, **kwargs):
-        investor = kwargs.pop("investor", None)
-        self.fields["investor"].initial = investor
-        self._fill_investor_choices()
-
-    def _fill_investor_choices(self):
-        self.investor_choices = [
-            (investor.id, _investor_description(investor))
-            for investor in Investor.objects.filter(fk_status_id__in=(2, 3)).order_by('name')
-        ]
-        self.fields["investor"].choices = list(self.fields["investor"].choices)[:1]
-        self.fields["investor"].choices.extend(self.investor_choices)
-
-
-# Change this to a livesearch widget once you got a working one
-InvestorField = forms.ChoiceField
-
-
-class InvestorForm(BaseForm, InvestorChoicesMixin):
-
-    # Investor
-    tg_investor = TitleField(required=False, label="", initial=_("Investor"))
-    investor = InvestorField(required=False, label=_("Existing investor"), choices=())#, widget=LivesearchSelect)
-    investor_name = forms.CharField(required=False, label=_("Name"), max_length=255)
-    country = forms.ChoiceField(required=False, label=_("Country"), choices=())
-    region = forms.ModelChoiceField(required=False, label=_("Region"), widget=forms.HiddenInput, queryset=Region.objects.all().order_by('name'))
-    classification = forms.ChoiceField(
-            required=False, label=_("Classification"), choices=Investor.classification_choices, widget=forms.RadioSelect
-    )
-    investment_ratio = forms.DecimalField(max_digits=19, decimal_places=2, required=False, label=_("Percentage of investment"), help_text=_("%"))
-    tg_general_comment = forms.CharField(required=False, label=_("Additional comments"), widget=CommentInput)
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        investor = kwargs.pop("investor", None)
-        self.fields["investor"].initial = investor
-        self._fill_investor_choices()
-        self._fill_country_choices()
-
-    def clean_investor(self):
-        investor = int(self.cleaned_data["investor"] or 0)
-        if investor and (investor not in [s[0] for s in self.investor_choices]):
-            raise forms.ValidationError("%s is no valid investor." % investor)
-        return investor
-
-    def clean(self):
-        cleaned_data = super().clean()
-        investor = cleaned_data.get("investor", None)
-        investor_name = cleaned_data.get("investor_name", None)
-        if not investor and not investor_name:
-            raise forms.ValidationError("Please select an investor or investor name.")
-
-        return cleaned_data
-
-    def has_investor(self):
-        if self.initial.get("investor"):
-            return True
-        elif self.is_valid() and self.cleaned_data.get("investor"):
-            return True
-        return False
-
-    def _fill_country_choices(self):
-        self.fields["country"].choices = [
-            ("", str(_("---------"))),
-            (0, str(_("Multinational enterprise (MNE)")))
-        ]
-        self.fields["country"].choices.extend([(c.id, c.name) for c in Country.objects.all().order_by("name")])
-
-
-class ParentStakeholderForm(BaseForm, InvestorChoicesMixin):
-
-    stakeholder = InvestorField(required=False, label=_("Existing investor"), choices=())
-    percentage = forms.DecimalField(
-        max_digits=5, decimal_places=2, required=False, label=_("Percentage of investment"), help_text=_("%")
-    )
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        investor = kwargs.pop("stakeholder", None)
-        self.fields["stakeholder"].initial = investor
-        self._fill_investor_choices()
-
-    def _fill_investor_choices(self):
-        self.investor_choices = [
-            (investor.id, _investor_description(investor))
-            for investor in Investor.objects.filter(fk_status_id__in=(2, 3)).order_by('name')
-        ]
-        self.fields["stakeholder"].choices = list(self.fields["stakeholder"].choices)[:1]
-        self.fields["stakeholder"].choices.extend(self.investor_choices)
-
-ParentStakeholderFormSet = formset_factory(ParentStakeholderForm, extra=1)
 
 BaseInvestorFormSet = formset_factory(InvestorForm, extra=1)
 
--- global_app/views/stakeholder_view.py
+++ global_app/views/stakeholder_view.py
@@ -1,7 +1,8 @@
 from django.template.context import RequestContext
 from django.views.generic.base import TemplateView
 
-from global_app.forms.investor_formset import InvestorForm, InvestorFormSet, ParentStakeholderFormSet
+from global_app.forms.investor_formset import InvestorForm
+from global_app.forms.parent_stakeholder_formset import ParentStakeholderFormSet
 from global_app.views.view_aux_functions import render_to_response
 
 
@@ -11,9 +12,14 @@ class StakeholderView(TemplateView):
 
     def dispatch(self, request, *args, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['investor_form'] = InvestorForm()
+        context['investor_form'] = InvestorForm(InvestorForm.get_data(request.POST))
+        # context['parent_stakeholders'] = ParentStakeholderFormSet(ParentStakeholderFormSet.get_data(request.POST))
         context['parent_stakeholders'] = ParentStakeholderFormSet()
+        # context['parent_investors'] = ParentStakeholderFormSet(ParentStakeholderFormSet.get_data(request.POST))
         context['parent_investors'] = ParentStakeholderFormSet()
 
         return render_to_response(self.template_name, context, RequestContext(request))
 
+def get_form(deal, form_class):
+    data = form_class[1].get_data(deal)
+    return form_class[1](initial=data)
