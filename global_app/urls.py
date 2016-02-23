fe6db5b6013ec78c2586dbdfb0cfb58e99ad0783
Lene Preuss <lene.preuss@gmail.com>
2016-02-23 17:28:04 +0100

    cache view output for 1 hour
    Former-commit-id: 5892b51ce8555128b579d93068d69fc72cca62ee

--- global_app/urls.py
+++ global_app/urls.py
@@ -7,6 +7,7 @@ from global_app.views.add_deal_view import AddDealView
 from global_app.views.stakeholder_view import StakeholderView
 
 from django.conf.urls import url, patterns
+from django.views.decorators.cache import cache_page
 
 __author__ = 'Lene Preuss <lp@sinnwerkstatt.com>'
 
@@ -23,18 +24,32 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
 
+CACHE_TIMEOUT = 3600
+
 urlpatterns = patterns('globalapp.views',
-    url(r'^$', AllDealsView.as_view(), name='app_main'),
-    url(r'^all(?P<type>\.csv)?/$', AllDealsView.as_view(), name='all_deal'),
-    url(r'^all(?P<type>\.xml)?/$', AllDealsView.as_view(), name='all_deal'),
-    url(r'^all(?P<type>\.xls)?/$', AllDealsView.as_view(), name='all_deal'),
-    url(r'^compare/(?P<activity_1_id>[\d]+)/(?P<activity_2_id>[\d]+)/$', DealComparisonView.as_view(), name='compare_deals'),
-    url(r'^compare/(?P<activity_1>[\d_\.]+)/$', DealComparisonView.as_view(), name='compare_deals'),
-    url(r'^compare/(?P<activity_1>.+)/$', DealComparisonView.as_view(), name='compare_deals'),
+    url(r'^$', cache_page(CACHE_TIMEOUT)(AllDealsView.as_view()), name='app_main'),
+    url(r'^all(?P<type>\.csv)?/$', cache_page(CACHE_TIMEOUT)(AllDealsView.as_view()), name='all_deal'),
+    url(r'^all(?P<type>\.xml)?/$', cache_page(CACHE_TIMEOUT)(AllDealsView.as_view()), name='all_deal'),
+    url(r'^all(?P<type>\.xls)?/$', cache_page(CACHE_TIMEOUT)(AllDealsView.as_view()), name='all_deal'),
+    url(
+        r'^compare/(?P<activity_1_id>[\d]+)/(?P<activity_2_id>[\d]+)/$',
+        cache_page(CACHE_TIMEOUT)(DealComparisonView.as_view()),
+        name='compare_deals'
+    ),
+    url(
+        r'^compare/(?P<activity_1>[\d_\.]+)/$',
+        cache_page(CACHE_TIMEOUT)(DealComparisonView.as_view()),
+        name='compare_deals'
+    ),
+    url(
+        r'^compare/(?P<activity_1>.+)/$',
+        cache_page(CACHE_TIMEOUT)(DealComparisonView.as_view()),
+        name='compare_deals'
+    ),
     url(r'^add/$', AddDealView.as_view(), name='add_deal'),
     url(r'^stakeholder/$', StakeholderView.as_view(), name='stakeholder_form'),
     url(r'^change/(?P<deal_id>[\d]+)/$', ChangeDealView.as_view(), name='change_deal'),
-    url(r'^(?P<group>.+)/(?P<list>.+)/$', TableGroupView.as_view(), name='table_list'),
-    url(r'^(?P<deal_id>[\d]+)/$', DealDetailView.as_view(), name='deal_detail'),
-    url(r'^(?P<deal_id>[\d_\.]+)/$', DealDetailView.as_view(), name='deal_detail'),
+    url(r'^(?P<group>.+)/(?P<list>.+)/$', cache_page(CACHE_TIMEOUT)(TableGroupView.as_view()), name='table_list'),
+    url(r'^(?P<deal_id>[\d]+)/$', cache_page(CACHE_TIMEOUT)(DealDetailView.as_view()), name='deal_detail'),
+    url(r'^(?P<deal_id>[\d_\.]+)/$', cache_page(CACHE_TIMEOUT)(DealDetailView.as_view()), name='deal_detail'),
 )
