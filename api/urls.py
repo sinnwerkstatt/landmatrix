d249752bc0016b8ded5c0c374ad1957de5d6f346
Lene Preuss <lene.preuss@gmail.com>
2016-02-25 12:55:37 +0100

    cache output of api as well

--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,15 @@
 from api.views.json_view import JSONView
 
 from django.conf.urls import include, url, patterns
+from django.views.decorators.cache import cache_page
 
 __author__ = 'Lene Preuss <lp@sinnwerkstatt.com>'
+
+CACHE_TIMEOUT = 24*3600
 
 urlpatterns = patterns('')
 
 urlpatterns.append(
-    url(r'^(?P<type>.*\.json)', JSONView.as_view(), name='landmatrix_api')
+    url(r'^(?P<type>.*\.json)', cache_page(CACHE_TIMEOUT)(JSONView.as_view()), name='landmatrix_api')
 )
 
