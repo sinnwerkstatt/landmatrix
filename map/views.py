e0fe568866cb7f44cf475720c490b5bc0e5ce848
Lene Preuss <lene.preuss@gmail.com>
2016-02-25 12:30:36 +0100

    pass request to template so the current state can be read from template
    Former-commit-id: 57b5007bd846809720ee88ac18c4d8639a1bd0a7

--- map/views.py
+++ map/views.py
@@ -1,7 +1,16 @@
-__author__ = 'Lene Preuss <lp@sinnwerkstatt.com>'
+from grid.views.view_aux_functions import render_to_response
 
 from django.views.generic import TemplateView
 from django.template import RequestContext
 
+__author__ = 'Lene Preuss <lp@sinnwerkstatt.com>'
+
+
 class MapView(TemplateView):
     template_name = 'map/map.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        context['request'] = request
+        return render_to_response(self.template_name, context, RequestContext(request))
+
