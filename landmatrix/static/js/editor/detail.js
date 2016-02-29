4a327b5db885a18187373b13a590b3aa2469d151
riot <riot@hackerfleet.org>
2016-02-29 09:52:39 +0100

    Fixes and investor popup implementation
    Former-commit-id: 384b948c4e19ffbbd4b51cdf3035b9423e16ca5f

--- landmatrix/static/js/editor/detail.js
+++ landmatrix/static/js/editor/detail.js
@@ -1,6 +1,38 @@
+var openInvestorPopup = function(investorId) {
+    if (typeof investorId === 'undefined') {
+        investorId = 'add';
+    }
+    window.open("/en/stakeholder/" + investorId);
+};
+
 $(document).ready(function () {
     console.log('Deal Detail js loaded.');
 
     // TODO: Get Investor ID from Select and replace edit url
-    $(".investorfield").parent().append('<span class="investorops"><a href="/stakeholder" target="_blank"><i class="lm lm-plus"></i></a><a href="/stakeholder/25" target="_blank"><i class="lm lm-pencil"></i></a></span>');
+
+
+    var generateButtons = function(field) {
+        var investorId = field.val();
+        console.log("Heya:", field, investorId);
+
+        var buttons = '<a onClick="openInvestorPopup()" href="javascript:void(0);" class="noul"><i class="lm lm-plus"></i></a>';
+        buttons += '<a onClick="openInvestorPopup(' + investorId + ')" href="javascript:void(0);" class="noul"><i class="lm lm-pencil"></i></a>';
+        var wrap = '<span class="investorops">' + buttons + '</span>';
+
+        field.parent().find('.investorops').remove();
+        field.parent().append(wrap);
+
+    };
+
+    $(".investorfield").each(function () {
+        var investorId = $(this).val();
+        $(this).select2({
+            placeholder: 'Select Investor'
+        });
+        console.log('Investor:', investorId);
+        generateButtons($(this));
+        $(this).on('change', function () {
+             generateButtons($(this));
+        })
+    });
 });
