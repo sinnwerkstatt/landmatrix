42c03b69f207f9b100bf4adb6a98bb3d438e4a93
Lene Preuss <lene.preuss@gmail.com>
2016-03-01 16:38:47 +0100

    validate multiple selected values correctly

--- grid/widgets/nested_multiple_choice_field.py
+++ grid/widgets/nested_multiple_choice_field.py
@@ -12,20 +12,16 @@ class NestedMultipleChoiceField(forms.MultipleChoiceField):
 
     def valid_value(self, value):
         "Check to see if the provided value is a valid choice"
-        for k, v, c in self.choices:
-            if isinstance(v, (list, tuple)):
-                # This is an optgroup, so look inside the group for options
-                for k2, v2 in v:
-                    if value == smart_text(k2):
-                        return True
-            else:
-                if value == smart_text(k):
-                    return True
-                elif c:
-                    for k2, v2 in c:
-                        if value == smart_text(k2):
-                            return True
-        return False
+        return int(value) in self._valid_keys()
+
+    def _valid_keys(self):
+        values = []
+        for k, v, sub_choices in self.choices:
+            values.append(int(k))
+            if isinstance(sub_choices, (list, tuple)):
+                for k2, v2 in sub_choices:
+                    values.append(int(k2))
+        return values
 
     def get_value(self, key):
         for k, v, c in self.choices:
