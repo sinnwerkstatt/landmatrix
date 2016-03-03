0acac07c6382d9e6920bfbfcbb57da2fa20aae2a
Lene Preuss <lene.preuss@gmail.com>
2016-03-03 15:00:26 +0100

    cache country choices field in investor form so instantiating the form is significantly quicker this especially helps with instantiating filters (on almost every page) because BrowseFilterConditions instantiates all forms once
    Former-commit-id: ed6fe921fa686d9fef40e2fbd89db5d1f342250f

--- grid/forms/parent_stakeholder_formset.py
+++ grid/forms/parent_stakeholder_formset.py
@@ -3,7 +3,7 @@ from django_select2.forms import ModelSelect2Widget
 
 from grid.forms.base_form import BaseForm
 from grid.forms.investor_form import InvestorField
-from grid.forms.operational_stakeholder_form import _investor_description
+from grid.forms.operational_stakeholder_form import investor_description
 
 from landmatrix.models.investor import Investor, InvestorVentureInvolvement
 
@@ -42,7 +42,7 @@ class ParentStakeholderForm(BaseForm):
 
     def _fill_investor_choices(self):
         self.investor_choices = [
-            (investor.id, _investor_description(investor))
+            (investor.id, investor_description(investor))
             for investor in Investor.objects.filter(fk_status_id__in=(2, 3)).order_by('name')
         ]
         self.fields["stakeholder"].choices = list(self.fields["stakeholder"].choices)[:1]
@@ -84,7 +84,7 @@ class ParentInvestorForm(BaseForm):
 
     def _fill_investor_choices(self):
         self.investor_choices = [
-            (investor.id, _investor_description(investor))
+            (investor.id, investor_description(investor))
             for investor in Investor.objects.filter(fk_status_id__in=(2, 3)).order_by('name')
         ]
         self.fields["parent_investor"].choices = list(self.fields["parent_investor"].choices)[:1]
