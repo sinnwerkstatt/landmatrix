9a19017c9d19f4ab1c9fcfd54294bbffc5e5c6d8
Lene Preuss <lene.preuss@gmail.com>
2016-03-07 12:19:20 +0100

    changed all imports from editor to old_editor after having to change the app name editor in the old landmatrix to old_editor after the app dashboard was changed to editor in the new landmatrix (sigh)
    Former-commit-id: d6a6576262853ce9816b4dafd7705cfa56eccc01

--- from_v1/mapping/map_crop.py
+++ from_v1/mapping/map_crop.py
@@ -1,13 +1,13 @@
 from mapping.map_model import MapModel
 import landmatrix.models
-import editor.models
+import old_editor.models
 from mapping.map_agricultural_produce import MapAgriculturalProduce
 
 __author__ = 'Lene Preuss <lp@sinnwerkstatt.com>'
 
 
 class MapCrop(MapModel):
-    old_class = editor.models.Crop
+    old_class = old_editor.models.Crop
     new_class = landmatrix.models.Crop
     attributes = {
         'agricultural_produce': 'fk_agricultural_produce',
