085da7f787fc8ebd7877ca09d50bddf973876ebd
Lene Preuss <lene.preuss@gmail.com>
2016-03-10 18:30:23 +0100

    added bodytext field to root page so it can work as a real page and not just a footer template
    Former-commit-id: d78580577afe174b025531091ce22875ae46862f

--- wagtailcms/models.py
+++ wagtailcms/models.py
@@ -6,12 +6,14 @@ __author__ = 'Lene Preuss <lp@sinnwerkstatt.com>'
 
 
 class WagtailRootPage(Page):
+    body = RichTextField(blank=True)
     footer_column_1 = RichTextField(blank=True)
     footer_column_2 = RichTextField(blank=True)
     footer_column_3 = RichTextField(blank=True)
     footer_column_4 = RichTextField(blank=True)
 
     content_panels = Page.content_panels + [
+        FieldPanel('body', classname="full"),
         FieldPanel('footer_column_1'),
         FieldPanel('footer_column_2'),
         FieldPanel('footer_column_3'),
