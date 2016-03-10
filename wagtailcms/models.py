63a04a0396d94ab266267f9059b425daabb1b47d
Lene Preuss <lene.preuss@gmail.com>
2016-03-10 18:03:04 +0100

    moved wagtailcms stuff to newly created app wagtailcms created root page model (which defines the footer columns) and page model (which defines page content) added context processor that passes the wagtail root page to every template

---
+++ wagtailcms/models.py
@@ -0,0 +1,24 @@
+from wagtail.wagtailcore.models import Page
+from wagtail.wagtailcore.fields import RichTextField
+from wagtail.wagtailadmin.edit_handlers import FieldPanel
+
+__author__ = 'Lene Preuss <lp@sinnwerkstatt.com>'
+
+
+class WagtailRootPage(Page):
+    footer_column_1 = RichTextField(blank=True)
+    footer_column_2 = RichTextField(blank=True)
+    footer_column_3 = RichTextField(blank=True)
+    footer_column_4 = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('footer_column_1'),
+        FieldPanel('footer_column_2'),
+        FieldPanel('footer_column_3'),
+        FieldPanel('footer_column_4')
+    ]
+
+
+class WagtailPage(Page):
+    body = RichTextField(blank=True)
+    content_panels = Page.content_panels + [FieldPanel('body', classname="full")]
