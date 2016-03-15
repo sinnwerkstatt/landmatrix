b37e35c670323c092be9928c1aacabf66f4d9dee
Lene Preuss <lene.preuss@gmail.com>
2016-03-15 14:56:42 +0100

    moved query set classes out into their respective source files
    Former-commit-id: 8b695931bf494c8cd5abc883a20472b1972767fc

---
+++ api/query_sets/countries_query_set.py
@@ -0,0 +1,13 @@
+from api.query_sets.simple_fake_query_set import SimpleFakeQuerySet
+from landmatrix.models.country import Country
+
+__author__ = 'Lene Preuss <lp@sinnwerkstatt.com>'
+
+
+class CountriesQuerySet(SimpleFakeQuerySet):
+    def all(self):
+        if self.get_data.get('region'):
+            countries = Country.objects.filter(fk_region__slug=self.get_data['region']).order_by('name')
+        else:
+            countries = Country.objects.all().order_by('name')
+        return [[country.slug, country.name] for country in countries]
