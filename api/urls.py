0988d07928698c5b79c3affb437820dd642331e6
Lene Preuss <lene.preuss@gmail.com>
2016-03-16 12:34:19 +0100

    set, remove and list filters in the session via a REST API. the stored filters are not evaluated yet.

--- api/urls.py
+++ api/urls.py
@@ -1,4 +1,5 @@
 from api.views.json_view import JSONView
+from api.views.filter_view import FilterView
 
 from django.conf.urls import include, url, patterns
 from django.views.decorators.cache import cache_page
@@ -9,7 +10,8 @@ CACHE_TIMEOUT = 24*3600
 
 urlpatterns = patterns('')
 
-urlpatterns.append(
-    url(r'^(?P<type>.*\.json)', cache_page(CACHE_TIMEOUT)(JSONView.as_view()), name='landmatrix_api')
-)
+urlpatterns.extend([
+    url(r'^filter.json', FilterView.as_view(), name='landmatrix_api'),
+    url(r'^(?P<type>.*\.json)', cache_page(CACHE_TIMEOUT)(JSONView.as_view()), name='landmatrix_api'),
+])
 
---
+++ api/views/filter_view.py
@@ -0,0 +1,57 @@
+from django.http.response import HttpResponse
+from django.views.generic.base import TemplateView
+
+import json
+
+__author__ = 'Lene Preuss <lp@sinnwerkstatt.com>'
+
+
+class Filter(dict):
+
+    filter_number = 0
+
+    def __init__(self, variable, operator, value, name=None):
+        Filter.filter_number += 1
+        super().__init__(
+            {'name': name if name else 'filter_{}'.format(Filter.filter_number),
+             'variable': variable, 'operator': operator, 'value': value}
+        )
+
+    @property
+    def name(self):
+        return self['name']
+
+
+class FilterView(TemplateView):
+
+    filters = {}
+
+    def dispatch(self, request, *args, **kwargs):
+        values = dict(request.GET)
+        values.update(request.POST)
+        action = values.pop('action', None)
+        if isinstance(action, list):
+            action = action.pop()
+
+        if request.user not in self.filters:
+            self.filters[request.user] = {}
+
+        stored_filters = request.session.get('filters', {})
+        if action.lower() == 'set':
+            new_filter = Filter(values['variable'], values['operator'], values['value'], values.get('name'))
+            stored_filters[new_filter.name] = new_filter
+            request.session['filters'] = stored_filters
+            self.filters[request.user] = stored_filters
+            print(self.filters)
+            return HttpResponse(json.dumps(new_filter['name']), content_type="application/json")
+        elif action.lower() == 'remove':
+            name = values.get('name').pop()
+            print(stored_filters)
+            print(name)
+            stored_filters.pop(name, None)
+            request.session['filters'] = stored_filters
+            self.filters[request.user] = stored_filters
+            return HttpResponse(json.dumps(''), content_type="application/json")
+        elif action.lower() == 'list':
+            return HttpResponse(json.dumps(stored_filters))
+
