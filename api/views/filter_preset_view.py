442aec43a6d19d52379235f8cdac01ebcd8d1d3d
Lene Preuss <lene.preuss@gmail.com>
2016-03-24 14:40:59 +0100

    added an API endpoint to group preset filters and list them by group
    Former-commit-id: b73596c878d1a4e87c6921f942127c67bbacdbd1

---
+++ api/views/filter_preset_view.py
@@ -0,0 +1,27 @@
+from landmatrix.models.filter_preset import FilterPreset
+
+from django.http.response import HttpResponse
+from django.views.generic.base import TemplateView
+
+import json
+
+__author__ = 'Lene Preuss <lp@sinnwerkstatt.com>'
+
+
+class FilterPresetView(TemplateView):
+
+    def dispatch(self, request, *args, **kwargs):
+        values = request.GET
+        if 'show_groups' in values:
+            groups = (FilterPreset.objects.values_list('group', flat=True).distinct()
+            return HttpResponse(json.dumps(tuple(groups)))
+
+        objects = FilterPreset.objects
+        if 'group' in values:
+            objects = objects.filter(group=values['group'])
+        presets = objects.values('id', 'group', 'name')
+        return HttpResponse(json.dumps(tuple(presets)))
+
+
+
+
