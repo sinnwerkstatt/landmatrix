65e613feabb6015dfa393b0696072d03a5206c04
Lene Preuss <lene.preuss@gmail.com>
2016-03-24 14:40:59 +0100

    added an API endpoint to group preset filters and list them by group
    Former-commit-id: 44af9dbfca80c3a7009a264596b700a6a1debaf8

--- api/urls.py
+++ api/urls.py
@@ -1,3 +1,4 @@
+from api.views.filter_preset_view import FilterPresetView
 from api.views.json_view import JSONView
 from api.views.filter_view import FilterView
 
@@ -12,7 +13,8 @@ urlpatterns = patterns('')
 
 urlpatterns.extend([
     url(r'^filter.json', FilterView.as_view(), name='landmatrix_api'),
-    url(r'^(?P<type>.*\.json)', cache_page(CACHE_TIMEOUT)(JSONView.as_view()), name='landmatrix_api'),
+    url(r'^filter_preset.json', FilterPresetView.as_view(), name='landmatrix_api'),
+url(r'^(?P<type>.*\.json)', cache_page(CACHE_TIMEOUT)(JSONView.as_view()), name='landmatrix_api'),
     # url(r'^(?P<type>.*\.json)', JSONView.as_view(), name='landmatrix_api'),
 ])
 
