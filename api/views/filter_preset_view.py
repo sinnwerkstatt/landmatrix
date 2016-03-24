72edf572425a52745c872c0c677f5fc335e0cbe4
Lene Preuss <lene.preuss@gmail.com>
2016-03-24 16:06:25 +0100

    remove generic BrowseConditions from preset filters, only use those that come from FilterPresets

--- api/views/filter_preset_view.py
+++ api/views/filter_preset_view.py
@@ -13,7 +13,7 @@ class FilterPresetView(TemplateView):
     def dispatch(self, request, *args, **kwargs):
         values = request.GET
         if 'show_groups' in values:
-            groups = (FilterPreset.objects.values_list('group', flat=True).distinct()
+            groups = FilterPreset.objects.values_list('group', flat=True).distinct()
             return HttpResponse(json.dumps(tuple(groups)))
 
         objects = FilterPreset.objects
