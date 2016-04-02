9a0dae9c473299184123222c57149e254962e0db
Simon Liedtke <sl@sinnwerkstatt.com>
2016-04-02 22:58:03 +0200

    Introduced get_attributes, add/save deal working
    Former-commit-id: 49786658058bb9ac6f9e896c88cfc04c9e36827b

--- grid/views/add_deal_view.py
+++ grid/views/add_deal_view.py
@@ -6,15 +6,20 @@ __author__ = 'Lene Preuss <lp@sinnwerkstatt.com>'
 
 
 class AddDealView(SaveDealView):
-
     template_name = 'add-deal.html'
+    #success_message = _('Added successfully.')
 
-    def get_activity(self, **kwargs):
+    def post(self, request, *args, **kwargs):
+    	# Create activity first
         activity_identifier = Activity.objects.values().aggregate(Max('activity_identifier'))['activity_identifier__max'] + 1
-        return Activity(activity_identifier=activity_identifier, fk_status_id=1)
+        self.activity = Activity.objects.create(
+        	activity_identifier=activity_identifier,
+        	fk_status_id=1
+        )
+        return super(AddDealView, self).post(request, *args, **kwargs)
 
     def get_forms(self, data=None):
-            forms = []
-            for form in self.FORMS:
-                forms.append(form(data or None))
-            return forms
+        forms = []
+        for form in self.FORMS:
+            forms.append(form(data or None))
+        return forms
