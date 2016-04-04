52f5518c9bed21d1a8f580289ab520ed845a94d2
Lene Preuss <lene.preuss@gmail.com>
2016-04-04 14:06:43 +0200

    Revert "Revert "added second coordinate fields to country and region so they store the full extent and not just the central point""
    This reverts commit 0192886d049e60eab724746ed1dce4e1d3bb8640 [formerly 234f5e54ead4c187c2a03f77eac6387969cb1008].
    
    
    Former-commit-id: 170ddba0ed3a1d012ddb6ee6f4ee45e890084e82

--- landmatrix/models/region.py
+++ landmatrix/models/region.py
@@ -8,6 +8,8 @@ from landmatrix.models.default_string_representation import DefaultStringReprese
 class Region(models.Model, DefaultStringRepresentation):
     name = models.CharField("Name", max_length=255)
     slug = models.SlugField("Slug")
-    point_lat = models.DecimalField(_("Point lat"), max_digits=18, decimal_places=12, blank=True, null=True)
-    point_lon = models.DecimalField(_("Point lon"), max_digits=18, decimal_places=12, blank=True, null=True)
+    point_lat_min = models.DecimalField(_("Latitude of northernmost point"), max_digits=18, decimal_places=12, blank=True, null=True)
+    point_lon_min = models.DecimalField(_("Longitude of westernmost point"), max_digits=18, decimal_places=12, blank=True, null=True)
+    point_lat_max = models.DecimalField(_("Latitude of southernmost point"), max_digits=18, decimal_places=12, blank=True, null=True)
+    point_lon_max = models.DecimalField(_("Longitude of easternmost point"), max_digits=18, decimal_places=12, blank=True, null=True)
 
