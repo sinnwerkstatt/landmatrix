3102de7b1616857b85928bae757c0f22107bd19a
Lene Preuss <lene.preuss@gmail.com>
2016-04-06 18:00:41 +0200

    investor/stakeholder network API endpoint in /api/investor_network.json
    Former-commit-id: 86240d98f5df48d86802185fc98c0245ac7def4d

---
+++ api/views/investor_network_json_view.py
@@ -0,0 +1,67 @@
+from django.http.response import HttpResponse
+from django.views.generic.base import TemplateView
+
+import json
+
+from landmatrix.models.investor import Investor, InvestorVentureInvolvement
+
+__author__ = 'Lene Preuss <lp@sinnwerkstatt.com>'
+
+
+class InvestorNetworkJSONView(TemplateView):
+
+    def dispatch(self, request, *args, **kwargs):
+        operational_stakeholder = Investor.objects.get(pk=int(request.GET.get('operational_stakeholder')))
+        involvements = InvestorVentureInvolvement.objects.filter(fk_venture=operational_stakeholder)
+        print(operational_stakeholder)
+        print(involvements)
+        stakeholder_involvements = involvements.filter(role='ST')
+        investor_involvements = involvements.filter(role='IN')
+
+        nodes = []
+        stakeholder_index = None
+        if stakeholder_involvements.count():
+            nodes.append({
+                "name": "Stakeholders",
+                "id": "stakeholders"
+            })
+            stakeholder_index = 0
+
+        investor_index = None
+        if investor_involvements.count():
+            investor_index = len(nodes)
+            nodes.append({
+                "name": "Investors",
+                "id": "investors"
+            })
+
+        stakeholder_start_index = len(nodes)
+        for involvement in stakeholder_involvements:
+            nodes.append({
+                'name': involvement.fk_investor.name,
+                'id': 'stakeholder_{}'.format(involvement.fk_investor_id)
+            })
+        investor_start_index = len(nodes)
+        for involvement in investor_involvements:
+            nodes.append({
+                'name': involvement.fk_investor.name,
+                'id': 'investor_{}'.format(involvement.fk_investor_id)
+            })
+        print(nodes)
+
+        links = []
+
+        for i, involvement in enumerate(stakeholder_involvements):
+            links.append({
+                'source': stakeholder_index,
+                'target': stakeholder_start_index+i,
+                'value': involvement.percentage
+            })
+        for i, involvement in enumerate(investor_involvements):
+            links.append({
+                'source': investor_index,
+                'target': investor_start_index+i,
+                'value': involvement.percentage
+            })
+
+        return HttpResponse(json.dumps({'nodes': nodes, 'links': links}))
