c0afc5b7895533731d9238bec5a12c22102b00cf
Simon Liedtke <sl@sinnwerkstatt.com>
2016-04-10 00:42:02 +0200

    Added dashboard filters, user filter still missing
    Former-commit-id: 8df742612d18cf289049520d1a5dc07950e1c54c

--- landmatrix/admin.py
+++ landmatrix/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from landmatrix.models.filter_condition import FilterCondition
-from landmatrix.models.filter_preset import FilterPreset
+
+from landmatrix.models import *
 
 admin.site.register(FilterPreset)
 admin.site.register(FilterCondition)
+admin.site.register(Country)
+admin.site.register(Region)
