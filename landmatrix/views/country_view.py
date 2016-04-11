14f26a73aba531bafce7ce87fec7f05532538fc6
Simon Liedtke <sl@sinnwerkstatt.com>
2016-04-11 23:28:51 +0200

    Country view updates
    Former-commit-id: 7e0ed527166abb85c67baacb3c05590b7badec97

--- landmatrix/views/country_view.py
+++ landmatrix/views/country_view.py
@@ -1,6 +1,12 @@
 from django.http.response import HttpResponse
 from django.views.generic.base import View
 
+from wagtailcms.models import CountryIndex, Country
+
 class CountryView(View):
     def get(self, *args, **kwargs):
-    	return HttpResponse()
+    	try:
+    		country = Country.objects.get(slug=kwargs.get('country_slug'))
+    	except:
+	    	country = CountryIndex.objects.get(slug='country')
+    	return country.serve(self.request)
