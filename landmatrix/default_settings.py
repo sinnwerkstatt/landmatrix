006273d6095a2ca39b7a96ba49b21f5c5fbebfba
riot <riot@hackerfleet.org>
2016-04-18 15:51:04 +0200

    Don't generate statics from statics
    Former-commit-id: 5f93576b9a671ce1a6ad1126af00205c8c9ee4be

--- landmatrix/default_settings.py
+++ landmatrix/default_settings.py
@@ -177,7 +177,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "landmatrix/static")
 STATIC_URL = '/static/'
 
 #SASS_PATH = os.path.join(BASE_DIR, 'landmatrix/static/css'),
