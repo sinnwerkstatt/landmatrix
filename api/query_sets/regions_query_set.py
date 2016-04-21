e2748e8adb7d3786a63f276ef87937e8f061c8dd
Simon Liedtke <sl@sinnwerkstatt.com>
2016-04-21 14:57:42 +0200

    Limit region dropdown to cms pages
    Former-commit-id: f1cb251a07d93933c0599a8fd750d6e5b6b10ca0

--- api/query_sets/regions_query_set.py
+++ api/query_sets/regions_query_set.py
@@ -1,10 +1,10 @@
 from api.query_sets.simple_fake_query_set import SimpleFakeQuerySet
-from landmatrix.models.region import Region
+from wagtailcms.models import Region
 
 __author__ = 'Lene Preuss <lp@sinnwerkstatt.com>'
 
 
 class RegionsQuerySet(SimpleFakeQuerySet):
     def all(self):
-        regions = Region.objects.all().order_by('name')
-        return [[region.id, region.slug, region.name] for region in regions]
+        regions = Region.objects.all().order_by('title')
+        return [[region.region.id, region.region.slug, region.region.name] for region in regions]
