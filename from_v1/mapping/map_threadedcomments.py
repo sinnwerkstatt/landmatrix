b346ac86d783b99b0d206ed42a86b49dcc237495
Lene Preuss <lene.preuss@gmail.com>
2016-04-26 17:21:46 +0200

    migrate django comments and threaded comments. threaded comments locked up when ThreadedComment.save() was called without kwarg skip_tree_path=True. worked around that issue by giving it. have not investigated the internals of ThreadedComment.save() yet, might need to change things.
    Former-commit-id: 29d859b4c40d701431e1975b71a4279b4de08fea

--- from_v1/mapping/map_threadedcomments.py
+++ from_v1/mapping/map_threadedcomments.py
@@ -5,6 +5,7 @@ from migrate import V1, V2
 
 from django.db import models, transaction
 from django_comments.models import Comment
+from threadedcomments.models import ThreadedComment
 
 __author__ = 'Lene Preuss <lp@sinnwerkstatt.com>'
 
@@ -20,4 +21,18 @@ class MapDjangoComments(MapModel):
 
 
 class MapThreadedComments(MapModel):
-    pass
+    old_class = ThreadedComment
+    new_class = ThreadedComment
+    depends = [MapDjangoComments]
+
+    @classmethod
+    def all_records(cls):
+        comment_ids = list(Comment.objects.using(V2).values_list('id', flat=True))
+        print(comment_ids, len(comment_ids))
+        return ThreadedComment.objects.using(V1).filter(pk__in=comment_ids).values()
+
+    @classmethod
+    def save_record(cls, new, save):
+        if save:
+            new.save(using=V2, skip_tree_path=True)
+
