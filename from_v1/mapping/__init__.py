ff9af7fbc7954feaedade63d5f10a8ecadc07c92
Lene Preuss <lene.preuss@gmail.com>
2016-04-26 16:21:06 +0200

    fixed errors from naively made former edits added a class (which does nothing yet) for mapping django comments
    Former-commit-id: 53c63d4f60c2cc42b8e6cbc5eed061e75d66f69f

--- from_v1/mapping/__init__.py
+++ from_v1/mapping/__init__.py
@@ -9,11 +9,11 @@ from mapping.map_region import MapRegion
 from mapping.map_country import MapCountry
 from mapping.map_browse_rule import MapBrowseRule
 from mapping.map_browse_condition import MapBrowseCondition
-from mapping.map_agricultural_produce import MapAgriculturalProduce
-from mapping.map_crop import MapCrop, MapAnimal, MapMineral
+from mapping.map_crop import MapCrop, MapAnimal, MapMineral, MapAgriculturalProduce
 from mapping.map_comment import MapComment
 from mapping.map_investor import MapInvestor
 from mapping.map_investor_activity_involvement import MapInvestorActivityInvolvement
 from mapping.map_stakeholder_investor import MapStakeholderInvestor
 from mapping.map_stakeholder_venture_involvement import MapStakeholderVentureInvolvement
 from mapping.map_public_interface_cache import MapPublicInterfaceCache
+from mapping.map_threadedcomments import MapDjangoComments, MapThreadedComments
