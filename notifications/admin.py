7f356372d0f8bc1bb3cb5ef40d21f6f2d1898223
Cole Maclean <hi@cole.io>
2016-05-03 14:51:20 -0700

    Got comment notifications actually working
    Former-commit-id: f1a736503e0d8a7096e38d2215451ca3a55b3914

--- notifications/admin.py
+++ notifications/admin.py
@@ -1,6 +1,10 @@
+from django.utils.translation import ugettext_lazy as _
 from django.contrib import admin
 
 from .models import NotificationEmail
+
+
+
 
 
 class NotificationEmailAdmin(admin.ModelAdmin):
@@ -12,6 +16,7 @@ class NotificationEmailAdmin(admin.ModelAdmin):
         'sent_exception', 'sent_status', 'created_on', 'sent_on', 'to', 'cc',
         'bcc', 'reply_to', 'subject', 'from_email', 'body_text', 'body_html',
     )
+    actions = ['resend_failed_emails']
 
     def has_add_permission(self, request, obj=None):
         return False
@@ -19,5 +24,11 @@ class NotificationEmailAdmin(admin.ModelAdmin):
     def has_delete_permission(self, request, obj=None):
         return False
 
+    def resend_failed_emails(self, request, queryset):
+        # Exclude already sent ones
+        queryset = queryset.exclude(sent_status=NotificationEmail.STATUS_SENT)
+        for email in queryset:
+            email.send()
+    resend_failed_emails.short_description = _("Retry failed notifications")
 
 admin.site.register(NotificationEmail, NotificationEmailAdmin)
