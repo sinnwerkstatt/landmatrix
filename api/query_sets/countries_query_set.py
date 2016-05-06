0294844826df90bcec9859377bcf5b1558d1bfe3
Simon Liedtke <sl@sinnwerkstatt.com>
2016-05-06 16:39:38 +0200

    Fixed #145
    Former-commit-id: e02de3e1b25a46958550a4d45731816b8017bba4

--- api/query_sets/countries_query_set.py
+++ api/query_sets/countries_query_set.py
@@ -1,5 +1,8 @@
+from django.utils.translation import get_language, ugettext_lazy as _
+
 from api.query_sets.simple_fake_query_set import SimpleFakeQuerySet
 from landmatrix.models.country import Country
+from wagtailcms.models import Country as WagtailCountry
 
 __author__ = 'Lene Preuss <lp@sinnwerkstatt.com>'
 
@@ -9,5 +12,16 @@ class CountriesQuerySet(SimpleFakeQuerySet):
         #if self.get_data.get('region'): #FIXME: Doesn't work, get_data returns request
         #    countries = Country.objects.filter(fk_region__slug=self.get_data['region']).order_by('name')
         #else:
-        countries = Country.objects.all().order_by('name')
-        return [[country.id, country.slug, country.name] for country in countries]
+        # Return country pages then all other countries in two option groups
+        response = []
+        countries = WagtailCountry.objects.all().order_by('title')
+        response.append({
+        	'text': str(_('Country observatories')),
+        	'children': [[country.id, country.slug, country.title] for country in countries]
+        })
+        countries = Country.objects.exclude(id__in=[c.country.id for c in countries]).order_by('name')
+        response.append({
+        	'text': str(_('Other countries')),
+        	'children': [[country.id, country.slug, country.name] for country in countries]
+        })
+        return response
