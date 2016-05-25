3e6e38f021e47d7efaa838407e16311989bc6ce7
Simon Liedtke <sl@sinnwerkstatt.com>
2016-05-25 21:31:28 +0200

    Updated requirements for custom wagtail model translation

--- wagtail_modeltranslation/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-"""
-This app is a fork from django-modeltranslation - https://github.com/deschler/django-modeltranslation
-"""
-
-
-default_app_config = 'wagtail_modeltranslation.apps.ModeltranslationConfig'
