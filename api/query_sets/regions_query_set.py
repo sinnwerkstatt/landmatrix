2e9b88010a7e9f4a46d5f2083a42d5a6d20bc99e
Simon Liedtke <sl@sinnwerkstatt.com>
2016-07-02 18:53:05 +0200

    Finished region pages

--- api/query_sets/regions_query_set.py
+++ api/query_sets/regions_query_set.py
@@ -1,10 +1,11 @@
 from api.query_sets.simple_fake_query_set import SimpleFakeQuerySet
 from wagtailcms.models import RegionPage
+from landmatrix.models import Region
 
 __author__ = 'Lene Preuss <lp@sinnwerkstatt.com>'
 
 
 class RegionsQuerySet(SimpleFakeQuerySet):
     def all(self):
-        regions = RegionPage.objects.all().order_by('title')
-        return [[region.region.id, region.slug, region.title] for region in regions]
+        regions = Region.objects.all().order_by('name')
+        return [[region.id, region.slug, region.name] for region in regions]
