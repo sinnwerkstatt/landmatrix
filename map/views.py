601fab01326e45ae103ec43501e0434296c52743
Simon Liedtke <sl@sinnwerkstatt.com>
2016-07-22 10:51:25 +0200

    Fixed #784

--- map/views.py
+++ map/views.py
@@ -2,7 +2,10 @@ from grid.views.view_aux_functions import render_to_response
 
 from django.views.generic import TemplateView
 from django.template import RequestContext
+
 from grid.views.filter_widget_mixin import FilterWidgetMixin
+from landmatrix.models.country import Country
+from landmatrix.models.region import Region
 
 __author__ = 'Lene Preuss <lp@sinnwerkstatt.com>'
 
@@ -21,5 +24,18 @@ class MapView(TemplateView, FilterWidgetMixin):
 
         context = self.get_context_data(**kwargs)
         self.add_filter_context_data(context, request)
+
+        # Target country set?
+        filters = request.session.get('filters')
+        if 'Target country' in filters:
+            try:
+                context['country'] = Country.objects.get(pk=filters['Target country']['value'])
+            except:
+                pass
+        if 'Target region' in filters:
+            try:
+                context['region'] = Country.objects.get(pk=filters['Target region']['value'])
+            except:
+                pass
 
         return render_to_response(self.template_name, context, RequestContext(request))
