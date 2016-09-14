f133e9bbd5ca884a103510306bc8972565aa14d0
Cole Maclean <hi@cole.io>
2016-09-13 20:08:04 -0700

    Declaring test bankruptcy
    We had a lot of tests... but none of them have been able to run for ages
    now. Rather then just let them fall further out of date, clean everything
    up so that we can write and run new ones easily.

--- charts/tests.py
+++ charts/tests.py
@@ -1,13 +1,5 @@
 from django.test import TestCase
 
 
-from .pdfgen import pdfize_url
-
-
-class PDFGenTests(TestCase):
-
-    def test_pdfize_url_returns_streaminghttpresponse(self):
-        response = pdfize_url('http://google.com', 'test.pdf')
-        self.assertTrue(hasattr(response, 'streaming_content'))
-        self.assertEqual('attachment; filename="test.pdf"',
-                         response['Content-Disposition'])
+class ChartTests(TestCase):
+    pass
--- landmatrix/tests/with_status.py
+++
@@ -1,8 +0,0 @@
-__author__ = 'Lene Preuss <lp@sinnwerkstatt.com>'
-
-from django.test import TestCase
-from landmatrix.models.status import Status
-
-class WithStatus(TestCase):
-    def setUp(self):
-        self.status = Status.objects.get(id=1)
