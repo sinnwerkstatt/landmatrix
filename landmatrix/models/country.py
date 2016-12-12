34b13ce54933f0e1651d8439ab6324f0ef38d783
Cole Maclean <hi@cole.io>
2016-12-12 15:02:09 -0800

    Fixed populate countries command

--- landmatrix/models/country.py
+++ landmatrix/models/country.py
@@ -16,31 +16,32 @@ class Country(models.Model):
     name = models.CharField("Name", max_length=255)
     slug = models.SlugField("Slug", max_length=100)
     point_lat = models.DecimalField(
-        _("Latitude of central point"), max_digits=18, decimal_places=12, blank=True, null=True
-    )
+        _("Latitude of central point"), max_digits=18, decimal_places=12,
+        blank=True, null=True)
     point_lon = models.DecimalField(
-        _("Longitude of central point"), max_digits=18, decimal_places=12, blank=True, null=True
-    )
+        _("Longitude of central point"), max_digits=18, decimal_places=12,
+        blank=True, null=True)
     point_lat_min = models.DecimalField(
-        _("Latitude of northernmost point"), max_digits=18, decimal_places=12, blank=True, null=True
-    )
+        _("Latitude of southernmost point"), max_digits=18, decimal_places=12,
+        blank=True, null=True)
     point_lon_min = models.DecimalField(
-        _("Longitude of westernmost point"), max_digits=18, decimal_places=12, blank=True, null=True
-    )
+        _("Longitude of westernmost point"), max_digits=18, decimal_places=12,
+        blank=True, null=True)
     point_lat_max = models.DecimalField(
-        _("Latitude of southernmost point"), max_digits=18, decimal_places=12, blank=True, null=True
-    )
+        _("Latitude of northernmost point"), max_digits=18, decimal_places=12,
+        blank=True, null=True)
     point_lon_max = models.DecimalField(
-        _("Longitude of easternmost point"), max_digits=18, decimal_places=12, blank=True, null=True
-    )
+        _("Longitude of easternmost point"), max_digits=18, decimal_places=12,
+        blank=True, null=True)
     democracy_index = models.DecimalField(
-        _("Democracy index"), max_digits=3, decimal_places=2, blank=True, null=True
-    )
+        _("Democracy index"), max_digits=3, decimal_places=2, blank=True,
+        null=True)
     corruption_perception_index = models.DecimalField(
-        _("Corruption perception index"), max_digits=2, decimal_places=1, blank=True, null=True
-    )
+        _("Corruption perception index"), max_digits=2, decimal_places=1,
+        blank=True, null=True)
     high_income = models.BooleanField(_("High income"), default=False)
-    is_target_country = models.BooleanField(_("Is target country"), default=False)
+    is_target_country = models.BooleanField(
+        _("Is target country"), default=False)
 
     objects = CountryManager()
 
