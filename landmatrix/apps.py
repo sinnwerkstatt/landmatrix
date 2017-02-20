3e8cf6ffead5d035fb70b86dea47eb551bae83c1
Simon Liedtke <sl@sinnwerkstatt.com>
2017-02-20 18:10:04 +0100

    Added post_save signal for historical activity

---
+++ landmatrix/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+class LandMatrixConfig(AppConfig):
+    name = 'landmatrix'
+    verbose_name = "Land Matrix"
+
+    def ready(self):
+        from landmatrix import signals
