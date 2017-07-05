d53dfcf6ec7d99c3dfc607492b5afe4126867236
Simon Liedtke <sl@sinnwerkstatt.com>
2017-07-05 11:07:08 +0200

    Minor fixes

--- landmatrix/default_settings.py
+++ landmatrix/default_settings.py
@@ -126,6 +126,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sites.middleware.CurrentSiteMiddleware',
 
     # populate the history user automatically
     'simple_history.middleware.HistoryRequestMiddleware',
