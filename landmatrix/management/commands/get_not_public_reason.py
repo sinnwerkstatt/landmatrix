30aaa9a3efc27c333b126eae75d6d8e32c54b3a6
Simon Liedtke <sl@sinnwerkstatt.com>
2017-08-13 11:46:26 +0200

    Minor fixes

---
+++ landmatrix/management/commands/get_not_public_reason.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+from django.core.management import BaseCommand
+
+from landmatrix.models import Activity
+
+
+class Command(BaseCommand):
+    help = 'Check why deal is not public'
+
+    def add_arguments(self, parser):
+        parser.add_argument('activity_identifier', nargs='+', type=str)
+
+    def handle(self, *args, **options):
+        activity = Activity.objects.get(activity_identifier=options['activity_identifier'][0])
+        self.stdout.write(activity.get_not_public_reason())
