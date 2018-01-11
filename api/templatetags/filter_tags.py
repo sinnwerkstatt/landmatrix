5e99fd24e07590452750018082a4ae791edc92df
Simon Liedtke <sl@sinnwerkstatt.com>
2018-01-11 23:00:01 +0100

    Minor fixes

--- api/templatetags/filter_tags.py
+++ api/templatetags/filter_tags.py
@@ -22,3 +22,22 @@ def filter_query_params(context):
         params = ''
 
     return params
+
+
+@register.simple_tag(takes_context=True)
+def regional_params(context):
+    country = context.get('country', '')
+    region = context.get('region', '')
+    page = context.get('page', '')
+    request = context.get('request', '')
+    if country:
+        return '?country={}'.format(country.id)
+    elif region:
+        return '?region={}'.format(region.id)
+    elif hasattr(page, 'country'):
+        return '?country={}'.format(page.country.id)
+    elif hasattr(page, 'region'):
+        return '?region={}'.format(page.region.id)
+    elif request.GET:
+        return '?{}'.format(request.GET.urlencode())
+    return ''
