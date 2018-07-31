cae2d0d054e5aca9f9d9a6b9f48598da624862f3
Simon Liedtke <simon.liedtke@gmail.com>
2018-07-31 23:14:20 +0200

    Minor fixes

--- from_v1/mapping/map_investor.py
+++ from_v1/mapping/map_investor.py
@@ -29,8 +29,8 @@ def get_classification(_):
 
 
 def get_history_data(record):
-    changeset = old_editor.models.SH_Changeset.objects.using(V1)
-    changeset = changeset.filter(fk_stakeholder_id=record['id']).last()
+    changeset = old_editor.models.PI_Changeset.objects.using(V1)
+    changeset = changeset.filter(fk_primary_investor_id=record['id']).last()
     history_user_id = None
     if changeset:
         history_date = changeset.timestamp
@@ -71,6 +71,8 @@ ORDER BY pi.primary_investor_identifier
         versions = get_primary_investor_versions(new)
         for i, version in enumerate(versions):
             history_date, history_user_id = get_history_data(version)
+            import pdb
+            pdb.set_trace()
             landmatrix.models.HistoricalInvestor.objects.create(
                 id=version['id'],
                 investor_identifier=version['primary_investor_identifier'],
