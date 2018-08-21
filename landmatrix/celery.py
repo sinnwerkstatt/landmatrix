f4ec02a4c4adf37fbdecb7a62a59970a925097c9
Simon Liedtke <simon.liedtke@gmail.com>
2018-08-21 17:19:25 +0200

    Fixed celery app specific indexing

--- landmatrix/celery.py
+++ landmatrix/celery.py
@@ -7,7 +7,7 @@ from django.conf import settings
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'landmatrix.settings')
 
-app = Celery(namespace=settings.CELERY_NAME)
+app = Celery(settings.CELERY_NAME)
 
 # Using a string here means the worker will not have to
 # pickle the object when using Windows.
--- landmatrix/tasks.py
+++ landmatrix/tasks.py
@@ -1,13 +1,15 @@
 from __future__ import absolute_import
+from django.conf import settings
 
 from celery import shared_task
 from api.elasticsearch import es_save
 
-@shared_task
+
+@shared_task(name='%s.tasks.index_activity' % settings.CELERY_NAME)
 def index_activity(activity_id):
     es_save.index_activity_by_id(activity_id=activity_id)
 
 
-@shared_task
+@shared_task(name='%s.tasks.delete_activity' % settings.CELERY_NAME)
 def delete_activity(activity_identifier):
     es_save.delete_activity(activity_identifier=activity_identifier)
