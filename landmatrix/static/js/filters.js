dca35e369bef19b13ccd37e928b8feab37cfb1cd
Simon Liedtke <simon.liedtke@gmail.com>
2018-08-21 10:32:18 +0200

    Fixed missing select2 in filters

--- landmatrix/static/js/filters.js
+++ landmatrix/static/js/filters.js
@@ -260,7 +260,7 @@ $(document).ready(function () {
         );
     });
 
-    //$("#id_columns,#id_status").select2()
+    $("#id_columns,#id_status").select2();
     //    .on('change', function () { $(this).closest("form").find(':submit').show(); });
     
     var selectAllColumns = function (event) {
