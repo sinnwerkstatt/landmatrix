adf39191dd3867edcb877a06faa7499132c59471
Simon Liedtke <simon.liedtke@gmail.com>
2018-08-22 10:32:12 +0200

    Fixed celery app specific indexing

--- landmatrix/tasks.py
+++ landmatrix/tasks.py
@@ -1,15 +1,14 @@
 from __future__ import absolute_import
-from django.conf import settings
 
-from celery import shared_task
+from celery import app
 from api.elasticsearch import es_save
 
 
-@shared_task(name='%s.tasks.index_activity' % settings.CELERY_NAME)
+@app.task
 def index_activity(activity_id):
     es_save.index_activity_by_id(activity_id=activity_id)
 
 
-@shared_task(name='%s.tasks.delete_activity' % settings.CELERY_NAME)
+@app.task
 def delete_activity(activity_identifier):
     es_save.delete_activity(activity_identifier=activity_identifier)
