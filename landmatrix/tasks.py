2c88e5f916564956f0a7b1c2abfc5ebe7edceae5
Simon Liedtke <simon.liedtke@gmail.com>
2018-08-22 11:24:50 +0200

    Potential fix for timing issue when indexing

--- landmatrix/tasks.py
+++ landmatrix/tasks.py
@@ -6,11 +6,11 @@ from landmatrix.celery import app
 from api.elasticsearch import es_save
 
 
-@app.task('%s.tasks.index_activity' % settings.CELERY_NAME, bind=True)
+@app.task(name='%s.tasks.index_activity' % settings.CELERY_NAME, bind=True)
 def index_activity(self, activity_id):
     es_save.index_activity_by_id(activity_id=activity_id)
 
 
-@app.task('%s.tasks.delete_activity' % settings.CELERY_NAME, bind=True)
+@app.task(name='%s.tasks.delete_activity' % settings.CELERY_NAME, bind=True)
 def delete_activity(self, activity_identifier):
     es_save.delete_activity(activity_identifier=activity_identifier)
