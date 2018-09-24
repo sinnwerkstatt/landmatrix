96865d7f6d348f7880cfd90ce8a9ba9b6219fd3b
Simon Liedtke <simon.liedtke@gmail.com>
2018-09-24 15:29:34 +0200

    CMS changes

--- landmatrix/default_settings.py
+++ landmatrix/default_settings.py
@@ -322,4 +322,4 @@ DEBUG_TOOLBAR_PANELS = [
     'template_timings_panel.panels.TemplateTimings.TemplateTimings',
 ]
 
-BLOG_LIMIT_AUTHOR_CHOICES_GROUP = 'CMS Editors'
+BLOG_LIMIT_AUTHOR_CHOICES_GROUP = 'CMS Global (Editors)'
