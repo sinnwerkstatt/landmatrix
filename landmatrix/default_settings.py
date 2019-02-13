471c99490a3833ec84eb41ea3f7a2287f9cfacf9
Simon Liedtke <simon.liedtke@gmail.com>
2019-02-13 21:51:40 +0100

    Fixed requirements

--- landmatrix/default_settings.py
+++ landmatrix/default_settings.py
@@ -196,7 +196,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static-collected")
 STATIC_URL = '/static/'
 
 SASS_PATH = os.path.join(BASE_DIR, 'static/css'),
