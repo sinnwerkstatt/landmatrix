0d091d01191e3e995346a041d733bfc563381ce7
Simon Liedtke <simon.liedtke@gmail.com>
2019-04-29 12:01:22 +0200

    Fix update related bugs

--- landmatrix/views/handler500.py
+++ landmatrix/views/handler500.py
@@ -1,9 +1,8 @@
-from django.shortcuts import render_to_response
+from django.shortcuts import render
 from django.template import RequestContext
 
 
 def handler500(request):
-    response = render_to_response('500.html', {},
-                                  context_instance=RequestContext(request))
+    response = render('500.html', context=RequestContext(request))
     response.status_code = 500
     return response
