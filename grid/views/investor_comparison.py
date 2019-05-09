b7b499cea73ce87bf80e0a43452aa2b6924094b2
Simon Liedtke <simon.liedtke@gmail.com>
2019-05-09 11:27:00 +0200

    Fixed forms

--- grid/views/investor_comparison.py
+++ grid/views/investor_comparison.py
@@ -62,6 +62,7 @@ def has_changed(form_1, form_2):
     # Formset initialized with a dict throw a KeyError in _construct_form
     # So I'm replacing _construct_form with my customized version that
     # catches the KeyError here.
+    construct_form = BaseFormSet._construct_form
     BaseFormSet._construct_form = _construct_form
 
     if isinstance(form_1, BaseFormSet):
@@ -79,36 +80,9 @@ def has_changed(form_1, form_2):
             if str(field) != str(list(form_2)[i]):
                 return False
 
+    BaseFormSet._construct_form = construct_form
+
     return True
-
-
-# Hacked version of BaseFormSet._construct_form
-def _construct_form(self, i, **kwargs):
-        """
-        Instantiates and returns the i-th form instance in a formset.
-        """
-        defaults = {
-            'auto_id': self.auto_id,
-            'prefix': self.add_prefix(i),
-            'error_class': self.error_class,
-        }
-        if self.is_bound:
-            defaults['data'] = self.data
-            defaults['files'] = self.files
-        if self.initial and 'initial' not in kwargs:
-            try:
-                defaults['initial'] = self.initial[i]
-            # this is the line that has been changed!
-            except (IndexError, KeyError):
-                pass
-        # Allow extra forms to be empty, unless they're part of
-        # the minimum forms.
-        if i >= self.initial_form_count() and i >= self.min_num:
-            defaults['empty_permitted'] = True
-        defaults.update(kwargs)
-        form = self.form(**defaults)
-        self.add_fields(form, i)
-        return form
 
 
 def investor_from_id(investor_id):
@@ -139,3 +113,35 @@ def _investor_from_id_and_timestamp(id_and_timestamp):
                                                                                 timestamp))
 
     return old_version
+
+
+# Hacked version of BaseFormSet._construct_form
+def _construct_form(self, i, **kwargs):
+    """
+    Instantiates and returns the i-th form instance in a formset.
+    """
+    defaults = {
+        'auto_id': self.auto_id,
+        'prefix': self.add_prefix(i),
+        'error_class': self.error_class,
+        # Don't render the HTML 'required' attribute as it may cause
+        # incorrect validation for extra, optional, and deleted
+        # forms in the formset.
+        'use_required_attribute': False,
+    }
+    if self.is_bound:
+        defaults['data'] = self.data
+        defaults['files'] = self.files
+    if self.initial and 'initial' not in kwargs:
+        try:
+            defaults['initial'] = self.initial[i]
+        except (IndexError, KeyError):
+            pass
+    # Allow extra forms to be empty, unless they're part of
+    # the minimum forms.
+    if i >= self.initial_form_count() and i >= self.min_num:
+        defaults['empty_permitted'] = True
+    defaults.update(kwargs)
+    form = self.form(**defaults)
+    self.add_fields(form, i)
+    return form
