460862c4e45e1348d20a1d8ee772cd3bc8be0fe8
Simon Liedtke <simon.liedtke@gmail.com>
2019-05-16 16:26:12 +0200

    Fixed user query

--- grid/forms/deal_action_comment_form.py
+++ grid/forms/deal_action_comment_form.py
@@ -63,7 +63,7 @@ class DealActionCommentForm(BaseForm):
 
     def __init__(self, *args, **kwargs):
         super(DealActionCommentForm, self).__init__(*args, **kwargs)
-        self.fields['assign_to_user'].queryset = get_user_model().objects.filter(
+        self.fields['assign_to_user'].queryset = get_user_model().objects.filter(is_active=True,
             groups__name__in=("Editors", "Administrators")).order_by("first_name", "last_name")
 
     def get_attributes(self, request=None):
