fc7a22c728081df7667e20ca661ee4ce0aefc0fd
Simon Liedtke <simon.liedtke@gmail.com>
2019-08-09 09:32:41 +0200

    Added missing settings

--- config/settings/base.py
+++ config/settings/base.py
@@ -231,7 +231,7 @@ ACCOUNT_ACTIVATION_DAYS = 7
 
 WKHTMLTOPDF_CMD = env('DJANGO_WKHTMLTOPDF_CMD', default=None)
 
-ALLOWED_HOSTS = env('DJANGO_ALLOWED_HOSTS', default=[])
+ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['127.0.0.1', 'localhost'])
 
 # TODO: Remove this settings from code
 CONVERT_DB = False
