946fb7594c7808e0da0ce5c36cad13a1156f4493
Andreas Nüßlein <andreas@nuessle.in>
2019-08-30 19:39:10 +0200

    Forgot raven in dependencies

--- config/settings/base.py
+++ config/settings/base.py
@@ -35,7 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    # 'django.contrib.staticfiles',
+    'django.contrib.staticfiles',
     'django.contrib.humanize',
     'django.contrib.postgres',
     'wagtail_modeltranslation',
