74f9c2df227405fb2e19fc8822bff847d866a131
Andreas Nüßlein <andreas@nuessle.in>
2020-02-17 13:40:10 +0100

    Use pytest - it's way shiny

--- config/settings/ci.py
+++ config/settings/ci.py
@@ -5,14 +5,6 @@ SECRET_KEY = env("DJANGO_SECRET_KEY", default="CHANGEME")
 
 INTERNAL_IPS = ("127.0.0.1",)
 
-INSTALLED_APPS = [
-    "coverage",
-    "test_without_migrations",
-    # 'django_nose',
-    # 'debug_toolbar',
-    # 'template_timings_panel',
-] + INSTALLED_APPS
-
 # Recaptcha spam protection for comments
 # https://developers.google.com/recaptcha/docs/faq
 RECAPTCHA_USE_SSL = True
@@ -20,15 +12,6 @@ NOCAPTCHA = True
 RECAPTCHA_PUBLIC_KEY = "6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI"
 RECAPTCHA_PRIVATE_KEY = "6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe"
 SILENCED_SYSTEM_CHECKS = ["captcha.recaptcha_test_key_error"]
-
-# Use nose to run all tests
-# TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
-
-# Tell nose to measure coverage on the 'foo' and 'bar' apps
-# NOSE_ARGS = [
-#    '--with-coverage',
-#    '--cover-package=api,charts,editor,feeds,grid,landmatrix,map,wagtailcms'
-# ]
 
 BROKER_URL = "redis://redis:6379/0"
 CELERY_REDIS_BACKEND = BROKER_URL
