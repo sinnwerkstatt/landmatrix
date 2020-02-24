1aab520880248221b93fe5175acc94fcd0df3fab
Andreas Nüßlein <andreas@nuessle.in>
2020-02-24 15:46:06 +0100

    Re-enable ES updates on deal-view save

--- apps/grid/views/deal.py
+++ apps/grid/views/deal.py
@@ -132,7 +132,7 @@ class DealBaseView(TemplateView):
                 hactivity.fully_updated = self.get_fully_updated(form)
             else:  # pragma: no cover
                 hactivity.fully_updated = False
-            hactivity.save(update_elasticsearch=False)
+            hactivity.save()
             self.create_involvement(hactivity, investor_form)
 
             if not is_admin:
