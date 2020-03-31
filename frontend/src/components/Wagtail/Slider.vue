3dc1815e5855a5e9860e30e8ddf8c5218563a452
Andreas Nüßlein <andreas@nuessle.in>
2020-03-31 20:45:57 +0200

    Docs update for ES5 and 7, Vue-Slider-improvement

--- frontend/src/components/Wagtail/Slider.vue
+++ frontend/src/components/Wagtail/Slider.vue
@@ -1,17 +1,19 @@
 <template>
-  <b-carousel
-    :id="value.id"
-    :interval="3000"
-    controls
-    indicators
-    style="width: 100%; height: 100%;"
-  >
-    <b-carousel-slide v-for="image in value.images" :key="image.id">
-      <template v-slot:img>
-        <wagtail-image :value="image" />
-      </template>
-    </b-carousel-slide>
-  </b-carousel>
+  <div class="slider">
+    <b-carousel
+        :id="value.id"
+        :interval="3000"
+        controls
+        indicators
+        style="width: 100%; height: 100%;"
+    >
+      <b-carousel-slide v-for="image in value.images" :key="image.id">
+        <template v-slot:img>
+          <wagtail-image :value="image"/>
+        </template>
+      </b-carousel-slide>
+    </b-carousel>
+  </div>
 </template>
 
 <script>
@@ -20,4 +22,9 @@
   };
 </script>
 
-<style scoped></style>
+<style>
+  .slider {
+    max-width: 600px;
+    max-height: 800px;
+  }
+</style>
--- tmp_gnd/gnd_elastic.sh
+++ tmp_gnd/gnd_elastic.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-podman run --name es_seven -p 9201:9200 -e "discovery.type=single-node" -e "ES_JAVA_OPTS=-Xmx1g -Xms1g" elasticsearch:7.6.1
-podman start -a es_seven
+# Get Elastic 5 in a container (in userspace with podman)
+podman pull docker.elastic.co/elasticsearch/elasticsearch:5.6.16
+podman run --name es_five -p 9205:9200  -e "discovery.type=single-node" -e "xpack.security.enabled=false" -e "ES_JAVA_OPTS=-Xmx1g -Xms1g" elasticsearch:5.6.16
 
-#podman stop es_seven
+podman start -a es_five
+
+# Get Elastic 7 in a container (in userspace with podman)
+podman run --name es_seven -p 9207:9200 -e "discovery.type=single-node" -e "ES_JAVA_OPTS=-Xmx1g -Xms1g" elasticsearch:7.6.1
+
+podman start -a es_seven
