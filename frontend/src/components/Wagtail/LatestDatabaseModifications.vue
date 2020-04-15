180cab03252e37e125da1580285fd083a705bff5
Andreas Nüßlein <andreas@nuessle.in>
2020-04-15 21:30:47 +0200

    Frontend improved Twitter and LastDBMods

--- frontend/src/components/Wagtail/LatestDatabaseModifications.vue
+++ frontend/src/components/Wagtail/LatestDatabaseModifications.vue
@@ -1,11 +1,59 @@
 <template>
-  <div>
-    {{value}}
-  </div>
+  <table
+    class="table table-striped"
+    id="latest-database-modifications"
+    v-if="modifications"
+  >
+    <thead>
+      <tr>
+        <th>Deal #</th>
+        <th>Date</th>
+        <th>Status</th>
+        <th>Target country</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="deal in modifications" :key="deal.id">
+        <td>
+          <router-link
+            :to="{ name: 'deal_detail', params: { deal_id: deal.id } }"
+            class="label label-default"
+          >
+            {{ deal.id }}</router-link
+          >
+        </td>
+        <td>{{ deal.timestamp }}</td>
+        <td>{{deal.status}}</td>
+        <td>{{ deal.country }}</td>
+      </tr>
+    </tbody>
+  </table>
 </template>
 
 <script>
+  import axios from "axios";
+
   export default {
     props: ["value"],
+    data: function () {
+      return {
+        modifications: [],
+      };
+    },
+    created() {
+      let query = `{ deals(sort:"-timestamp", limit: ${this.value.limit})
+      { id target_country { name } status timestamp }
+      }`;
+      axios.post("/graphql/", { query: query }).then((response) => {
+        this.modifications = response.data.data.deals.map((d) => {
+          return {
+            id: d.id,
+            timestamp: d.timestamp.split("T")[0],
+            country: d.target_country.name,
+            status: "TODO",
+          };
+        });
+      });
+    },
   };
 </script>
--- frontend/src/components/Wagtail/Twitter.vue
+++ frontend/src/components/Wagtail/Twitter.vue
@@ -1,11 +1,74 @@
 <template>
-  <div>twitter
-    {{value}}
+  <div class="widget-twitter-timeline">
+    <div
+      v-if="value.timeline"
+      v-for="status in value.timeline"
+      class="twitter-timeline-update my-3"
+    >
+      <div class="twitter-timeline-meta">
+        <a
+          :href="`https://twitter.com/${status.screen_name}`"
+          target="_blank"
+          class="twitter-timeline-username"
+        >
+          {{ status.name }}
+        </a>
+        ·
+        <a
+          :href="`https://twitter.com/${status.screen_name}`"
+          target="_blank"
+          class="twitter-timeline-screenname"
+        >
+          @{{ status.screen_name }}
+        </a>
+        ·
+        <a
+          target="_blank"
+          :href="`https://twitter.com/statuses/${status.id_str}`"
+          class="twitter-timeline-time"
+        >
+          {{ dayjs(status.created_at).format("MMM. DD, YYYY, H:mm") }}
+        </a>
+      </div>
+      <div class="twitter-timeline-text" v-html="status.text" />
+    </div>
+    <div v-else class="twitter-timeline-empty">
+      Feed currently not available.
+    </div>
+
+    <a
+      :href="`https://twitter.com/${value.username}`"
+      target="_blank"
+      class="btn tweets-by-btn"
+      >Tweets by @{{ value.username }}</a
+    >
   </div>
 </template>
 
 <script>
+  import dayjs from "dayjs";
+
   export default {
     props: ["value"],
+    methods: {
+      dayjs: dayjs,
+    },
   };
 </script>
+
+<style lang="scss">
+  .twitter-timeline-username {
+    font-weight: bold;
+  }
+
+  .tweets-by-btn {
+    color: #fc941f;
+    border-color: #fc941f;
+    border-width: 2px;
+    background: #fcfcfc;
+    font-weight: bold;
+    /* opacity: 0.5; */
+    box-shadow: none;
+    text-shadow: none;
+  }
+</style>
