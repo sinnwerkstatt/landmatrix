a3f5a633377d39e15ff833962f5beadff946a554
Ralph Delfs <ralph.delfs@sinnwerkstatt.com>
2020-11-25 21:18:47 +0100

    Landing Page: Static Map

--- frontend/src/components/QuasiStaticMap.vue
+++ frontend/src/components/QuasiStaticMap.vue
@@ -1,45 +1,157 @@
 <template>
-  <div class="container">
-    <div style="width: 100%; height: 200px; position: relative;">
-      <div
-        style="position: absolute; width: 100%; height: 100%; z-index: 100000;"
-      ></div>
-      <BigMap
-        :options="{
-          zoomControl: false,
-          dragging: false,
-          doubleClickZoom: false,
-          boxZoom: false,
-        }"
-        :center="[12, 30]"
-        :containerStyle="{ 'min-height': '100%', height: '100%' }"
-        :hideLayerSwitcher="true"
-        @ready="bigMapReady"
-      ></BigMap>
+  <div class="static-map" @click="goToGlobalMap">
+    <div class="shield">
+      <span class="hover-text">Go to map</span>
     </div>
+    <BigMap
+      :options="{
+        zoomControl: false,
+        dragging: false,
+        doubleClickZoom: false,
+        boxZoom: false,
+      }"
+      :center="[12, 30]"
+      :containerStyle="{ 'min-height': '100%', height: '100%' }"
+      :hideLayerSwitcher="true"
+      @ready="bigMapReady"
+    ></BigMap>
   </div>
 </template>
 <script>
   import BigMap from "./BigMap";
+
+  import { groupBy } from "lodash";
+
   export default {
     name: "QuasiStaticMap",
     components: { BigMap },
-    props: ["country_id", "region_id"],
+    props: ["country_id", "region_id", "deals"],
+    data() {
+      return {
+        map: null,
+        roc: null
+      };
+    },
+    computed: {
+      markers() {
+        let markers_list = [];
+        for (let deal of this.deals) {
+          for (let loc of deal.locations) {
+            if (loc.point) {
+              let marker = new L.marker([loc.point.lat, loc.point.lng]);
+              if (deal.country) {
+                marker.country_id = deal.country.id;
+              }
+              markers_list.push(marker);
+            }
+          }
+        }
+        return markers_list;
+      }
+    },
     methods: {
       bigMapReady(map) {
-        let type = "region";
-        let id = this.region_id;
-        if (this.country_id) {
-          type = "country";
-          id = this.country_id;
-        }
-        let roc = this.$store.getters.getCountryOrRegion({ type: type, id: id });
-        console.log("x", roc);
         map.fitBounds([
-          [roc.point_lat_min, roc.point_lon_min],
-          [roc.point_lat_max, roc.point_lon_max],
+          [this.roc.point_lat_min, this.roc.point_lon_min],
+          [this.roc.point_lat_max, this.roc.point_lon_max]
         ]);
+        this.map = map;
       },
+      refreshMap() {
+        // group by countries (for region pages)
+        Object.entries(groupBy(this.markers, (mark) => mark.country_id)).forEach(
+          ([key, val]) => {
+            let mcluster = L.markerClusterGroup({
+              chunkedLoading: true
+            });
+            val.forEach((mark) => mcluster.addLayer(mark));
+            this.map.addLayer(mcluster);
+          }
+        );
+      },
+      goToGlobalMap() {
+        if (this.country_id) {
+          this.$store.dispatch("setFilter", {
+            filter: "country_id",
+            value: this.country_id
+          });
+        } else {
+          this.$store.dispatch("setFilter", {
+            filter: "region_id",
+            value: this.region_id
+          });
+        }
+        this.$router.push({ name: "map" });
+      }
     },
+    watch: {
+      deals() {
+        this.refreshMap();
+      }
+    },
+    created() {
+      let type = "region";
+      let id = this.region_id;
+      if (this.country_id) {
+        type = "country";
+        id = this.country_id;
+      }
+      this.roc = this.$store.getters.getCountryOrRegion({ type: type, id: id });
+    }
   };
 </script>
+<style lang="scss">
+  @import "../scss/colors";
+
+  .static-map {
+    width: 100%;
+    min-height: 300px;
+    height: 30vh;
+    position: relative;
+    border: 1px solid $lm_orange;
+    border-radius: 3px;
+    box-shadow: 3px 3px 5px rgba(black, 0.3);
+
+    &:hover {
+      cursor: pointer;
+      box-shadow: 5px 5px 5px rgba(black, 0.3);
+      border-color: rgba($lm_orange, 0.7)
+    }
+
+    .shield {
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      z-index: 100000;
+      background-color: transparent;
+      display: flex;
+
+      &:before {
+        content: '';
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        background-color: rgba($lm_orange, 0.3);
+      }
+
+      .hover-text {
+        display: none;
+        color: white;
+        font-weight: bold;
+        font-size: 4rem;
+        align-self: center;
+        text-align: center;
+        width: 100%;
+        z-index: 1;
+      }
+
+      &:hover {
+        background-color: rgba($lm_orange, 0.5);
+
+        .hover-text {
+          display: block;
+        }
+      }
+    }
+  }
+</style>
--- frontend/src/views/Data/query.js
+++ frontend/src/views/Data/query.js
@@ -1,37 +1,39 @@
 import gql from "graphql-tag";
 
-export const data_deal_query = {
-  query: gql`
-    query Deals($limit: Int!, $subset: Subset, $filters: [Filter]) {
-      deals(limit: $limit, subset: $subset, filters: $filters) {
+export const data_deal_query_gql = gql`
+  query Deals($limit: Int!, $subset: Subset, $filters: [Filter]) {
+    deals(limit: $limit, subset: $subset, filters: $filters) {
+      id
+      deal_size
+      country {
         id
-        deal_size
-        country {
-          id
-          fk_region {
-            id
-          }
-        }
-        current_intention_of_investment
-        current_negotiation_status
-        current_implementation_status
-        locations {
-          id
-          point
-          level_of_accuracy
-        }
-        fully_updated_at # for listing
-        operating_company {
-          # for map pin popover & listing
-          id
-        }
-        top_investors {
-          # for listing
+        fk_region {
           id
         }
       }
+      current_intention_of_investment
+      current_negotiation_status
+      current_implementation_status
+      locations {
+        id
+        point
+        level_of_accuracy
+      }
+      fully_updated_at # for listing
+      operating_company {
+        # for map pin popover & listing
+        id
+      }
+      top_investors {
+        # for listing
+        id
+      }
     }
-  `,
+  }
+`;
+
+export const data_deal_query = {
+  query: data_deal_query_gql,
   variables() {
     let user = this.$store.state.page.user;
     return {
