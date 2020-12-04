19d34c6da3b926f57f3b705e90e5703b26ecf983
Andreas Nüßlein <andreas@nuessle.in>
2020-12-04 11:39:18 +0000

    Put urls.py switch for new interface

--- config/settings/base.py
+++ config/settings/base.py
@@ -196,7 +196,8 @@ print(f"Using elasticsearch index {ELASTICSEARCH_INDEX_NAME}")
 sys.stdout.flush()
 
 # GreenNewDeal
-OLD_ELASTIC = env("OLD_ELASTIC", default=True)
+OLD_ELASTIC = env.bool("OLD_ELASTIC", default=True)
+NEW_ROUTES = env.bool("NEW_ROUTES", default=True)
 
 # CELERY SETTINGS
 BROKER_URL = "redis://localhost:6379/0"
--- frontend/src/router.js
+++ frontend/src/router.js
@@ -20,7 +20,7 @@ Vue.use(Router);
 
 const router = new Router({
   mode: "history",
-  base: "/", //process.env.BASE_URL,
+  base: process.env.NEW_ROUTES === "False" ? "/newdeal/" : "/",
   routes: [
     {
       path: "/map/",
