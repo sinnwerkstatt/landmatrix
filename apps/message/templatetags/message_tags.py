8741aadf57930b5a1c4fabd64f77f76eab09d443
Ralph Delfs <ralph.delfs@sinnwerkstatt.com>
2020-12-13 20:33:21 +0100

    Style/Hide messages on old templates

--- apps/message/templatetags/message_tags.py
+++ apps/message/templatetags/message_tags.py
@@ -1,10 +1,20 @@
 from django import template
+from django.utils import timezone
 
 from apps.message.models import Message
 
 register = template.Library()
 
 
-@register.simple_tag
-def custom_messages():
-    return Message.objects.filter(is_active=True)
+@register.simple_tag(takes_context=True)
+def custom_messages(context):
+    request = context.request
+    base_url = request.scheme + "://" + request.META['HTTP_HOST']
+    is_internal_referer = 'HTTP_REFERER' in request.META and request.META[
+        'HTTP_REFERER'].startswith(base_url)
+    if is_internal_referer:
+        return []
+    else:
+        return Message.objects.filter(is_active=True).exclude(
+            expires_at__lte=timezone.localdate()
+        )
--- apps/message/views.py
+++ apps/message/views.py
@@ -5,10 +5,17 @@ from apps.message.models import Message
 
 
 def messages_json(request):
-    msgs = [
-        msg.to_dict()
-        for msg in Message.objects.filter(is_active=True).exclude(
-            expires_at__lte=timezone.localdate()
-        )
-    ]
+    import ipdb; ipdb.set_trace()
+    base_url = request.scheme + "://" + request.META['HTTP_HOST']
+    is_internal_referer = 'HTTP_REFERER' in request.META and request.META[
+        'HTTP_REFERER'].startswith(base_url)
+    if is_internal_referer:
+        msgs = []
+    else:
+        msgs = [
+            msg.to_dict()
+            for msg in Message.objects.filter(is_active=True).exclude(
+                expires_at__lte=timezone.localdate()
+            )
+        ]
     return JsonResponse({"messages": msgs})
