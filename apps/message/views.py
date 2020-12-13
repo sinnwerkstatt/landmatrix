90fdb0cfe60a6a09268b995889c5ae05e15a8ccf
Ralph Delfs <ralph.delfs@sinnwerkstatt.com>
2020-12-13 20:36:43 +0100

    Upsidaysi

--- apps/message/views.py
+++ apps/message/views.py
@@ -5,20 +5,10 @@ from apps.message.models import Message
 
 
 def messages_json(request):
-    import ipdb
-
-    ipdb.set_trace()
-    base_url = request.scheme + "://" + request.META["HTTP_HOST"]
-    is_internal_referer = "HTTP_REFERER" in request.META and request.META[
-        "HTTP_REFERER"
-    ].startswith(base_url)
-    if is_internal_referer:
-        msgs = []
-    else:
-        msgs = [
-            msg.to_dict()
-            for msg in Message.objects.filter(is_active=True).exclude(
-                expires_at__lte=timezone.localdate()
-            )
-        ]
+    msgs = [
+        msg.to_dict()
+        for msg in Message.objects.filter(is_active=True).exclude(
+            expires_at__lte=timezone.localdate()
+        )
+    ]
     return JsonResponse({"messages": msgs})
