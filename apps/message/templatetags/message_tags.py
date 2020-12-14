11254e3ea4d92c2f886e5db95a8782aee0b6bc64
Ralph Delfs <ralph.delfs@sinnwerkstatt.com>
2020-12-14 09:10:08 +0100

    Fix tests failing

--- apps/message/templatetags/message_tags.py
+++ apps/message/templatetags/message_tags.py
@@ -9,13 +9,15 @@ register = template.Library()
 @register.simple_tag(takes_context=True)
 def custom_messages(context):
     request = context.request
-    base_url = request.scheme + "://" + request.META["HTTP_HOST"]
-    is_internal_referer = "HTTP_REFERER" in request.META and request.META[
-        "HTTP_REFERER"
-    ].startswith(base_url)
-    if is_internal_referer:
-        return []
-    else:
-        return Message.objects.filter(is_active=True).exclude(
-            expires_at__lte=timezone.localdate()
-        )
+
+    if 'HTTP_HOST' in request.META:
+        base_url = request.scheme + "://" + request.META["HTTP_HOST"]
+        is_internal_referer = "HTTP_REFERER" in request.META and request.META[
+            "HTTP_REFERER"
+        ].startswith(base_url)
+        if is_internal_referer:
+            return []
+
+    return Message.objects.filter(is_active=True).exclude(
+        expires_at__lte=timezone.localdate()
+    )
