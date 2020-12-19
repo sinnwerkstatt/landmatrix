46e8f7dfeb8c11b8cb47d3cf5c98d27035c6323f
Andreas Nüßlein <andreas@nuessle.in>
2020-12-19 23:28:25 +0000

    Only show Deal/Inv edit button for logged in users

--- frontend/src/views/Investor/Detail.vue
+++ frontend/src/views/Investor/Detail.vue
@@ -8,7 +8,11 @@
         <h1>Investor #{{ investor.id }}</h1>
       </div>
       <div class="col-sm-7 col-md-9 panel-container">
-        <a :href="`/legacy/investor/edit/${investor.id}/`" target="_blank">
+        <a
+          v-if="$store.getters.userAuthenticated"
+          :href="`/legacy/investor/edit/${investor.id}/`"
+          target="_blank"
+        >
           <i class="fas fa-edit"></i> Edit
         </a>
         <div class="meta-panel">
