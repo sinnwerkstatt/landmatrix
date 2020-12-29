a05727bf7132777434e13ffea1c02d63810a1830
Andreas Nüßlein <andreas@nuessle.in>
2020-12-29 09:39:26 +0000

    Fix tests

---
+++ tmp_gnd/gnd_elastic.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Get Elastic 5 in a container (in userspace with podman)
+podman pull docker.elastic.co/elasticsearch/elasticsearch:5.6.16
+podman run --name es_five -p 9205:9200  -e "discovery.type=single-node" -e "xpack.security.enabled=false" -e "ES_JAVA_OPTS=-Xmx1g -Xms1g" elast
+
+podman start -a es_five
+
+# Get Elastic 7 in a container (in userspace with podman)
+podman run --name es_seven -p 9200:9200 -e "discovery.type=single-node" -e "ES_JAVA_OPTS=-Xmx1g -Xms1g" elasticsearch:7.7.0
+
+podman start -a es_seven
