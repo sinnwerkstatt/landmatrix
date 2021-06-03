6d9ea0c818d912b8dd057addbc69fe1bf60eb9ad
Andreas Nüßlein <andreas@nuessle.in>
2021-06-03 22:43:04 +0100

    enhance linebreaks filter

--- frontend/src/utils/filters.js
+++ frontend/src/utils/filters.js
@@ -1,5 +1,5 @@
 export function linebreaks(value) {
-  let paras = value.split(/(\n{2,})/);
+  let paras = value.split(/\n{2,}/);
   paras = paras.map((p) => `<p>${p.replace("\n", "<br>")}</p>`);
   return paras.join("\n");
 }
