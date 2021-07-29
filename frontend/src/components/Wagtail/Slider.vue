4654804d3ae9467759ee95c6c2aa205128678579
Andreas Nüßlein <andreas@nuessle.in>
2021-07-29 15:02:22 +0200

    homepage slider: fix margin

--- frontend/src/components/Wagtail/Slider.vue
+++ frontend/src/components/Wagtail/Slider.vue
@@ -5,7 +5,7 @@
       :interval="3000"
       controls
       indicators
-      style="width: 100%; height: 100%;"
+      style="width: 100%; height: 100%"
     >
       <b-carousel-slide v-for="image in value.images" :key="image.id">
         <template #img>
@@ -26,6 +26,7 @@
 
 <style>
   .slider {
+    margin: 0 auto;
     max-width: 600px;
     max-height: 800px;
   }
