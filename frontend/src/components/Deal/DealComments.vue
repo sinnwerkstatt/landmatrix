925a8318901a46520e00de04f6b290f6df6f89b8
Andreas Nüßlein <andreas@nuessle.in>
2021-08-06 16:41:29 +0100

    try hCaptcha

--- frontend/src/components/Deal/DealComments.vue
+++ frontend/src/components/Deal/DealComments.vue
@@ -16,6 +16,30 @@
         </li>
       </ul>
     </div>
+    <form>
+      <h4>{{ $t("Add a comment") }}</h4>
+      <div class="add-a-comment-header">
+        <input :placeholder="$t('Name')" required />
+        <input :placeholder="$t('Email')" type="email" required />
+      </div>
+      <input :placeholder="$t('Comment title')" />
+      <textarea cols="50" rows="3" :placeholder="$t('Comment')" required></textarea>
+      <label>
+        <input required type="checkbox" style="width: inherit" />
+        {{ $t("I've read and agree to the") }}
+        <a href="/about/data-policy/" target="_blank">{{ $t("Data policy") }}</a
+        >.
+      </label>
+      <vue-hcaptcha
+        sitekey="ee77472a-0484-49a2-9e80-bcb765c774b1"
+        re-captcha-compat="false"
+      />
+      <div>
+        <button disabled type="submit" class="btn btn-primary">
+          {{ $t("Submit") }}
+        </button>
+      </div>
+    </form>
   </div>
 </template>
 
@@ -24,11 +48,13 @@
 
   import utc from "dayjs/plugin/utc";
   import timezone from "dayjs/plugin/timezone";
+  import VueHcaptcha from "@hcaptcha/vue-hcaptcha";
 
   dayjs.extend(utc);
   dayjs.extend(timezone);
 
   export default {
+    components: { VueHcaptcha },
     props: { comments: { type: Array, required: true } },
     methods: { dayjs },
   };
@@ -50,5 +76,28 @@
     font-weight: normal;
     font-size: 0.7125rem;
     padding-bottom: 0.5rem;
+  }
+  h4 {
+    font-size: 1.125rem;
+  }
+  .add-a-comment-header {
+    display: flex;
+    width: 100%;
+    flex-flow: column;
+    gap: 1em;
+
+    @media screen and (min-width: 800px) {
+      flex-flow: row nowrap;
+    }
+  }
+  input,
+  textarea {
+    width: 100%;
+    margin: 0.5em 0;
+    //&[required] {
+    //  font-weight: bold;
+    //  color: red;
+    //  display: inline;
+    //}
   }
 </style>
