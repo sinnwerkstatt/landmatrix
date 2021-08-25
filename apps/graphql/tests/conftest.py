45e11907b5f5041353e8f82e04b72c2df4bab842
Andreas Nüßlein <andreas@nuessle.in>
2021-08-25 21:42:10 +0100

    change pytest fixture scope

--- apps/graphql/tests/conftest.py
+++ apps/graphql/tests/conftest.py
@@ -6,13 +6,13 @@ from django.core.management import call_command
 User = get_user_model()
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="module")
 def django_db_setup(django_db_setup, django_db_blocker):
     with django_db_blocker.unblock():
         call_command("loaddata", "apps/landmatrix/fixtures/countries_and_regions.json")
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="module")
 def django_db_setup(django_db_setup, django_db_blocker):
     with django_db_blocker.unblock():
         for gname in ["Reporters", "Editors", "Administrators"]:
