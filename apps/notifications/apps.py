45bc6505b1a46c287b8cd0ee631e914757459f42
Andreas Nüßlein <andreas@nuessle.in>
2022-02-24 14:05:52 +0000

    thin out apps.notifications

--- apps/notifications/apps.py
+++ apps/notifications/apps.py
@@ -4,7 +4,3 @@ from django.apps import AppConfig
 class NotificationsConfig(AppConfig):
     name = "apps.notifications"
     verbose_name = "Notifications"
-
-    def ready(self):
-        # noinspection PyUnresolvedReferences
-        from apps.notifications import signals
