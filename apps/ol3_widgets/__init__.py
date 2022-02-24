cd08c4241b15164bdfba44f9753f543218397b23
Andreas Nüßlein <andreas@nuessle.in>
2022-02-24 14:05:58 +0000

    remove ol3_widget

--- apps/ol3_widgets/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Openlayers 3 compatible GIS Form widgets, taken from
-https://github.com/claudep/django
-
-TODO: Move this into the map app. It's now very intertwined with everything
-else.
-"""
