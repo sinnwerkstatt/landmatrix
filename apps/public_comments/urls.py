530729b686bdad63fa66cc1474e6cafc7e3cdc2b
Andreas Nüßlein <andreas@nuessle.in>
2022-02-24 14:06:05 +0000

    remove public_comments

--- apps/public_comments/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import include
-from django.urls import path
-
-from .views import EditCommentView
-
-urlpatterns = [
-    path("", include("django_comments.urls")),
-    path("edit/<int:comment_id>/", EditCommentView.as_view(), name="comments-edit"),
-]
