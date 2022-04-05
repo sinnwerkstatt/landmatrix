cd9d164dfb736ae83c857af1276ad1d1d899ee25
Charlotte <83395926+Schl0dd1@users.noreply.github.com>
2022-04-05 12:54:24 +0200

    add BooleanField

---
+++ newfront/src/components/Fields/Display/BooleanField.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import type { FormField } from "$components/Fields/fields";
+  import { _ } from "svelte-i18n";
+
+  export let formfield: FormField;
+  export let value: boolean;
+  export let model: string;
+</script>
+
+<div class="boolean_field">
+  {#if value}
+    {$_("Yes")}
+  {:else if value === false}
+    {$_("No")}
+  {:else}NULL
+  {/if}
+</div>
