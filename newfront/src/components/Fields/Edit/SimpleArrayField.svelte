a3b89cafb2b83b18d58ae640cd46d282c7dd365e
Andreas Nüßlein <andreas@nuessle.in>
2022-05-20 17:12:19 +0100

    improve SimpleArrayField.svelte a bit

--- newfront/src/components/Fields/Edit/SimpleArrayField.svelte
+++ newfront/src/components/Fields/Edit/SimpleArrayField.svelte
@@ -5,42 +5,31 @@
   export let formfield: FormField;
   export let value: Array<string | number> = [];
 
-  // computed: {
-  //   val: {
-  //     get() {
-  //       let retval = this.value || [];
-  //       if (!this.formfield.choices) {
-  //         retval = retval.join("\n");
-  //       }
-  //       return retval;
-  //     },
+  $: value = value === null ? "" : value;
   //     set(v) {
-  //       if (this.formfield.choices) {
   //         // deal with weird "[null]" array
   //         v = v.length === 1 && v[0] === null ? [] : v;
   //         this.$emit("input", v);
-  //       } else {
-  //         this.$emit("input", v.split("\n"));
-  //       }
   //     },
-  //   },
-  // },
 </script>
 
 <div class="simplearray_field">
   {#if formfield.choices}
     <div>
       <select bind:value multiple class="inpt">
-        {#each Object.entries(formfield.choices) as [value, label]}
-          <option {value}>{label}</option>
+        {#each Object.entries(formfield.choices) as [v, label]}
+          <option value={v}>{label}</option>
         {/each}
       </select>
     </div>
   {:else}
-    <!--this is not transformed yet -->
-    <span class="italic text-red-600">HUHUHUHUHU!!!!</span>
-    <div>
-      <textarea v-model="val" class="form-control" />
+    <div class="flex flex-col">
+      <textarea
+        value={value ? value.join("\n") : ""}
+        on:input={(x) => (value = x.target.value.split("\n"))}
+        class="inpt"
+        rows="5"
+      />
       <small class="form-text text-muted">
         {$_("Put each value on a new line, i.e. press enter between each name")}
       </small>
