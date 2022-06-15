2116c3c05536cdd512fb4d869107cad1f43aa8de
Charlotte <83395926+Schl0dd1@users.noreply.github.com>
2022-06-15 11:21:06 +0200

    add link to investor

--- newfront/src/components/Fields/Edit/InvestorForeignKey.svelte
+++ newfront/src/components/Fields/Edit/InvestorForeignKey.svelte
@@ -3,6 +3,7 @@
   import { _ } from "svelte-i18n";
   import Select from "svelte-select";
   import { client } from "$lib/apolloClient";
+  import EditField from "../EditField.svelte";
 
   export let value: number;
 
@@ -31,6 +32,7 @@
 <div class="currency_foreignkey_field">
   {#if investors}
     <!--{JSON.stringify(investors)}-->
+    <!--{JSON.stringify(value)}-->
     <Select
       items={investors}
       {value}
@@ -42,5 +44,18 @@
       getSelectionLabel={(o) => `${o.name} (#${o.id})`}
       showChevron
     />
+  {/if}
+  {#if value}
+    <div class="container p-2">
+      <!--      <a href={`../../investor/${value.id}`} class=""-->
+      <!--        ><button class="rounded bg-pelorous py-1 px-2 mr-1 text-white font-bold "-->
+      <!--          >{value.id}</button-->
+      <!--        >-->
+      <!--        <span class="text-black">{value.name}</span></a-->
+      <!--      >-->
+      <a href={`../../investor/${value.id}`} class="">
+        Show details for #{value.id} {value.name}...</a
+      >
+    </div>
   {/if}
 </div>
