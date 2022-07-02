bb11f41a5b159050a56641b6757e1eabf8ec44e7
Andreas Nüßlein <andreas@nuessle.in>
2022-07-02 11:32:48 +0100

    add nanoIDs to current drafts too

--- apps/landmatrix/management/commands/add_nanoids.py
+++ apps/landmatrix/management/commands/add_nanoids.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand
 
-from apps.landmatrix.models import Deal
+from apps.landmatrix.models import Deal, DealVersion
 
 from nanoid import generate
 
@@ -8,19 +8,111 @@ from nanoid import generate
 class Command(BaseCommand):
     def handle(self, *args, **options):
         for deal in Deal.objects.all().order_by("id"):
-            print(deal.id)
-            for x in deal.locations:
-                if not x.get("id") or isinstance(x["id"], int):
-                    x["old_id"] = x.get("id")
-                    x["id"] = generate(size=8)
+            print(deal.id, end="")
+            updated = False
+
+            # datasources and contracts
             for x in deal.datasources:
                 if not x.get("id") or isinstance(x["id"], int):
+                    updated = True
                     x["old_id"] = x.get("id")
                     x["id"] = generate(size=8)
 
             for x in deal.contracts:
                 if not x.get("id") or isinstance(x["id"], int):
+                    updated = True
                     x["old_id"] = x.get("id")
                     x["id"] = generate(size=8)
+            # #####################################
 
-            deal.save()
+            for x in deal.locations:
+                updated = True
+                if not x.get("id") or isinstance(x["id"], int):
+                    x["old_id"] = x.get("id")
+                    x["id"] = generate(size=8)
+                if x.get("areas"):
+                    for feat in x["areas"].get("features"):
+                        if (
+                            isinstance(feat["properties"]["id"], str)
+                            and len(feat["properties"]["id"]) == 8
+                        ):
+                            continue
+                        # live_id = [
+                        #     y
+                        #     for y in deal.locations
+                        #     if y["old_id"] == feat["properties"]["id"]
+                        # ][0]["id"]
+                        feat["properties"]["id"] = x["id"]
+
+            if updated:
+                print(" - updated", end="")
+                deal.save()
+
+            if deal.current_draft:
+                ddata = deal.current_draft.serialized_data
+                # datasources and contracts
+                # print(json.dumps(ddata["datasources"], indent=2))
+                # print(json.dumps(deal.datasources, indent=2))
+                for x in ddata["datasources"]:
+                    if not x.get("id") or isinstance(x["id"], int):
+                        if not x.get("id"):
+                            x["id"] = generate(size=8)
+                            continue
+                        if x["id"] in [y["id"] for y in deal.datasources]:
+                            print("found ID")
+                            continue
+                        updated = True
+                        try:
+                            live_id = [
+                                y
+                                for y in deal.datasources
+                                if y.get("old_id") == x.get("id")
+                            ][0]["id"]
+                        except IndexError:
+                            live_id = generate(size=8)
+                        x["id"] = live_id
+
+                for x in ddata["contracts"]:
+                    if not x.get("id") or isinstance(x["id"], int):
+                        if not x.get("id"):
+                            x["id"] = generate(size=8)
+                            continue
+                        if x["id"] in [y["id"] for y in deal.contracts]:
+                            continue
+                        updated = True
+                        try:
+                            live_id = [
+                                y
+                                for y in deal.contracts
+                                if y.get("old_id") == x.get("id")
+                            ][0]["id"]
+                        except IndexError:
+                            live_id = generate(size=8)
+                        x["id"] = live_id
+                # #####################################
+
+                for x in ddata["locations"]:
+                    if not x.get("id") or isinstance(x["id"], int):
+                        updated = True
+                        try:
+                            live_id = [
+                                y
+                                for y in deal.locations
+                                if y.get("old_id") == x.get("id")
+                            ][0]["id"]
+                        except IndexError:
+                            live_id = generate(size=8)
+                        x["id"] = live_id
+                    if x.get("areas"):
+                        for feat in x["areas"].get("features"):
+                            if (
+                                isinstance(feat["properties"]["id"], str)
+                                and len(feat["properties"]["id"]) == 8
+                            ):
+                                continue
+                            feat["properties"]["id"] = x["id"]
+                if updated:
+                    print(" - draft updated", end="")
+                    deal.current_draft.save()
+
+            print()
