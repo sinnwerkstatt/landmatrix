d3d1bb62d83e93d4ffbf9dcbd6e0f5acdde2efd1
Andreas Nüßlein <andreas@nuessle.in>
2022-07-22 22:03:34 +0100

    [svelte] checkboxswitch improvements

--- newfront/src/components/LowLevel/CheckboxSwitch.svelte
+++ newfront/src/components/LowLevel/CheckboxSwitch.svelte
@@ -1,61 +1,40 @@
 <script lang="ts">
   export let checked = false;
-  export let label = "label";
+  export let title: string;
 </script>
 
-<div class="flex {$$props.class}">
-  <label class="switch align-middle relative block flex justify-center items-center">
-    <input class="opacity-0 w-0 h-0" type="checkbox" bind:checked on:input on:change />
-    <span
-      class="slider transition-transform delay-300 bg-gray-300 absolute cursor-pointer inset-0"
-    />
-  </label>
-  <p class={checked ? "text-orange" : "text-gray-500"}>{label}</p>
-</div>
+<label class="flex items-center gap-1 mb-1 cursor-pointer {$$props.class}" {title}>
+  <input type="checkbox" bind:checked on:change|preventDefault />
+  <span class={checked ? "text-orange" : "text-gray-500"}><slot /></span>
+</label>
 
 <style>
-  /* The switch - the box around the slider */
-  .switch {
-    font-size: 1rem;
-    width: 2.5em;
-    height: 1.375em;
-    margin: 0 0.3125em 0.3125em 0;
+  input {
+    appearance: none;
+    margin: 0;
+    background-color: #c9c9c9;
+    display: grid;
+    place-content: center;
+    width: 2.25em;
+    height: 1.25em;
+    transition: background-color 100ms ease-in-out;
+    cursor: pointer;
+  }
+  input:checked {
+    background-color: var(--color-lm-orange);
   }
 
-  /* The slider */
-  .slider {
-    --color-highlight: orange;
-    transition: transform 400ms, background-color 400ms;
-  }
-
-  .slider::before {
-    position: absolute;
+  input::before {
     content: "";
-    height: 1.125em;
-    width: 1.125em;
-    left: 0.125em;
-    bottom: 0.125em;
-    background-color: white;
-    transition: transform 100ms;
+    width: 1em;
+    height: 1em;
+    transform: translateX(-50%);
+    transition: 100ms transform ease-in-out;
+    box-shadow: inset 1.15em 1.15em white;
+    cursor: pointer;
   }
 
-  input:focus + .slider {
-    border: 1px solid black;
-  }
-  input:checked + .slider {
-    background-color: var(--color-highlight);
-  }
-
-  input:checked:disabled + .slider {
-    opacity: 0.3;
-    cursor: not-allowed;
-  }
-
-  input:focus + .slider {
-    box-shadow: 0 0 1px var(--color-highlight);
-  }
-
-  input:checked + .slider:before {
-    transform: translateX(1.125em);
+  input:checked::before {
+    transform: translateX(50%);
   }
 </style>
