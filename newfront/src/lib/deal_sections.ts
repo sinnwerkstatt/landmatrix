eb224ba0b0146f931c67caf47b3ff3a9f754686f
Charlotte <83395926+Schl0dd1@users.noreply.github.com>
2022-07-29 16:59:41 +0200

    create investorEditForm (WIP)

--- newfront/src/lib/deal_sections.ts
+++ newfront/src/lib/deal_sections.ts
@@ -7,12 +7,7 @@ export const dealSections: { [key: string]: DealSection[] } = {
   general_info: [
     {
       name: "Land area",
-      fields: [
-        "intended_size",
-        "contract_size",
-        "production_size",
-        "land_area_comment",
-      ],
+      fields: ["intended_size", "contracsize", "production_size", "land_area_comment"],
     },
     {
       name: "Intention of investment",
---
+++ newfront/src/routes/investor/edit/[...IDs].svelte
@@ -0,0 +1,47 @@
+<script context="module" lang="ts">
+  import type { Load } from "@sveltejs/kit";
+  import type { Investor } from "$lib/types/investor";
+  import { investor_gql_query } from "../queries";
+
+  export const load: Load = async ({ params, stuff }) => {
+    if (!stuff.user) return { status: 403, error: "Permission denied" };
+
+    let [investorID, investorVersion] = params.IDs.split("/").map((x) =>
+      x ? +x : undefined
+    );
+    const { data } = await stuff.secureApolloClient.query<{ investor: Investor[] }>({
+      query: investor_gql_query,
+      variables: {
+        id: investorID,
+        version: investorVersion,
+        subset: "UNFILTERED",
+        depth: 0,
+        includeDeals: false,
+      },
+    });
+    if (data.investor === null)
+      return {
+        status: 404,
+        error: investorVersion ? "Deal version not found" : "Deal not found",
+      };
+
+    return {
+      props: {
+        investorID,
+        investorVersion,
+        investor: JSON.parse(JSON.stringify(data.investor)),
+      },
+    };
+  };
+</script>
+
+<script lang="ts">
+  import InvestorEditForm from "$views/InvestorEditForm.svelte";
+
+  export let investor: Investor;
+  export let investorID: number;
+  export let investorVersion: number;
+  console.log(investor);
+</script>
+
+<InvestorEditForm bind:investor bind:investorID bind:investorVersion />
