91107fdcc2df07db543f6098c11b5b1caddd29a2
Charlotte <83395926+Schl0dd1@users.noreply.github.com>
2022-08-10 15:52:04 +0200

    Test: create observatory index and child page (WIP)

---
+++ tests/cms.spec.ts
@@ -0,0 +1,78 @@
+import { expect, test } from "@playwright/test";
+
+test.use({ storageState: "playwright-storageState.json" });
+
+test("basic cms", async ({ context, page }) => {
+  await page.goto("/cms");
+
+  //delete after fixing global setup:
+  await page.locator('[placeholder="Enter your username"]').click();
+  await page.locator('[placeholder="Enter your username"]').fill("shakespeare");
+  await page.locator('[placeholder="Enter password"]').fill("hamlet4eva");
+  await page.locator('button:has-text("Sign in")').click();
+
+  // create observatory index page
+  await page.locator("text=Pages").first().click();
+  await page.locator("text=Land MatrixEnglish").click();
+  await page.locator("[aria-label=\"Add a child page to \\'Land Matrix\\' \"]").click();
+
+  //check if observatory index page already exists:
+  const indexPage = await page.$("text='Observatory index page'");
+  if (!indexPage) {
+    console.log("obs index page already exists");
+    await page.locator("text=Pages").first().click();
+    await page.locator('h3:has-text("Land Matrix")').click();
+    // await page
+    //   .locator("[aria-label=\"View more options for \\'observatory\\'\"]")
+    //   .click();
+    await page.locator("text=observatory").first().click();
+    await page.locator("div.dropdown-toggle").last().click();
+    await page.pause(); //throws an error without this pause
+    await Promise.all([page.waitForNavigation(), page.locator("text=Delete").click()]);
+    await page.pause();
+    await Promise.all([
+      page.waitForNavigation(),
+      await page.locator("text=Yes, delete it").click(),
+    ]);
+    await Promise.all([
+      page.waitForNavigation(),
+      await page
+        .locator("[aria-label=\"Add a child page to \\'Land Matrix\\' \"]")
+        .click(),
+    ]);
+    await page.reload();
+  }
+  await page.locator("text=Pages").first().click();
+  await page.locator("text=Land MatrixEnglish").click();
+  await Promise.all([
+    page.waitForNavigation(),
+    await page
+      .locator("[aria-label=\"Add a child page to \\'Land Matrix\\' \"]")
+      .click(),
+  ]);
+
+  await page.locator("text=Observatory index page").first().click();
+  await page.locator('input[name="title_en"]').fill("observatory");
+  await page.locator('input[name="title_en"]').press("Enter");
+
+  //create observatory child page
+  await page.locator("text=Pages").first().click();
+  await page.locator("a:has-text(\"View child pages of 'Land Matrix'\")").click();
+  await page.locator('h3:has-text("observatory")').click();
+  await page.locator("text=Add child page").click();
+  await page.locator('input[name="title_en"]').fill("global");
+  await page.locator("text=Paragraph").first().click();
+  await page.locator('div[role="textbox"] div').nth(2).click();
+  await page.keyboard.type("test global child page");
+  await page.locator("div.dropdown-toggle").last().click();
+  await page.pause(); //throws an error without this pause
+  await Promise.all([
+    page.waitForNavigation(),
+    page.locator('button:has-text("Publish")').click(),
+  ]);
+
+  await Promise.all([
+    page.waitForNavigation(),
+    page.locator("text=View live").first().click(),
+  ]);
+});
