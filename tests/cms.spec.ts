e9bc8418690e7d77ee5b10aecc24272e5a60939a
Charlotte <83395926+Schl0dd1@users.noreply.github.com>
2022-08-16 14:24:03 +0200

    add test "delete pages created"

--- tests/cms.spec.ts
+++ tests/cms.spec.ts
@@ -15,6 +15,15 @@ async function publishNewPage(page) {
   await page.locator("text=View live").first().click();
 }
 
+async function login(page) {
+  await page.goto("/cms");
+  //delete after fixing global setup:
+  await page.locator('[placeholder="Enter your username"]').click();
+  await page.locator('[placeholder="Enter your username"]').fill("shakespeare");
+  await page.locator('[placeholder="Enter password"]').fill("hamlet4eva");
+  await page.locator('button:has-text("Sign in")').click();
+}
+
 test("basic cms", async ({ context, page }) => {
   await page.goto("/");
   await page.locator("text=Data").click();
@@ -33,39 +42,13 @@ test("basic cms", async ({ context, page }) => {
 });
 
 test("create observatory page", async ({ context, page }) => {
-  await page.goto("/cms");
-  //delete after fixing global setup:
-  await page.locator('[placeholder="Enter your username"]').click();
-  await page.locator('[placeholder="Enter your username"]').fill("shakespeare");
-  await page.locator('[placeholder="Enter password"]').fill("hamlet4eva");
-  await page.locator('button:has-text("Sign in")').click();
+  login(page);
 
   // create observatory index page
   await page.locator("text=Pages").first().click();
   await page.locator("text=Land MatrixEnglish").click();
   await page.locator("[aria-label=\"Add a child page to \\'Land Matrix\\' \"]").click();
 
-  //check if observatory index page already exists:
-  const indexPage = await page.$("text='Observatory index page'");
-  if (!indexPage) {
-    console.log("obs index page already exists");
-    await page.locator("text=Pages").first().click();
-    await page.locator('h3:has-text("Land Matrix")').click();
-    await page.locator("text=observatory").first().click();
-    await page.locator("div.dropdown-toggle").last().click();
-    await Promise.all([page.waitForNavigation(), page.locator("text=Delete").click()]);
-    await Promise.all([
-      page.waitForNavigation(),
-      await page.locator("text=Yes, delete it").click(),
-    ]);
-    await Promise.all([
-      page.waitForNavigation(),
-      await page
-        .locator("[aria-label=\"Add a child page to \\'Land Matrix\\' \"]")
-        .click(),
-    ]);
-    await page.reload();
-  }
   await page.locator("text=Pages").first().click();
   await page.locator("text=Land MatrixEnglish").click();
   await Promise.all([
@@ -98,12 +81,7 @@ test("create observatory page", async ({ context, page }) => {
 });
 
 test("create wagtail page", async ({ context, page }) => {
-  await page.goto("/cms");
-  //delete after fixing global setup:
-  await page.locator('[placeholder="Enter your username"]').click();
-  await page.locator('[placeholder="Enter your username"]').fill("shakespeare");
-  await page.locator('[placeholder="Enter password"]').fill("hamlet4eva");
-  await page.locator('button:has-text("Sign in")').click();
+  login(page);
 
   await page.locator("text=Pages").first().click();
   await page.locator('h3:has-text("Land Matrix")').click();
@@ -120,4 +98,28 @@ test("create wagtail page", async ({ context, page }) => {
   ]);
 
   await expect(page.locator("text=Test Title")).toBeVisible();
+});
+
+test("delete pages created", async ({ context, page }) => {
+  login(page);
+  //check if observatory index page already exists:
+  await page.locator("text=Pages").first().click();
+  await page.locator('h3:has-text("Land Matrix")').click();
+  await page.locator("text=observatory").first().click();
+  await page.locator("div.dropdown-toggle").last().click();
+  await Promise.all([page.waitForNavigation(), page.locator("text=Delete").click()]);
+  await Promise.all([
+    page.waitForNavigation(),
+    await page.locator("text=Yes, delete it").click(),
+  ]);
+
+  await page.locator("text=Pages").first().click();
+  await page.locator('h3:has-text("Land Matrix")').click();
+  await page.locator("text=Contribute").first().click();
+  await page.locator("div.dropdown-toggle").last().click();
+  await Promise.all([page.waitForNavigation(), page.locator("text=Delete").click()]);
+  await Promise.all([
+    page.waitForNavigation(),
+    await page.locator("text=Yes, delete it").click(),
+  ]);
 });
