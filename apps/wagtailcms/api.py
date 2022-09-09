738ea5deff7b7d177f5b0dfa310f85db90eabb53
Andreas Nüßlein <andreas@nuessle.in>
2022-09-09 16:29:28 +0100

    add wagtail headless preview

--- apps/wagtailcms/api.py
+++ apps/wagtailcms/api.py
@@ -1,10 +1,54 @@
-from wagtail.api.v2.views import PagesAPIViewSet
+from django.contrib.contenttypes.models import ContentType
+from rest_framework.response import Response
 from wagtail.api.v2.router import WagtailAPIRouter
-from wagtail.images.api.v2.views import ImagesAPIViewSet
+from wagtail.api.v2.views import PagesAPIViewSet
 from wagtail.documents.api.v2.views import DocumentsAPIViewSet
+from wagtail.images.api.v2.views import ImagesAPIViewSet
+from wagtail_headless_preview.models import PagePreview
 
 api_router = WagtailAPIRouter("wagtailapi")
+
+
+# class MyImagesAPIViewSet(ImagesAPIViewSet):
+#     body_fields = ImagesAPIViewSet.body_fields + ["caption", "copyright"]
+#     listing_default_fields = ImagesAPIViewSet.listing_default_fields + [
+#         "caption",
+#         "copyright",
+#     ]
+#     nested_default_fields = ImagesAPIViewSet.nested_default_fields + ["caption"]
+
+
+class PagePreviewAPIViewSet(PagesAPIViewSet):
+    known_query_parameters = PagesAPIViewSet.known_query_parameters.union(
+        ["content_type", "token"]
+    )
+
+    def listing_view(self, request):
+        page = self.get_object()
+        serializer = self.get_serializer(page)
+        return Response(serializer.data)
+
+    def detail_view(self, request, pk):
+        page = self.get_object()
+        serializer = self.get_serializer(page)
+        return Response(serializer.data)
+
+    def get_object(self):
+        app_label, model = self.request.GET["content_type"].split(".")
+        content_type = ContentType.objects.get(app_label=app_label, model=model)
+
+        page_preview = PagePreview.objects.get(
+            content_type=content_type, token=self.request.GET["token"]
+        )
+        page = page_preview.as_page()
+        if not page.pk:
+            # fake primary key to stop API URL routing from complaining
+            page.pk = 0
+
+        return page
+
 
 api_router.register_endpoint("pages", PagesAPIViewSet)
 api_router.register_endpoint("images", ImagesAPIViewSet)
 api_router.register_endpoint("documents", DocumentsAPIViewSet)
+api_router.register_endpoint("page_preview", PagePreviewAPIViewSet)
