371da9189105abb52284d3d4d4c8620cfad01427
Marcus Theisen <marcus.theisen@sinnwerkstatt.com>
2022-09-14 00:10:55 +0200

    Run black.

--- apps/landmatrix/management/commands/create_playwright_test_users.py
+++ apps/landmatrix/management/commands/create_playwright_test_users.py
@@ -7,22 +7,28 @@ User = get_user_model()
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        if not User.objects.filter(username='shakespeare').exists():
-            will = User.objects.create_superuser('shakespeare', 'william@shakespeare.dev', 'hamlet4eva')
+        if not User.objects.filter(username="shakespeare").exists():
+            will = User.objects.create_superuser(
+                "shakespeare", "william@shakespeare.dev", "hamlet4eva"
+            )
             will.level = 3
             cms_editors, _ = Group.objects.get_or_create(name="CMS Global (Editors)")
             will.groups.set([cms_editors])
             will.save()
-            print('Created will')
+            print("Created will")
 
-        if not User.objects.filter(username='test_editor').exists():
-            user = User.objects.create_user('test_editor', 'editor@test.dev', 'love2edit')
+        if not User.objects.filter(username="test_editor").exists():
+            user = User.objects.create_user(
+                "test_editor", "editor@test.dev", "love2edit"
+            )
             user.level = 2
             user.save()
-            print('Created test editor')
+            print("Created test editor")
 
-        if not User.objects.filter(username='test_reporter').exists():
-            user = User.objects.create_user('test_reporter', 'reporter@test.dev', 'love2report')
+        if not User.objects.filter(username="test_reporter").exists():
+            user = User.objects.create_user(
+                "test_reporter", "reporter@test.dev", "love2report"
+            )
             user.level = 1
             user.save()
-            print('Created test reporter')
+            print("Created test reporter")
