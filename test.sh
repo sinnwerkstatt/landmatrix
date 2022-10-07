8458f0b97f9e9bd09edecded9f97bfe4507a24aa
Ralph Delfs <ralph.delfs@sinnwerkstatt.com>
2022-10-07 10:07:34 +0200

    WIP: integration tests in CI

--- test.sh
+++ test.sh
@@ -4,20 +4,24 @@
 # The output is discarded to see the actual test results.
 # If you want to see the output run `npm run dev:test` in one terminal
 # and `npm run playwright` in another terminal.
+echo 'Starting backend, frontend (dev) and proxy'
 concurrently npm:backend npm:frontend npm:caddy > /dev/null &
 PID=$!
 
 # 2. Wait for app to be responsive
-while ! nc -z localhost 3000; do
+echo 'Waiting for backend'
+while ! nc -z localhost 8000; do
   sleep 0.3 # wait for 3/10 of the second before check again
 done
 echo 'Django online'
 
-while ! nc -z localhost 8000; do
+echo 'Waiting for frontend'
+while ! nc -z localhost 3000; do
   sleep 0.3 # wait for 3/10 of the second before check again
 done
 echo 'Frontend online'
 
+echo 'Waiting for proxy'
 while ! nc -z localhost 9000; do
   sleep 0.3 # wait for 3/10 of the second before check again
 done
