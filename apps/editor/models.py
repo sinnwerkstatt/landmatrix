2b8da2be10a1ae8a05d6d67c3614b88b78e9293e
Andreas Nüßlein <andreas@nuessle.in>
2022-12-15 21:54:09 +0000

    apps/editor is obsolete probably

--- apps/editor/models.py
+++ apps/editor/models.py
@@ -4,6 +4,7 @@ from django.db import models
 from apps.landmatrix.models.country import Country, Region
 
 
+# TODO delete this after big migration to new frontend.
 class UserRegionalInfo(models.Model):
 
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
