af0d9115fd7e185d1a981e2346cbd1fbd6e6b33a
Marcus Theisen <marcus.theisen@sinnwerkstatt.com>
2023-01-19 12:27:53 +0100

    Delete old frontend.

--- frontend/src/env.d.ts
+++
@@ -1,9 +0,0 @@
-interface ImportMeta {
-  env: {
-    PROD: boolean;
-    VITE_HCAPTCHA_SITEKEY: string;
-    VITE_MEDIA_URL: string;
-    VITE_GAPI_KEY: string;
-    VITE_SENTRY_DSN: string;
-  };
-}
