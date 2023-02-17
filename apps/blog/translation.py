6032a09a2a90f68daf037707cb258a56ead4e086
Marcus Theisen <marcus.theisen@sinnwerkstatt.com>
2023-02-17 18:50:41 +0100

    Proposal i18n BlogCategory.

--- apps/blog/translation.py
+++ apps/blog/translation.py
@@ -12,3 +12,14 @@ class BlogIndexPageTR(TranslationOptions):
 @register(BlogPage)
 class BlogPageTR(TranslationOptions):
     pass
+
+
+# after this run:
+# ./manage.py makemigrations
+# ./manage.py migrate
+# ./manage.py sync_page_translation_fields
+# ./manage.py update_translation_fields
+#
+# @register(BlogCategory)
+# class BlogCategoryTR(TranslationOptions):
+#     fields = ("name",)
