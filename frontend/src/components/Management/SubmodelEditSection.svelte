2534a6ee3abcecca1871fed97fd3b23afa440e4d
Marcus Theisen <marcus.theisen@sinnwerkstatt.com>
2023-03-14 15:49:49 +0100

    Add temp Ids for involvements in FE.

--- frontend/src/components/Management/SubmodelEditSection.svelte
+++ frontend/src/components/Management/SubmodelEditSection.svelte
@@ -12,9 +12,11 @@
   import PlusIcon from "$components/icons/PlusIcon.svelte"
   import TrashIcon from "$components/icons/TrashIcon.svelte"
 
+  type Entry = Contract | DataSource | Involvement
+
   export let model: "datasource" | "contract" | "involvement"
   export let modelName: string
-  export let entries: Array<Contract | DataSource | Involvement>
+  export let entries: Entry[]
   export let entriesFilter: (i: Involvement) => boolean = () => true
   export let newEntryExtras = {}
   export let id: string
@@ -25,18 +27,24 @@
 
   // ensure entries is not null
   $: entries = entries ?? []
+  // ensure entries to have non-null ids
+  // new involvements have null ids when investor still in draft mode
+  $: entries = createTempIdsIfNull(entries)
+
+  const createTempIdsIfNull = (entries: Entry[]) =>
+    entries.reduce((acc: Entry[], val: Entry): Entry[] => {
+      const existingIds = acc.map(entry => entry.id.toString())
+      return [...acc, { ...val, id: val.id ?? newNanoid(existingIds) } as Entry]
+    }, [])
 
   function addEntry() {
-    const currentIDs = entries.map(x => x?.id?.toString())
-    const newEntry = { id: newNanoid(currentIDs), ...newEntryExtras } as
-      | Contract
-      | DataSource
-      | Involvement
+    const currentIDs = entries.map(entry => entry.id.toString())
+    const newEntry: Entry = { id: newNanoid(currentIDs), ...newEntryExtras }
     entries = [...entries, newEntry]
     activeEntry = entries.length - 1
   }
 
-  function removeEntry(entry: Contract | DataSource) {
+  function removeEntry(entry: Entry) {
     if (!isEmptySubmodel(entry)) {
       const areYouSure = confirm(`${$_("Remove")} ${modelName} ${entry.id}?`)
       if (!areYouSure) return
--- frontend/src/lib/utils/data_processing.ts
+++ frontend/src/lib/utils/data_processing.ts
@@ -1,6 +1,8 @@
 import type { Contract, DataSource, Deal } from "$lib/types/deal"
 import type { Involvement } from "$lib/types/investor"
 
+export type SubmodelEntry = Contract | DataSource | Location | Involvement
+
 export function sum(items: Deal[], prop: keyof Deal): number {
   return items.reduce(function (a, b) {
     return a + (b[prop] as number)
@@ -16,22 +18,17 @@ export function custom_is_null(field: unknown): boolean {
   )
 }
 
-function sieveSubmodel(
-  entry: Contract | DataSource | Location | Involvement,
-  ignoreKeys = ["id", "role"],
-) {
+function sieveSubmodel(entry: SubmodelEntry, ignoreKeys = ["id", "role"]) {
   return Object.entries(entry).filter(([k, v]) =>
     ignoreKeys.includes(k) ? false : !custom_is_null(v),
   )
 }
 
-export function isEmptySubmodel(entry: Contract | DataSource | Location): boolean {
+export function isEmptySubmodel(entry: SubmodelEntry): boolean {
   const fieldsWithValues = sieveSubmodel(entry)
   return fieldsWithValues.length === 0
 }
-export function removeEmptyEntries<
-  T extends Contract | DataSource | Location | Involvement,
->(objectlist: T[]): T[] {
+export function removeEmptyEntries<T extends SubmodelEntry>(objectlist: T[]): T[] {
   // this function throws out any entries that have only an ID field and otherwise empty values.
   return objectlist.filter(con => sieveSubmodel(con).some(x => !!x))
 }
