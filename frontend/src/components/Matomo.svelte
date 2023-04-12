85df2962a160ce8f215c9069406cf0bdbb1c0257
Marcus Theisen <marcus.theisen@sinnwerkstatt.com>
2023-04-12 11:49:06 +0200

    Hopefully fix matomo download tracking.

--- frontend/src/components/Matomo.svelte
+++ frontend/src/components/Matomo.svelte
@@ -1,42 +1,30 @@
 <script lang="ts">
+  import { onMount } from "svelte"
+
   import { afterNavigate } from "$app/navigation"
   import { page } from "$app/stores"
-  import { onMount } from "svelte"
-  import { writable } from "svelte/store"
 
-  export let url = import.meta.env.VITE_MATOMO_URL
-  export let siteId = import.meta.env.VITE_MATOMO_SITE_ID
+  import { tracker } from "$lib/stores/tracker"
+
+  export let url: string = import.meta.env.VITE_MATOMO_URL
+  export let siteId: number = import.meta.env.VITE_MATOMO_SITE_ID
   export let cookies = true
   export let consentRequired = false
-
   export let doNotTrack = false
   export let heartBeat = 2000
 
-  let tracker = writable()
-
-  interface Tracker {
-    disableCookies()
-    requireConsent()
-    setDoNotTrack()
-    enableHeartBeatTimer(heartBeat)
-    setCustomUrl(url): void
-    setCustomDimension(dimensionID: number, value: any): this
-    trackPageView(): void
-  }
-  interface Matomo {
-    getTracker(trackerUrl, siteId): Tracker | undefined
-  }
   async function initializeMatomo() {
-    const _matomo: Matomo | undefined = window.Matomo
-    if (!_matomo) return
-    const track = _matomo.getTracker(`${url}/matomo.php`, siteId)
+    const matomo = window.Matomo
+    if (!matomo) return
+    const track = matomo.getTracker(`${url}/matomo.php`, siteId)
     if (!track) return
 
     if (!cookies) track.disableCookies()
     if (consentRequired) track.requireConsent()
-    if (doNotTrack) track.setDoNotTrack()
+    if (doNotTrack) track.setDoNotTrack(true)
     if (heartBeat) track.enableHeartBeatTimer(heartBeat)
-    await tracker.set(track)
+
+    tracker.set(track)
 
     track.setCustomDimension(1, $page.data.user ? "true" : "false")
     track.setCustomUrl($page.url.href)
