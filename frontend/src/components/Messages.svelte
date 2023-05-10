57405b04ad7ec3bf4385bcacdee2f8861b7a7e8e
Marcus Theisen <marcus.theisen@sinnwerkstatt.com>
2023-05-10 12:52:22 +0200

    Fix messages for private browsers - sentry 1377

--- frontend/src/components/Messages.svelte
+++ frontend/src/components/Messages.svelte
@@ -5,48 +5,78 @@
 
   import Overlay from "$components/Overlay.svelte"
 
-  async function getMessages() {
-    if (!browser) return []
-    if (
-      document.referrer?.startsWith(
-        window.location.protocol + "//" + window.location.hostname,
-      )
+  interface Message {
+    id: number
+    title: string
+    text: string
+    level: "debug" | "info" | "success" | "warning" | "error"
+    is_active: boolean
+    allow_users_to_hide: boolean
+  }
+
+  async function getMessages(): Promise<Message[]> {
+    const isSameSiteReferrer = document.referrer?.startsWith(
+      window.location.protocol + "//" + window.location.hostname,
     )
+    if (isSameSiteReferrer) {
       return []
+    }
+
     const res = await fetch(`/api/newdeal_legacy/messages/`)
-    if (res.ok) return (await res.json()).messages
-    else throw new Error(await res.text())
+    if (!res.ok) {
+      throw new Error(await res.text())
+    }
+
+    return (await res.json()).messages
   }
 
-  let messages = getMessages()
+  let storage: Storage | object = {}
 
-  let acknowledgedMessages: number[] = JSON.parse(
-    (browser && sessionStorage.getItem("acknowledgedMessages")) || "[]",
-  )
-  const ackMsg = id => {
+  if (browser) {
+    try {
+      storage = window.sessionStorage || {}
+    } catch {
+      // sessionStorage disabled due to private browsing
+      // https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API#private_browsing_incognito_modes
+    }
+  }
+
+  let acknowledgedMessages: number[] =
+    JSON.parse(storage["acknowledgedMessages"] ?? null) || []
+
+  const ackMsg = (id: number) => {
     acknowledgedMessages.push(id)
-    sessionStorage.setItem("acknowledgedMessages", JSON.stringify(acknowledgedMessages))
+    storage["acknowledgedMessages"] = JSON.stringify(acknowledgedMessages)
   }
-  const levelClasses = {
+
+  const levelClasses: { [key in Message["level"]]: string } = {
     error: "border-red-600 border-2",
     warning: "border-orange border-2",
     info: "border-orange border",
+    debug: "border-gray-600 border-2",
+    success: "border-green-600 border-2",
   }
 </script>
 
-{#await messages then msgs}
+{#await getMessages() then msgs}
   {#each msgs.filter(m => !m.allow_users_to_hide || !acknowledgedMessages.includes(m.id)) as msg}
     <Overlay
       visible
       title={msg.title}
       class={levelClasses[msg.level]}
       closeButtonText={$_("OK")}
+      on:close={() => {
+        const checkbox = document.getElementById(`do-not-show-again-${msg.id}`)
+        if (checkbox && checkbox.checked) {
+          ackMsg(msg.id)
+        }
+      }}
     >
       {@html msg.text}
 
       {#if msg.allow_users_to_hide}
         <label class="block">
-          <input type="checkbox" on:click={() => ackMsg(msg.id)} />
+          <input id={`do-not-show-again-${msg.id}`} type="checkbox" />
           {$_("Don't show this message again")}
         </label>
       {/if}
