dd598c1cee9cb773eb42cfe791f29d598bcf64f3
Marcus Theisen <marcus.theisen@sinnwerkstatt.com>
2023-06-10 19:11:15 +0200

    Add margins to page title.

--- frontend/src/components/PageTitle.svelte
+++ frontend/src/components/PageTitle.svelte
@@ -1,10 +1,10 @@
 {#if $$slots?.default}
-  <div class="container isolate mx-auto mt-9 mb-5">
+  <div class="container isolate mx-auto mt-16 mb-12">
     <h1
       class="relative text-center text-3xl before:absolute before:top-1/2 before:block before:h-[1px] before:w-full before:bg-gray-300 before:content-[''] dark:before:bg-gray-500"
     >
       <span
-        class="relative z-10 bg-white px-4 font-bold uppercase uppercase text-orange dark:bg-gray-800 {$$props.class}"
+        class="relative z-10 bg-white px-4 font-bold uppercase text-orange dark:bg-gray-800 {$$props.class}"
       >
         <slot />
       </span>
