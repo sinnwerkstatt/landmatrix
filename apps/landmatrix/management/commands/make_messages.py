95d70149d3cb75ad4c352a9abda6909af122588f
Andreas Nüßlein <andreas@nuessle.in>
2023-10-14 10:04:11 +0100

    fix make_messages, add some new translation strings

--- apps/landmatrix/management/commands/make_messages.py
+++ apps/landmatrix/management/commands/make_messages.py
@@ -2,10 +2,21 @@ from django.core.management.commands import makemessages
 
 
 class Command(makemessages.Command):
-    msgmerge_options = [
-        "-q",
-        "--previous",
-        "--no-fuzzy-matching",
+    msgattrib_options = makemessages.Command.msgattrib_options + [
+        "--no-fuzzy",
+        "--no-location",
+        "--no-wrap",
+    ]
+    msguniq_options = makemessages.Command.msguniq_options + [
         "--no-wrap",
         "--no-location",
+    ]
+    msgmerge_options = makemessages.Command.msgmerge_options + [
+        "--no-location",
+        "--no-wrap",
+        "--no-fuzzy-matching",
+    ]
+    xgettext_options = makemessages.Command.xgettext_options + [
+        "--no-location",
+        "--no-wrap",
     ]
