ccc22e97927b8b8d09645f91daad4936f171a8d9
Andreas Nüßlein <andreas@nuessle.in>
2023-10-16 15:41:48 +0100

    add styleguide (beta)

---
+++ frontend/src/routes/styleguide/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+  import ColorStyles from "./ColorStyles.svelte"
+  import TypographyStyles from "./TypographyStyles.svelte"
+</script>
+
+<div class="container mx-auto mb-32">
+  <h1 class="heading2">Style guide</h1>
+
+  <ColorStyles />
+  <div class="p-28" />
+  <TypographyStyles />
+</div>
---
+++ frontend/src/routes/styleguide/ColorDef.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import { allColors } from "./colors.js"
+
+  export let tileColor = ""
+  export let title = ""
+  export let defs = ""
+</script>
+
+<div>
+  <div class="h-40 w-40 rounded-lg {tileColor}" />
+  <div class="mt-3">
+    <b>{title}</b>
+    <ul>
+      {#each allColors[defs] ?? [] as def}
+        <li>{def}</li>
+      {/each}
+    </ul>
+  </div>
+</div>
