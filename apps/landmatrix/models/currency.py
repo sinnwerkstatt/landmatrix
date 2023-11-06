bf2b81b6ae997a664da5ee888bc23c3f8e828821
Andreas Nüßlein <andreas@nuessle.in>
2023-11-06 12:50:21 +0000

    cleanup: remove arbitrary obsolete max_lengths

--- apps/landmatrix/models/currency.py
+++ apps/landmatrix/models/currency.py
@@ -3,8 +3,8 @@ from django.db import models
 
 class Currency(models.Model):
     code = models.CharField("Code", max_length=3)
-    name = models.CharField("Name", max_length=255)
-    symbol = models.CharField("Symbol", max_length=255)
+    name = models.CharField("Name")
+    symbol = models.CharField("Symbol")
     country = models.CharField("Country", max_length=2)
     ranking = models.IntegerField("Ranking")
 
