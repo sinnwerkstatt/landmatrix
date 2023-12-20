22848b4a886c434841f6df6eec54ff4541f41c56
Marcus Theisen <marcus.theisen@sinnwerkstatt.com>
2023-12-20 11:29:18 +0100

    Install postcss-import. Add root package name.

--- frontend/postcss.config.cjs
+++ frontend/postcss.config.cjs
@@ -1,8 +1,10 @@
 const tailwindcss = require("tailwindcss")
 const autoprefixer = require("autoprefixer")
+const postcss_import = require("postcss-import")
 
 const config = {
   plugins: [
+    postcss_import(),
     //Some plugins, like tailwindcss/nesting, need to run before Tailwind,
     tailwindcss(),
     //But others, like autoprefixer, need to run after,
