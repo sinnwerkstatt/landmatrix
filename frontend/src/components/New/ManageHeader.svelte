86f5f3fcbb5ddce38b1f76a613decb1a87249e0c
Andreas Nüßlein <andreas@nuessle.in>
2024-01-20 17:19:10 +0000

    ManageHeader cleanup

--- frontend/src/components/New/ManageHeader.svelte
+++ frontend/src/components/New/ManageHeader.svelte
@@ -27,131 +27,165 @@
   let showCopyOverlay = false
   let showDeletionOverlay = false
   let showConfidentialOverlay = false
+
+  $: oldVersion = ![object.active_version_id, object.draft_version_id].includes(
+    object.selected_version.id,
+  )
 </script>
 
 <div class="my-4 grid grid-cols-2 lg:grid-cols-3">
   <div class="col-span-2 rounded-tl bg-gray-100 dark:bg-gray-600">
     <div class="flex items-center justify-between gap-4 p-2">
-      <div>
+      <div class="flex items-center gap-8">
         <h1 class="heading4 my-0 text-[1.875rem]">
-          <slot name="heading" />
+          {#if object.selected_version.id !== object.active_version_id}
+            <a href="/{objType}/{object.id}/" class:investor={!isDeal(object)}>
+              <slot name="heading" />
+            </a>
+          {:else}
+            <slot name="heading" />
+          {/if}
         </h1>
+        <div
+          class={oldVersion
+            ? "pointer-events-none cursor-not-allowed opacity-40"
+            : undefined}
+        >
+          <DetailsSummary>
+            <div class="butn flex items-center gap-1" slot="summary">
+              <Cog6ToothIcon />
+              {$_("actions")}
+            </div>
+            <ul
+              class="absolute z-50 mt-1 rounded border border-gray-400 bg-white px-4 py-2 shadow-2xl dark:bg-gray-700"
+              slot="details"
+            >
+              {#if object.selected_version.id === object.active_version_id && !object.draft_version_id}
+                <li class="my-3">
+                  <div class="flex items-center gap-2">
+                    <a
+                      class="butn butn-primary min-w-[9rem]"
+                      href="/{objType}/edit/{object.id}/"
+                    >
+                      {$_("Edit")}
+                    </a>
+                    {$_("Create a new draft version of this investor")}
+                  </div>
+                </li>
+              {:else if object.selected_version.id !== object.draft_version_id}
+                <li class="my-3">
+                  <div class="flex items-center gap-2">
+                    <button class="butn butn-primary disabled min-w-[9rem]">
+                      {$_("Edit")}
+                    </button>
+                    {$_("There is already a draft version, that you can find here:")}
+                    <a href="/{objType}/{object.id}/{object.draft_version_id}/">
+                      {$_("Version")} #{object.draft_version_id}
+                    </a>
+                  </div>
+                </li>
+              {/if}
+
+              {#if isDeal(object) && $page.data.user?.role === UserRole.ADMINISTRATOR}
+                <li class="my-3">
+                  <div class="flex items-center gap-2">
+                    <button
+                      type="button"
+                      class="butn"
+                      on:click={() => (showCopyOverlay = true)}
+                    >
+                      {$_("Copy deal")}
+                    </button>
+                    {$_("Copy this deal")}
+                  </div>
+                </li>
+              {/if}
+              <li class="my-3">
+                <div class="flex items-center gap-2">
+                  <button
+                    type="button"
+                    class="butn butn-red min-w-[9rem]"
+                    on:click={() => (showDeletionOverlay = true)}
+                  >
+                    {#if object.deleted}
+                      {$_("Undelete")}
+                    {:else}
+                      {$_("Delete")}
+                    {/if}
+                  </button>
+                  {#if object.deleted}
+                    {isDeal(object)
+                      ? $_("Reactivate this deal")
+                      : $_("Reactivate this investor")}
+                  {:else}
+                    {isDeal(object)
+                      ? $_("Delete this deal")
+                      : $_("Delete this investor")}
+                  {/if}
+                </div>
+              </li>
+              {#if isDeal(object) && $page.data.user?.role === UserRole.ADMINISTRATOR}
+                <li class="my-3">
+                  <div class="flex items-center gap-2">
+                    <button
+                      type="button"
+                      class="butn butn-red"
+                      on:click={() => (showConfidentialOverlay = true)}
+                    >
+                      {object.confidential
+                        ? $_("Unset confidential")
+                        : $_("Set confidential")}
+                    </button>
+                    {#if object.confidential}
+                      this will unsset confidential
+                    {:else}
+                      this will set confidential
+                    {/if}
+                  </div>
+                </li>
+              {/if}
+            </ul>
+          </DetailsSummary>
+        </div>
       </div>
 
-      <DetailsSummary>
-        <div class="butn flex items-center gap-1" slot="summary">
-          <Cog6ToothIcon />
-          actions
-        </div>
-        <ul
-          class="absolute z-50 border border-black bg-white p-2 shadow-2xl dark:bg-gray-700"
-          slot="details"
-        >
-          <li class="my-2">
-            <div class="flex items-center gap-2">
-              <a class="butn" href="/{objType}/edit/{object.id}/">
-                {$_("Edit")}
-              </a>
-              {$_("Create a new draft version of this investor")}
-            </div>
-          </li>
-          {#if isDeal(object) && $page.data.user?.role === UserRole.ADMINISTRATOR}
-            <li class="my-2">
-              <div class="flex items-center gap-2">
-                <button
-                  type="button"
-                  class="butn"
-                  on:click={() => (showCopyOverlay = true)}
-                >
-                  {$_("Copy deal")}
-                </button>
-                {$_("Copy this deal")}
-              </div>
-            </li>
-          {/if}
-          <li class="my-2">
-            <div class="flex items-center gap-2">
-              <button
-                type="button"
-                class="butn butn-red"
-                on:click={() => (showDeletionOverlay = true)}
-              >
-                {#if object.deleted}
-                  {$_("Undelete")}
-                {:else}
-                  {$_("Delete")}
-                {/if}
-              </button>
-              {#if object.deleted}
-                {isDeal(object)
-                  ? $_("Reactivate this deal")
-                  : $_("Reactivate this investor")}
-              {:else}
-                {isDeal(object) ? $_("Delete this deal") : $_("Delete this investor")}
-              {/if}
-            </div>
-          </li>
-          {#if isDeal(object) && $page.data.user?.role === UserRole.ADMINISTRATOR}
-            <li class="my-2">
-              <div class="flex items-center gap-2">
-                <button
-                  type="button"
-                  class="butn butn-red"
-                  on:click={() => (showConfidentialOverlay = true)}
-                >
-                  {#if object.confidential}
-                    unset confidential
-                  {:else}
-                    set confidential
-                  {/if}
-                </button>
-                {#if object.confidential}
-                  this will unsset confidential
-                {:else}
-                  this will set confidential
-                {/if}
-              </div>
-            </li>
-          {/if}
-        </ul>
-      </DetailsSummary>
-      <DetailsSummary>
-        <div class="butn butn-primary flex items-center gap-1" slot="summary">
-          <Cog6ToothIcon />
-          versions
-        </div>
-        <ul
-          class="absolute z-50 border border-black bg-white p-2 shadow-2xl dark:bg-gray-700"
-          slot="details"
-        >
-          {#if object.active_version_id}
-            <li class="my-2">
-              <div class="flex items-center gap-2">
-                <a
-                  class="butn"
-                  href="/{objType}/{object.id}/{object.active_version_id}/"
-                >
-                  active version
-                </a>
-                go to current active version
-              </div>
-            </li>
-          {/if}
-          {#if object.draft_version_id}
-            <li class="my-2">
-              <div class="flex items-center gap-2">
-                <a
-                  class="butn"
-                  href="/{objType}/{object.id}/{object.draft_version_id}/"
-                >
-                  draft version
-                </a>
-                go to current draft version
-              </div>
-            </li>
-          {/if}
-        </ul>
-      </DetailsSummary>
+      <!--      <DetailsSummary>-->
+      <!--        <div class="butn butn-primary flex items-center gap-1" slot="summary">-->
+      <!--          <Cog6ToothIcon />-->
+      <!--          versions-->
+      <!--        </div>-->
+      <!--        <ul-->
+      <!--          class="absolute z-50 border border-black bg-white p-2 shadow-2xl dark:bg-gray-700"-->
+      <!--          slot="details"-->
+      <!--        >-->
+      <!--          {#if object.active_version_id}-->
+      <!--            <li class="my-2">-->
+      <!--              <div class="flex items-center gap-2">-->
+      <!--                <a-->
+      <!--                  class="butn"-->
+      <!--                  href="/{objType}/{object.id}/{object.active_version_id}/"-->
+      <!--                >-->
+      <!--                  active version-->
+      <!--                </a>-->
+      <!--                go to current active version-->
+      <!--              </div>-->
+      <!--            </li>-->
+      <!--          {/if}-->
+      <!--          {#if object.draft_version_id}-->
+      <!--            <li class="my-2">-->
+      <!--              <div class="flex items-center gap-2">-->
+      <!--                <a-->
+      <!--                  class="butn"-->
+      <!--                  href="/{objType}/{object.id}/{object.draft_version_id}/"-->
+      <!--                >-->
+      <!--                  draft version-->
+      <!--                </a>-->
+      <!--                go to current draft version-->
+      <!--              </div>-->
+      <!--            </li>-->
+      <!--          {/if}-->
+      <!--        </ul>-->
+      <!--      </DetailsSummary>-->
       <HeaderDates obj={object} />
       <!--      <div class="bg-white px-4 py-2">Draft version</div>-->
     </div>
