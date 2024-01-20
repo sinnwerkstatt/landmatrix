ea019b36307e406701dd4217ded81457333a75b3
Andreas Nüßlein <andreas@nuessle.in>
2024-01-20 15:42:56 +0000

    cleanup

--- frontend/src/components/HeaderDates.svelte
+++ frontend/src/components/HeaderDates.svelte
@@ -1,31 +1,41 @@
 <script lang="ts">
-  import { _ } from "svelte-i18n"
-
   import type { DealHull, InvestorHull } from "$lib/types/newtypes"
 
   import DisplayField from "$components/Fields/DisplayField.svelte"
 
   export let obj: DealHull | InvestorHull
+  const wrapperClass = "flex-col flex items-center"
+  const valueClass = "text-base md:text-lg"
+  const labelClass = "text-xs md:text-sm"
 </script>
 
 <div
-  class="my-2 flex w-auto items-center whitespace-nowrap bg-gray-50 p-3 text-center text-gray-700 dark:bg-gray-700 dark:text-white"
+  class="my-2 flex items-center justify-evenly gap-3 whitespace-nowrap bg-gray-50 p-3 text-gray-700 md:gap-5 dark:bg-gray-700 dark:text-white"
 >
-  <div class="mr-10 text-xs md:mx-5 md:text-sm">
-    {$_("Created")}
-    <DisplayField fieldname="created_at" value={obj.selected_version.created_at} />
-  </div>
-  <div class="mr-10 text-xs md:mx-5 md:text-sm">
-    {$_("Last update")}
+  <DisplayField
+    fieldname="created_at"
+    {labelClass}
+    showLabel
+    value={obj.first_created_at}
+    {valueClass}
+    {wrapperClass}
+  />
+  <DisplayField
+    fieldname="modified_at"
+    {labelClass}
+    showLabel
+    value={obj.selected_version.modified_at ?? obj.selected_version.created_at}
+    {valueClass}
+    {wrapperClass}
+  />
+  {#if "fully_updated_at" in obj}
     <DisplayField
-      fieldname="modified_at"
-      value={obj.selected_version.modified_at ?? obj.selected_version.created_at}
+      fieldname="fully_updated_at"
+      value={obj.fully_updated_at}
+      {wrapperClass}
+      {valueClass}
+      {labelClass}
+      showLabel
     />
-  </div>
-  {#if obj.fully_updated_at}
-    <div class="mr-10 text-xs md:mx-5 md:text-sm">
-      {$_("Last full update")}
-      <DisplayField fieldname="fully_updated_at" value={obj.fully_updated_at} />
-    </div>
   {/if}
 </div>
