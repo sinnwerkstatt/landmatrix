c7c8e7a4a3b5cfbf11b56fb8ec00a8a1fadece0a
Marcus Theisen <marcus.theisen@sinnwerkstatt.com>
2024-02-15 12:04:05 +0100

    Fix wagtail preview.

--- frontend/src/lib/queries.ts
+++ frontend/src/lib/queries.ts
@@ -1,16 +1,17 @@
-import type { LoadEvent } from "@sveltejs/kit"
+import type { LoadEvent, NumericRange } from "@sveltejs/kit"
 import { error } from "@sveltejs/kit"
 
-export async function pageQuery(url: URL, fetch: LoadEvent["fetch"]) {
-  const page_url =
-    url.pathname === "/wagtail-preview"
-      ? `/api/wagtail/v2/page_preview/1/?content_type=${encodeURIComponent(
-          url.searchParams.get("content_type") ?? "",
-        )}&token=${encodeURIComponent(url.searchParams.get("token") ?? "")}&format=json`
-      : `/api/wagtail/v2/pages/find/?html_path=${url.pathname}`
+const WAGTAIL_API_BASE_URL = "/api/wagtail/v2"
 
-  const res = await fetch(page_url, { headers: { Accept: "application/json" } })
-  if (!res.ok) error(res.status, (await res.json()).message)
+export async function pageQuery(url: URL, fetch: LoadEvent["fetch"]) {
+  const pageUrl = url.pathname.startsWith("/wagtail-preview")
+    ? `/page_preview/1/?${url.searchParams}&format=json`
+    : `/pages/find/?html_path=${url.pathname}`
+
+  const res = await fetch(WAGTAIL_API_BASE_URL + pageUrl, {
+    headers: { Accept: "application/json" },
+  })
+  if (!res.ok) error(res.status as NumericRange<400, 599>, (await res.json()).message)
 
   return await res.json()
 }
