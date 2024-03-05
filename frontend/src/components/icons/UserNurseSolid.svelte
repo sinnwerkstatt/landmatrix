1576e668278980973ce61f6de90f6b2526b327c1
Marcus Theisen <marcus.theisen@sinnwerkstatt.com>
2024-03-05 23:22:44 +0100

    Add default icon styles where missing.

--- frontend/src/components/icons/UserNurseSolid.svelte
+++ frontend/src/components/icons/UserNurseSolid.svelte
@@ -1,7 +1,7 @@
 <svg
   xmlns="http://www.w3.org/2000/svg"
   viewBox="0 0 448 512"
-  class={$$props.class}
+  class={$$props.class ?? "inline h-4 w-4"}
   fill="currentColor"
 >
   <!--! Font Awesome Pro 6.0.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
