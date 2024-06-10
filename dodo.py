efe7c4564f8896a61d949cf0967c882ac8e041aa
Marcus Theisen <marcus.theisen@sinnwerkstatt.com>
2024-06-10 12:50:38 +0200

    Undo pnpm changes, because CI=true set by gitlab.

--- dodo.py
+++ dodo.py
@@ -137,7 +137,7 @@ def task_test_watch():
 
 #############################
 def task_frontend_build():
-    actions = ["cd frontend; pnpm run ci"]
+    actions = ["cd frontend; pnpm install"]
     if get_var("production", False):
         actions += ["cd frontend; pnpm run build"]
     return {
