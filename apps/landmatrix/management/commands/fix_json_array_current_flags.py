e5239406400c9f4bf3abb3b07cea906a1c2271c6
Marcus Theisen <marcus.theisen@sinnwerkstatt.com>
2024-07-17 10:46:23 +0200

    Add scripts to check and fix database issues.

---
+++ apps/landmatrix/management/commands/fix_json_array_current_flags.py
@@ -0,0 +1,66 @@
+import re
+
+from django.core.management import BaseCommand
+
+from apps.landmatrix.models import schema
+from apps.landmatrix.models.new import DealVersion
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        confirm = input(
+            "***** ATTENTION ***** \n"
+            "This command potentially manipulates DB. \n"
+            "Make sure DB connection is configured correctly in .env. \n"
+            "Confirm to continue (y/N): "
+        )
+
+        if not confirm or not re.match("^y(es)?$", confirm, re.I):
+            print("Aborting")
+            return
+
+        # 547
+        version = DealVersion.objects.get(id=85669)
+        print(version)
+        version.implementation_status = schema.CurrentDateChoiceImplementationStatus(
+            set_current(version.implementation_status.model_dump())
+        )
+        version.save()
+
+        for v_id in [48727, 48728, 48729]:  # 688
+            version = DealVersion.objects.get(id=v_id)
+            print(version)
+            version.negotiation_status = schema.CurrentDateChoiceNegotiationStatus(
+                set_current(version.negotiation_status.model_dump())
+            )
+            version.save()
+
+        for v_id in [67385, 67386]:  # 4393
+            version = DealVersion.objects.get(id=v_id)
+            print(version)
+            version.contract_size = schema.CurrentDateAreaSchema(
+                set_current(version.contract_size.model_dump())
+            )
+            version.save()
+
+        for v_id in [81861, 81862]:  # 7978
+            version = DealVersion.objects.get(id=v_id)
+            print(version)
+            version.negotiation_status = schema.CurrentDateChoiceNegotiationStatus(
+                set_current(version.negotiation_status.model_dump())
+            )
+            version.save()
+
+        for v_id in [82404, 82405]:  # 8111
+            version = DealVersion.objects.get(id=v_id)
+            print(version)
+            version.production_size = schema.CurrentDateAreaSchema(
+                set_current(version.production_size.model_dump())
+            )
+            version.save()
+
+        print("DONE")
+
+
+def set_current(array: list) -> list:
+    return [*array[:-1], {**array[-1], "current": True}]
---
+++ apps/landmatrix/management/commands/fix_json_array_text_fields.py
@@ -0,0 +1,68 @@
+import re
+
+from tqdm import tqdm
+
+from django.core.management import BaseCommand
+
+from apps.landmatrix.models.new import DealVersion
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        confirm = input(
+            "***** ATTENTION ***** \n"
+            "This command potentially manipulates DB. \n"
+            "Make sure DB connection is configured correctly in .env. \n"
+            "Confirm to continue (y/N): "
+        )
+
+        if not confirm or not re.match("^y(es)?$", confirm, re.I):
+            print("Aborting")
+            return
+
+        count = 0
+        total = DealVersion.objects.count()
+        iterator = DealVersion.objects.iterator()
+
+        print("Iterating DealVersions.")
+        for version in tqdm(iterator, total=total):
+            updated = fix_text_array_fields(version)
+
+            if updated:
+                try:
+                    version.save()
+                    count += 1
+                except Exception as e:
+                    print(f"Could not save DealVersion {version}")
+                    print(f"Error: {e}")
+                    print()
+
+        print(f"Updated {count} DealVersions")
+        print("DONE")
+
+
+def is_non_empty_string(string: str) -> bool:
+    return bool(string.strip())
+
+
+def has_empty_strings(array: list[str]) -> bool:
+    return not all(map(is_non_empty_string, array))
+
+
+def filter_empty_strings(array: list[str]) -> list[str]:
+    return list(filter(is_non_empty_string, array))
+
+
+def fix_text_array_fields(version: DealVersion) -> bool:
+    updated = False
+
+    for field_name in ["name_of_indigenous_people", "name_of_community"]:
+        field_value = getattr(version, field_name)
+
+        if has_empty_strings(field_value):
+            new_field_value = filter_empty_strings(field_value)
+            # print(version.id, field_value, new_field_value)
+            setattr(version, field_name, new_field_value)
+            updated = True
+
+    return updated
