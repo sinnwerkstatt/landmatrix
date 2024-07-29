a914e905ee6e6253148786e32dcd7424cb9759d5
Marcus Theisen <marcus.theisen@sinnwerkstatt.com>
2024-07-29 20:43:06 +0200

    Add missing WFI version stati.

--- frontend/src/lib/newUtils.ts
+++ frontend/src/lib/newUtils.ts
@@ -3,7 +3,12 @@ import { derived } from "svelte/store"
 
 import type { Version2Status } from "$lib/types/data"
 
-type VersionStatusMap = { [key in Version2Status]: { title: string; classes: string } }
+type VersionStatusMap = {
+  [key in Version2Status | "DELETED" | "REACTIVATED"]: {
+    title: string
+    classes: string
+  }
+}
 
 export const stateMap = derived(
   _,
@@ -12,5 +17,8 @@ export const stateMap = derived(
     REVIEW: { title: $_("Review"), classes: "text-black bg-green-400" },
     ACTIVATION: { title: $_("Activation"), classes: "text-black bg-green-500" },
     ACTIVATED: { title: $_("Activated"), classes: "text-white bg-green-700" },
+    // FIXME: still used in workflowinfos
+    DELETED: { title: $_("Deleted"), classes: "text-white bg-red-500" },
+    REACTIVATED: { title: $_("Activated"), classes: "text-white bg-green-700" },
   }),
 )
