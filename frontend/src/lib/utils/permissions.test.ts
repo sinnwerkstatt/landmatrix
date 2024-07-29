bf090d52b7f05c10ab9ae563bf10f4deb7c9a109
Marcus Theisen <marcus.theisen@sinnwerkstatt.com>
2024-07-29 11:27:14 +0200

    Add isStaff and implicit isSuperuser permissions.

--- frontend/src/lib/utils/permissions.test.ts
+++ frontend/src/lib/utils/permissions.test.ts
@@ -4,6 +4,7 @@ import {
   isAnybodyOrAbove,
   isEditorOrAbove,
   isReporterOrAbove,
+  isStaff,
 } from "$lib/utils/permissions"
 
 describe("User permissions", () => {
@@ -14,6 +15,9 @@ describe("User permissions", () => {
     expect(isAnybodyOrAbove(REPORTER)).toBeTruthy()
     expect(isAnybodyOrAbove(EDITOR)).toBeTruthy()
     expect(isAnybodyOrAbove(ADMIN)).toBeTruthy()
+
+    expect(isAnybodyOrAbove(STAFF)).toBeTruthy()
+    expect(isAnybodyOrAbove(SUPERUSER)).toBeTruthy()
   })
   test("isReporterOrAbove", () => {
     expect(isReporterOrAbove(ANONYMOUS)).toBeFalsy()
@@ -22,6 +26,9 @@ describe("User permissions", () => {
     expect(isReporterOrAbove(REPORTER)).toBeTruthy()
     expect(isReporterOrAbove(EDITOR)).toBeTruthy()
     expect(isReporterOrAbove(ADMIN)).toBeTruthy()
+
+    expect(isReporterOrAbove(STAFF)).toBeFalsy()
+    expect(isReporterOrAbove(SUPERUSER)).toBeTruthy()
   })
   test("isEditorOrAbove", () => {
     expect(isEditorOrAbove(ANONYMOUS)).toBeFalsy()
@@ -30,6 +37,9 @@ describe("User permissions", () => {
 
     expect(isEditorOrAbove(EDITOR)).toBeTruthy()
     expect(isEditorOrAbove(ADMIN)).toBeTruthy()
+
+    expect(isEditorOrAbove(STAFF)).toBeFalsy()
+    expect(isEditorOrAbove(SUPERUSER)).toBeTruthy()
   })
   test("isAdmin", () => {
     expect(isAdmin(ANONYMOUS)).toBeFalsy()
@@ -38,6 +48,19 @@ describe("User permissions", () => {
     expect(isAdmin(EDITOR)).toBeFalsy()
 
     expect(isAdmin(ADMIN)).toBeTruthy()
+
+    expect(isAdmin(STAFF)).toBeFalsy()
+    expect(isAdmin(SUPERUSER)).toBeTruthy()
+  })
+  test("isStaff", () => {
+    expect(isStaff(ANONYMOUS)).toBeFalsy()
+    expect(isStaff(ANYBODY)).toBeFalsy()
+    expect(isStaff(REPORTER)).toBeFalsy()
+    expect(isStaff(EDITOR)).toBeFalsy()
+    expect(isStaff(ADMIN)).toBeFalsy()
+
+    expect(isStaff(STAFF)).toBeTruthy()
+    expect(isStaff(SUPERUSER)).toBeTruthy()
   })
 })
 
@@ -57,4 +80,14 @@ const REPORTER = {
 
 const ADMIN = {
   role: UserRole.ADMINISTRATOR,
+} as User
+
+const SUPERUSER = {
+  role: UserRole.ANYBODY,
+  is_superuser: true,
+} as User
+
+const STAFF = {
+  role: UserRole.ANYBODY,
+  is_staff: true,
 } as User
--- frontend/src/lib/utils/permissions.ts
+++ frontend/src/lib/utils/permissions.ts
@@ -3,9 +3,12 @@ import { UserRole, type LeanUser, type User } from "$lib/types/data"
 export const hasRoleOrAbove =
   (role: UserRole) =>
   (user: User | LeanUser | null): boolean =>
-    user !== null && user.role >= role
+    user !== null && ((user as User).is_superuser || user.role >= role)
 
 export const isAnybodyOrAbove = hasRoleOrAbove(UserRole.ANYBODY)
 export const isReporterOrAbove = hasRoleOrAbove(UserRole.REPORTER)
 export const isEditorOrAbove = hasRoleOrAbove(UserRole.EDITOR)
 export const isAdmin = hasRoleOrAbove(UserRole.ADMINISTRATOR)
+
+export const isStaff = (user: User | null): boolean =>
+  user !== null && (user.is_superuser || user.is_staff)
