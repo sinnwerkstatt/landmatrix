6e865e8bce249ad990ef35f15299b6c98327bc03
GradelerM <marie.gradeler@gmail.com>
2024-07-29 17:23:22 +0200

    must add permissions

--- apps/accountability/views.py
+++ apps/accountability/views.py
@@ -5,9 +5,12 @@ from django.db.models import Q
 from rest_framework import viewsets, generics
 from rest_framework.request import Request
 from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
 
 from drf_spectacular.utils import extend_schema, OpenApiParameter, OpenApiExample
 from drf_spectacular.types import OpenApiTypes
+
+from apps.landmatrix.permissions import IsAdministrator, IsReporterOrHigher
 
 from apps.accountability.models import VggtChapter, VggtArticle, VggtVariable
 from apps.accountability.models import DealScore, DealVariable, Project
