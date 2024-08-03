98cc2dd7270db51d3a532be12310a4a146af6fa2
Marcus Theisen <marcus.theisen@sinnwerkstatt.com>
2024-08-03 20:16:50 +0200

    Add script to check version status integrity.

---
+++ apps/landmatrix/management/commands/check_status_integrity.py
@@ -0,0 +1,24 @@
+from django.core.management import BaseCommand
+
+from apps.landmatrix.models.new import InvestorHull, DealHull
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+
+        for model in [InvestorHull, DealHull]:
+            print(f"Iterating {model.__name__}s.")
+
+            for obj in model.objects.filter(deleted=False):
+                if obj.active_version is not None:
+                    version = obj.active_version
+                    if not version.status == "ACTIVATED":
+                        print("should be active:", obj, version, version.status)
+
+                if obj.draft_version is not None:
+                    version = obj.draft_version
+                    if version.status == "ACTIVATED":
+                        print("should be draft:", obj, version, version.status)
+            print()
+
+        print("DONE")
