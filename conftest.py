576a387eb206d6ab77bdac0c5684441541010109
Marcus Theisen <marcus.theisen@sinnwerkstatt.com>
2024-10-05 17:41:32 +0200

    Fix some tests.

--- conftest.py
+++ conftest.py
@@ -1,13 +1,24 @@
+from pathlib import Path
+
 import pytest
 
 from django.core.management import call_command
+
+ROOT_DIR = Path(__file__).parent
+FIXTURES_DIR = ROOT_DIR / "apps/landmatrix/fixtures"
 
 
 @pytest.fixture(scope="session")
 def django_db_setup(django_db_setup, django_db_blocker):
     with django_db_blocker.unblock():
-        call_command("loaddata", "apps/landmatrix/fixtures/countries_and_regions.json")
-        call_command("loaddata", "apps/landmatrix/fixtures/users_and_groups.json")
+        call_command(
+            "loaddata",
+            FIXTURES_DIR / "countries_and_regions.json",
+        )
+        call_command(
+            "loaddata",
+            FIXTURES_DIR / "users_and_groups.json",
+        )
 
 
 @pytest.fixture(autouse=True)
