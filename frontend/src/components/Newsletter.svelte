9a6794b6c8bf746f7c1439075ceafeed6e30659c
Marcus Theisen <marcus.theisen@sinnwerkstatt.com>
2024-10-14 12:56:28 +0200

    Fix newsletter heading.

--- frontend/src/components/Newsletter.svelte
+++ frontend/src/components/Newsletter.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
-  const list_key = "cb4e9fb08e"
+  import { _ } from "svelte-i18n"
+
+  const listId = "cb4e9fb08e"
   const u = "93aa5e8fafa0f5b66b2a051cc"
 </script>
 
@@ -13,15 +15,17 @@
     target="_blank"
   >
     <input type="hidden" name="u" value={u} />
-    <input type="hidden" name="id" value={list_key} />
+    <input type="hidden" name="id" value={listId} />
     <input type="text" name="honeypot" id="honeypot" style="display:none;" />
     <input
       type="email"
       name="EMAIL"
-      placeholder="Enter your email"
+      placeholder={$_("Enter your email")}
       required
       class=" mx-2 w-[300px] rounded p-2 text-black"
     />
-    <button class="btn btn-orange mx-2 mt-2 sm:mt-0" type="submit">Subscribe</button>
+    <button class="btn btn-orange mx-2 mt-2 sm:mt-0" type="submit">
+      {$_("Subscribe")}
+    </button>
   </form>
 </div>
