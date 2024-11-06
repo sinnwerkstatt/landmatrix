d1c734bd6da5b125a5b1b2b57930831149b93d75
Marcus Theisen <marcus.theisen@sinnwerkstatt.com>
2024-11-06 15:04:55 +0100

    Create AboutIndex and ObservatoryIndex pages during initial_setup.

--- apps/landmatrix/management/commands/initial_setup.py
+++ apps/landmatrix/management/commands/initial_setup.py
@@ -7,7 +7,11 @@ from django.db.transaction import atomic
 from wagtail.models import Page, Site
 
 from apps.accounts.models import User
-from apps.wagtailcms.models import WagtailPage, WagtailRootPage
+from apps.wagtailcms.models import (
+    WagtailRootPage,
+    AboutIndexPage,
+    ObservatoryIndexPage,
+)
 
 UserModel: Type[User] = get_user_model()
 
@@ -55,28 +59,40 @@ class Command(BaseCommand):
         )
         root_page.add_child(instance=wt_root)
         wt_root.save()
-        Site.objects.get_or_create(
-            hostname="localhost", port=9000, root_page=wt_root, is_default_site=True
+
+        observatory_root = ObservatoryIndexPage(
+            title="Global Observatory",
+            slug="global",
         )
+        wt_root.add_child(instance=observatory_root)
+        observatory_root.save_revision().publish()
 
-        WagtailPage(title="Global Observatory", slug="global")
+        about_root = AboutIndexPage(
+            title="About",
+            slug="about",
+        )
+        wt_root.add_child(instance=about_root)
+        about_root.save_revision().publish()
 
-        # country_index = CountryIndex(title="Countries", slug="country")
-        # wt_root.add_child(instance=country_index)
-        # uganda = CountryPage(title="Uganda")
-        # country_index.add_child(instance=uganda)
-        #
-        # region_index = RegionIndex(title="Regions", slug="region")
-        # wt_root.add_child(instance=region_index)
-        # asia = RegionPage(
-        #     title="Asia", slug="asia", region=Region.objects.get(name="Asia")
-        # )
-        # region_index.add_child(instance=asia)
+        wt_root.save()
 
+        # Reset site to localhost 900
+        # TODO: This should only happen in in dev
+        Site.objects.get_or_create(
+            hostname="localhost",
+            port=9000,
+            root_page=wt_root,
+            is_default_site=True,
+        )
         print("\033[92m" + "OK" + "\033[0m")
 
         print("  Creating users... ", end="", flush=True)
         create_user(
-            "landmatrixuser", "testuser@domain.tld", "landmatrix", "Land", "Matrix"
+            "landmatrixuser",
+            "testuser@domain.tld",
+            "landmatrix",
+            "Land",
+            "Matrix",
+            superuser=True,
         )
         print("\033[92m" + "OK" + "\033[0m")
