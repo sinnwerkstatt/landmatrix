6e91db2a25c06d1199bc97107fcacae81ba1294d
Marcus Theisen <marcus.theisen@sinnwerkstatt.com>
2024-11-19 16:27:24 +0100

    Fix visibility for draft deals.

--- apps/landmatrix/signals.py
+++ apps/landmatrix/signals.py
@@ -1,24 +1,18 @@
 from django.db.models.signals import post_delete, post_save
 from django.dispatch import receiver
 
-from .models.deal import DealVersion
 from .models.investor import InvestorHull, Involvement
 
 
 @receiver(post_save, sender=InvestorHull)
 @receiver(post_delete, sender=InvestorHull)
-def investor_change_trigger_refresh_calculated_deal_fields(
-    sender, instance: InvestorHull, **kwargs
-):
-    dealversion: DealVersion
-    for dealversion in instance.get_affected_dealversions():
-        dealversion.save(recalculate_independent=False)
+def investor_updated(sender, instance: InvestorHull, **kwargs):
+    for deal_version in instance.get_involved_deal_versions():
+        deal_version.save(recalculate_independent=False)
 
 
 @receiver(post_save, sender=Involvement)
 @receiver(post_delete, sender=Involvement)
-def involvements_updated(sender, instance: Involvement, **kwargs):
-    # Only consider the child_investors' deals. Because:
-    # On an Involvement update the deals of the parent_investor are not affected.
-    for deal in instance.child_investor.get_affected_dealversions():
-        deal.save(recalculate_independent=False)
+def involvement_updated(sender, instance: Involvement, **kwargs):
+    for deal_version in instance.child_investor.get_involved_deal_versions():
+        deal_version.save(recalculate_independent=False)
