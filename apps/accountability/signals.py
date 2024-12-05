c7bdd6b05057e8e51e562aea49556d90e0a49776
Marcus Theisen <marcus.theisen@sinnwerkstatt.com>
2024-12-05 12:41:44 +0100

    Fix accountability signal and default score_options.

--- apps/accountability/signals.py
+++ apps/accountability/signals.py
@@ -9,11 +9,11 @@ from apps.accountability.models import DealScore, DealScoreVersion
 def create_deal_score(sender, instance, **kwargs):
     # Only care for activated deals
     if instance.status == "ACTIVATED":
-        if not DealScore.objects.filter(deal=instance.deal).exists():
-            deal_score = DealScore(deal=instance.deal)
-            deal_score.save()
+        deal_score, _ = DealScore.objects.get_or_create(deal=instance.deal)
+
         if not DealScoreVersion.objects.filter(deal_version=instance).exists():
             deal_score_version = DealScoreVersion(
-                score=deal_score, deal_version=instance
+                score=deal_score,
+                deal_version=instance,
             )
             deal_score_version.save()
