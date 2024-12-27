fa065a4390380fa00dec73a357709dc0e7b04e23
Andreas Nüßlein <andreas@nuessle.in>
2024-12-27 12:39:56 +0000

    svelte5: steps - label remove "fielddefinitions"

--- frontend/src/components/Fields/Display2/Label2.svelte
+++ frontend/src/components/Fields/Display2/Label2.svelte
@@ -1,43 +1,14 @@
 <script lang="ts">
-  // import { _ } from "svelte-i18n"
+  import { twMerge } from "tailwind-merge"
 
-  // import { page } from "$app/stores"
+  interface Props {
+    value: string
+    class?: string
+  }
 
-  import { clickOutside } from "$lib/helpers"
-
-  // import { fieldDefinitions } from "$lib/stores"
-  // import type { FieldDefinition } from "$lib/types/generics"
-
-  // import type { FormField } from "$components/Fields/fields"
-  // import QuestionMarkCircleIcon from "$components/icons/QuestionMarkCircleIcon.svelte"
-
-  export let value: string
-
-  let showDefinition = false
-  if (showDefinition) console.log("deleteme, just lint 'fix'")
+  let { value, class: className = "" }: Props = $props()
 </script>
 
-<div
-  class="flex items-center gap-2 font-medium {$$props.class ?? ''}"
-  on:outClick={() => (showDefinition = false)}
-  use:clickOutside
->
+<div class={twMerge("flex items-center gap-2 font-medium", className)}>
   {value}
-  <!--{#if fd && isStaff($page.data.user)}-->
-  <!--  <div class="relative flex items-center">-->
-  <!--    <button on:click|preventDefault={() => (showDefinition = true)}>-->
-  <!--      <QuestionMarkCircleIcon class="h-5 w-5 text-orange-400" />-->
-  <!--    </button>-->
-  <!--    {#if showDefinition}-->
-  <!--      <div-->
-  <!--        class="absolute top-full left-full z-30 w-80 border border-gray-900 bg-gray-100 px-4 py-3"-->
-  <!--      >-->
-  <!--        <div class="text-lg text-purple-500">{value}</div>-->
-  <!--        &lt;!&ndash;{fd.short_description}&ndash;&gt;-->
-  <!--        &lt;!&ndash;{JSON.stringify(fd)}&ndash;&gt;-->
-  <!--        &lt;!&ndash;x&ndash;&gt;-->
-  <!--      </div>-->
-  <!--    {/if}-->
-  <!--  </div>-->
-  <!--{/if}-->
 </div>
