3844d6459a700ee1d6219ec6d864141900b36a26
marcus <marcus.theisen@sinnwerkstatt.com>
2025-02-03 12:01:56 +0100

    Improve frontend registration form.

--- frontend/src/routes/(main)/account/register/+page.svelte
+++ frontend/src/routes/(main)/account/register/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import { toast } from "@zerodevx/svelte-toast"
   import { _ } from "svelte-i18n"
+  import type { FormEventHandler } from "svelte/elements"
 
   import { getCsrfToken, slugify } from "$lib/utils"
 
@@ -17,67 +18,80 @@
     password: "",
     password_confirm: "",
   })
-  let token = ""
-  let disabled = $state(true)
-  let register_failed_message = $state("")
-  let registration_successful = $state(false)
 
-  function captchaVerified(_token: string) {
-    token = _token
-    disabled = false
+  let arePasswordsEqual: boolean = $derived(user.password === user.password_confirm)
+
+  let captchaToken: string | null = $state(null)
+  let registrationSuccessful = $state(false)
+  let registrationErrorMessage = $state("")
+
+  let errorMessageMap = $derived({
+    captcha_problems: $_("Captcha validation failed. Please try again."),
+    username_already_exists: $_("Username already taken."),
+    unknown_error: $_("Registration failed. Please try again."),
+  })
+
+  let submitDisabled = $derived(captchaToken === null)
+
+  const setCaptchaToken = (token: string) => {
+    captchaToken = token
   }
 
-  function usernameChange() {
+  const sanitizeUsername: FormEventHandler<HTMLInputElement> = () => {
     user = { ...user, username: slugify(user.username) }
   }
 
-  async function register(e: SubmitEvent) {
+  const validateConfirmationPassword: FormEventHandler<HTMLInputElement> = event => {
+    const inputField = event.currentTarget as HTMLInputElement
+    inputField.setCustomValidity(arePasswordsEqual ? "" : $_("Passwords do not match."))
+  }
+
+  const register = async (e: SubmitEvent) => {
     e.preventDefault()
+
     const ret = await fetch("/api/user/register/", {
       method: "POST",
       credentials: "include",
-      body: JSON.stringify({ ...user, token }),
+      body: JSON.stringify({ ...user, token: captchaToken }),
       headers: {
         "X-CSRFToken": await getCsrfToken(),
         "Content-Type": "application/json",
       },
     })
-    const retJson = await ret.json()
-    // console.log(retJson)
+
     if (!ret.ok) {
-      return toast.push(`Unknown Problem: ${retJson}`, { classes: ["error"] })
-    }
-    if (!retJson.ok) {
-      register_failed_message = retJson.error
+      toast.push(`Registration failed: ${ret.status}`, { classes: ["error"] })
       return
     }
-    registration_successful = true
+
+    const retJson = await ret.json()
+
+    if (retJson.ok) {
+      registrationSuccessful = true
+    } else {
+      registrationErrorMessage = errorMessageMap[retJson.code as never]
+    }
   }
 </script>
 
 <PageTitle>{$_("Register")}</PageTitle>
 
-{#if registration_successful}
-  <div
-    class="mb-4 flex h-full w-full items-center justify-center text-gray-700 dark:text-white"
-  >
+{#if registrationSuccessful}
+  <div class="mb-4 flex h-full w-full items-center justify-center">
     {$_(
       "Registration successful. You can now close this window and check your emails.",
     )}
   </div>
 {:else}
-  <div
-    class="mb-4 flex h-full w-full items-center justify-center text-gray-700 dark:text-white"
-  >
+  <div class="mb-4 flex h-full w-full items-center justify-center">
     {$_(
-      "Your contact information will help our researchers get in touch with you for additional information. We respect and protect your privacy and anonymity, and will never share or publish your personal information. You can also write us directly at data@landmatrix.org.",
+      "Your contact information will help our researchers get in touch with you for additional information. " +
+        "We respect and protect your privacy and anonymity, and will never share or publish your personal information. " +
+        "You can also write us directly at data@landmatrix.org.",
     )}
   </div>
 
-  <form
-    class="my-6 grid gap-4 text-gray-700 md:grid-cols-2 dark:text-white"
-    onsubmit={register}
-  >
+  <form class="my-6 grid gap-4 md:grid-cols-2" onsubmit={register}>
     <label>
       {$_("Username")}
       <input
@@ -88,10 +102,11 @@
         type="text"
         required
         pattern="[@_+.a-zA-Z0-9 \-]+"
-        oninput={usernameChange}
+        oninput={sanitizeUsername}
       />
       <span class="text-xs">{$_("Letters, digits and @/./+/-/_ only.")}</span>
     </label>
+
     <label>
       {$_("Email")}
       <input
@@ -103,21 +118,25 @@
         required
       />
     </label>
+
     <label>
       {$_("First name")}
       <input
         bind:value={user.first_name}
         class="inpt"
+        autocomplete="name"
         placeholder={$_("First name")}
         type="text"
         required
       />
     </label>
+
     <label>
       {$_("Last name")}
       <input
         bind:value={user.last_name}
         class="inpt"
+        autocomplete="family-name"
         placeholder={$_("Last name")}
         type="text"
         required
@@ -129,18 +148,22 @@
       <input
         bind:value={user.phone}
         class="inpt"
+        type="tel"
+        autocomplete="tel"
         placeholder={$_("Phone")}
-        type="text"
       />
     </label>
+
     <label>
       {$_("User information")}
       <textarea
         bind:value={user.information}
         class="inpt"
+        autocomplete="off"
         placeholder={$_(
-          "Write something about yourself and your company. This won't be published.",
+          "Write something about the reasons and motivation why you need a user account.",
         )}
+        required
       ></textarea>
     </label>
 
@@ -152,8 +175,10 @@
         autocomplete="new-password"
         placeholder={$_("Password")}
         type="password"
+        required
       />
     </label>
+
     <label>
       {$_("Password confirmation")}
       <input
@@ -162,14 +187,22 @@
         autocomplete="new-password"
         placeholder={$_("Password confirmation")}
         type="password"
+        oninput={validateConfirmationPassword}
+        required
       />
+      {#if user.password_confirm && !arePasswordsEqual}
+        <span class="text-xs">{$_("Passwords do not match.")}</span>
+      {/if}
     </label>
-    <HCaptcha class="flex w-full justify-center" onsuccess={captchaVerified} />
+
+    <HCaptcha class="flex w-full justify-center" onsuccess={setCaptchaToken} />
+
     <div class="flex items-center justify-center">
-      <button class="btn btn-primary w-full" type="submit" {disabled}>
+      <button class="btn btn-primary w-full" type="submit" disabled={submitDisabled}>
         {$_("Register")}
       </button>
     </div>
-    <p class="text-danger small mt-3">{register_failed_message}</p>
+
+    <p class="small mt-3 text-red-500">{registrationErrorMessage}</p>
   </form>
 {/if}
